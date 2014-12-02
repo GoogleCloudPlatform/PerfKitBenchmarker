f942e9fcade520118cca95988c3dccd051387c14
Connor McCoy <connormccoy@google.com>
2014-12-02 14:58:06 -0800

    Add license header to files missing one.

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-from ez_setup import use_setuptools
-use_setuptools()
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from setuptools import setup, find_packages
 
 setup(
+    name='perfkitbenchmarker',
     url='https://github.com/GoogleCloudPlatform/PerfKitBenchmarker',
     version='0.6.0',
     license='Apache 2.0',
