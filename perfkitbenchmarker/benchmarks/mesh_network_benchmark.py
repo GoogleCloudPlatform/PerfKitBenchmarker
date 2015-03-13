85172914fbf23b1a8042731bfcbacdaf12b94f2d
yuyanting <yuyantingzero@gmail.com>
2015-03-12 17:26:23 -0700

    Improve error message in exception.

--- perfkitbenchmarker/benchmarks/mesh_network_benchmark.py
+++ perfkitbenchmarker/benchmarks/mesh_network_benchmark.py
@@ -115,7 +115,9 @@ def RunNetperf(vm, benchmark_name, servers, result):
   match = re.findall(r'(\d+\.\d+)\s+\n', output)
   value = 0
   if len(match) != (len(servers) - 1) * FLAGS.num_connections:
-    raise errors.Benchmarks.RunError('Netserver not started on remote vm.')
+    raise errors.Benchmarks.RunError(
+        'Netserver not reachable. Expecting %s results, getting %s.' %
+        ((len(servers) - 1) * FLAGS.num_connections, len(match)))
   for res in match:
     if benchmark_name == 'TCP_RR':
       value += 1.0 / float(res) * 1000.0
