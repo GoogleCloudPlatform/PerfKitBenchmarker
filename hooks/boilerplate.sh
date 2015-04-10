bb947069b48e4df3d65fa0803dff2b090ae67e6e
Skyler Schneider <skschneider@google.com>
2015-04-10 12:56:08 -0700

    Modify boilerplate.sh to allow non-2014 copyright dates.

--- hooks/boilerplate.sh
+++ hooks/boilerplate.sh
@@ -31,14 +31,36 @@ fi
 LINES=$(cat "${REF_FILE}" | wc -l | tr -d ' ')
 if [[ "${EXT}" == "py" && -x "${FILE}" ]]; then
   # remove shabang and blank line from top of executable python files.
-  DIFFER=$(cat "${FILE}" | tail --lines=+3 | head "-${LINES}" | diff -q - "${REF_FILE}")
+  HEADER=$(cat "${FILE}" | tail --lines=+3 | head "-${LINES}")
 else
-  DIFFER=$(head "-${LINES}" "${FILE}" | diff -q - "${REF_FILE}")
+  HEADER=$(head "-${LINES}" "${FILE}")
 fi
+DIFFER=$(echo "${HEADER}" | diff - "${REF_FILE}")
 
 if [[ -z "${DIFFER}" ]]; then
+  # Header does not differ.
   echo "1"
   exit 0
+fi
+
+if [ "$(echo "${DIFFER}" | wc -l)" -eq 4 ]; then
+  # Header differs by one line. Check if only copyright date differs. If so,
+  # the contents of "${DIFFER}" should resemble:
+  #     1c1
+  #     < # Copyright 2015 Google Inc. All rights reserved.
+  #     ---
+  #     > # Copyright 2014 Google Inc. All rights reserved.
+  LINE_PREFIX='# Copyright '
+  LINE_SUFFIX=' Google Inc[.] All rights reserved[.]'
+  HEADER_LINE=${LINE_PREFIX}'[0-9]+'${LINE_SUFFIX}
+  REF_LINE=${LINE_PREFIX}'2014'${LINE_SUFFIX}
+  DIFF_PREFIX='[[:alnum:]]+[[:space:]]*< '
+  DIFF_MIDDLE='[[:space:]]*---[[:space:]]*> '
+  DIFF_PATTERN='^'${DIFF_PREFIX}${HEADER_LINE}${DIFF_MIDDLE}${REF_LINE}'$'
+  if [[ "${DIFFER}" =~ $DIFF_PATTERN ]]; then
+    echo "1"
+    exit 0
+  fi
 fi
 
 echo "0"
