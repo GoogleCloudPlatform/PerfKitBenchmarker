c0d78eda37bf1dede01a29609f1b510db194d01d
Klaus Weidner <klausw@google.com>
2015-06-03 17:13:00 -0700

    pkb: Add DigitalOcean cloud support.
    Add initial support for DigitalOcean as a cloud provider.
    
    This uses the `curl` tool to access the DigitalOcean v2 API. You must set
     up authentication before use, see README.md for instructions.
    
    Since DigitalOcean does not provide add-on disks, droplets always use local
    SSD storage as mounted on a single root partition, and AFAIK this cannot be
    repartitioned. Benchmarks that request a data directory via vm.GetScratchDir()
    will use a subdirectory of the root partition.
    
    Benchmarks that need multiple scratch disks or raw devices are **NOT**
    supported and will fail to run in such configurations. This includes
    *copy_throughput* in disk-to-disk mode, or *aerospike* when using
    non-memory storage modes.
    
    Usage example:
    
    ```
    ./pkb.py --cloud=DigitalOcean --machine_type=16gb --benchmarks=iperf
    ```
    
    Working benchmarks on a 16gb VM (droplet):
    
    * aerospike, block_storage_workload, bonnie++, cassandra_stress,
      cluster_boot, coremark, fio, hpcc, iperf, mesh_network, mongodb, netperf,
      oldisim, ping, redis, scimark2, silo, sysbench_oltp, unixbench
    
    Test in progress:
    
    * speccpu2006
    
    Not yet tested:
    
    * hadoop_terasort, hbase_ycsb
    
    Unsupported benchmarks:
    
    * _copy_throughput:_ this needs multiple scratch disks.
    * _object_storage_service:_ DigitalOcean doesn't offer an object store.
      It should work to run the benchmark using an external object store
      provider, I haven't tested that.

---
+++ perfkitbenchmarker/digitalocean/digitalocean_network.py
@@ -0,0 +1,34 @@
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module containing classes related to DigitalOcean VM networking."""
+
+import threading
+
+from perfkitbenchmarker import network
+
+
+class DigitaloceanFirewall(network.BaseFirewall):
+  """A dummy firewall for DigitalOcean, this does nothing."""
+  pass
+
+class DigitaloceanNetwork(network.BaseNetwork):
+  """Object representing a DigitalOcean Network."""
+
+  def Create(self):
+    """Creates the actual network."""
+    pass
+
+  def Delete(self):
+    """Deletes the actual network."""
+    pass
