3e48f1c3b3c2cd6f44f0f0950653af32082e8418
Eric Hankland <ehankland@google.com>
2015-06-18 10:39:56 -0700

    Improve error message when a machine doesn't have enough local disks to run a benchmark.

--- perfkitbenchmarker/benchmark_spec.py
+++ perfkitbenchmarker/benchmark_spec.py
@@ -18,6 +18,7 @@ import logging
 import pickle
 
 from perfkitbenchmarker import disk
+from perfkitbenchmarker import errors
 from perfkitbenchmarker import flags
 from perfkitbenchmarker import static_virtual_machine
 from perfkitbenchmarker import virtual_machine
@@ -175,6 +176,13 @@ class BenchmarkSpec(object):
             not FLAGS['num_striped_disks'].present):
           num_striped_disks = (vm.max_local_disks /
                                benchmark_info['scratch_disk'])
+          if num_striped_disks == 0:
+            raise errors.Error(
+                'Not enough local disks to run benchmark "%s". It requires at '
+                'least %d local disk(s). The specified machine type has %d '
+                'local disk(s).' % (benchmark_info['name'],
+                                    int(benchmark_info['scratch_disk']),
+                                    vm.max_local_disks))
         else:
           num_striped_disks = FLAGS.num_striped_disks
         for i in range(benchmark_info['scratch_disk']):
