d6c576102e535cd6e5d9e68923ca4c4633b8dad3
Klaus Weidner <klausw@google.com>
2015-07-06 14:45:51 -0700

    Refactor hooks, add opt-in pre-push hook.
    See hooks/README.md for implementation notes.
    
    Motivtion: the old hooks are hard to run manually, and code reuse among
    hooks is difficult. Goal of this refactor is to cleanly split the steps
    involved:
    
    - determine set of changed files
    - run checks on specific files
    - report issues based on check results
    
    The old logic was based on running checks on individual files, printing
    "1" on STDOUT for bad files. Now, check-* scripts take a list of files
    as args and print filenames of bad files on STDOUT. A toplevel "check"
    script drives these individual checks and contains common hook logic
    such as problem reporting.  The toplevel hooks themselves use the
    "check" script to take appropriate actions.
    
    Also change checks to use strict `-euo pipefail` error handling where
    possible, and standardize variable naming. (Uppercase for top-of-file
    constants or vars exported to the environment, otherwise lowercase.)

---
+++ hooks/lib/check-lint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+if [[ -z "$(command -v flake8)" ]]; then
+  >&2 echo "Missing flake8. Install it via 'pip' to enable linting."
+  exit 1
+fi
+
+# Treat errors from grep other than the "not found" code 1 as fatal problems.
+modified_py=($(printf -- '%s\n' "$@" | grep '\.py$' || [[ $? -le 1 ]]))
+
+# Don't run linter with no arguments, that would check all files.
+if [[ "${#modified_py[@]}" -ne 0 ]]; then
+  # First, run flake with normal output so that the user sees errors.
+  # If there are problems, re-run flake8, printing filenames only.
+  if ! flake8 "${modified_py[@]}" >&2; then
+    flake8 -q "${modified_py[@]}" || true
+  fi
+fi
+
+exit 0
