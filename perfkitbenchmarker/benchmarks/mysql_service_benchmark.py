2709d801519a1785621da253e0ba643b4e1f5cfd
Stanley Feng <stfeng@google.com>
2015-07-13 22:00:11 -0700

    Continue adding support for sysbench, and implement logic to parse the output!

--- perfkitbenchmarker/benchmarks/mysql_service_benchmark.py
+++ perfkitbenchmarker/benchmarks/mysql_service_benchmark.py
@@ -21,6 +21,8 @@ Currently it focuses on managed MySQL services.
 """
 import json
 import logging
+import re
+import StringIO
 import time
 
 from perfkitbenchmarker import benchmark_spec as benchmark_spec_class
@@ -37,6 +39,19 @@ flags.DEFINE_integer('oltp_tables_count', 4,
                      'The number of tables used in sysbench oltp.lua tests')
 flags.DEFINE_integer('oltp_table_size', 100000,
                      'The number of rows of each table used in the oltp tests')
+flags.DEFINE_integer('sysbench_warmup_seconds', 120,
+                     'The duration of the warmup run in which results are '
+                     'discarded.')
+flags.DEFINE_integer('sysbench_run_seconds', 480,
+                     'The duration of the actual run in which results are '
+                     'collected.')
+flags.DEFINE_integer('sysbench_thread_count', 16,
+                     'The number of test threads on the client side.')
+flags.DEFINE_integer('sysbench_latency_percentile', 99,
+                     'The latency percentile we ask sysbench to compute.')
+flags.DEFINE_integer('sysbench_report_interval', 2,
+                     'The interval we ask sysbench to report results.')
+
 
 BENCHMARK_INFO = {'name': 'mysql_service',
                   'description': 'MySQL service benchmarks.',
@@ -52,6 +67,9 @@ DB_CREATION_STATUS_QUERY_LIMIT = 100
 
 # Constants defined for Sysbench tests.
 RAND_INIT_ON = 'on'
+DISABLE = 'disable'
+UNIFORM = 'uniform'
+OFF = 'off'
 MYSQL_ROOT_USER = 'root'
 MYSQL_ROOT_PASSWORD = ''
 
@@ -73,6 +91,86 @@ def GetInfo():
 
 class DBInstanceCreationError(Exception):
     pass
+
+
+def _PercentileCalculator(numbers):
+  numbers_sorted = sorted(numbers)
+  count = len(numbers_sorted)
+  total = sum(numbers_sorted)
+  result = {}
+  result['p1'] = numbers_sorted[int(count * 0.01)]
+  result['p5'] = numbers_sorted[int(count * 0.05)]
+  result['p50'] = numbers_sorted[int(count * 0.5)]
+  result['p90'] = numbers_sorted[int(count * 0.9)]
+  result['p99'] = numbers_sorted[int(count * 0.99)]
+  result['p99.9'] = numbers_sorted[int(count * 0.999)]
+  if count > 0:
+    average = total / float(count)
+    result['average'] = average
+    if count > 1:
+      total_of_squares = sum([(i - average) ** 2 for i in numbers])
+      result['stddev'] = (total_of_squares / (count - 1)) ** 0.5
+    else:
+      result['stddev'] = 0
+
+  return result
+
+
+def _ParseSysbenchOutput(sysbench_output):
+  """Parses sysbench output and extract relevant TPS and latency numbers.
+
+  Specifically, we are interested in tps numbers reported by each reporting
+  interval, and the summary latency numbers printed at the end of the run in
+  "General Statistics" -> "Response Time".
+  """
+  all_tps = []
+  seen_general_statistics = False
+  seen_response_time = False
+  min_response_time = ''
+  avg_response_time = ''
+  max_response_time = ''
+  percentile_response_time = ''
+
+  sysbench_output_io = StringIO.StringIO(sysbench_output)
+  for line in sysbench_output_io.readlines():
+    print line
+    if re.match('^\[', line):
+      tps = re.findall('tps: (.*?),', line)
+      print 'tps is %s' % tps[0]
+      all_tps.append(float(tps[0]))
+      continue
+
+    if line.startswith('General statistics:'):
+      seen_general_statistics = True
+      continue
+
+    if seen_general_statistics:
+      if re.match('^ +response time:.*', line):
+        seen_response_time = True
+        continue
+
+    if seen_general_statistics and seen_response_time:
+      if re.findall('min: +(.*)', line):
+        min_response_time = re.findall('min: +(.*)', line)[0]
+      if re.findall('avg: +(.*)', line):
+        avg_response_time = re.findall('avg: +(.*)', line)[0]
+      if re.findall('max: +(.*)', line):
+        max_response_time = re.findall('max: +(.*)', line)[0]
+      if re.findall('.* percentile: +(.*)', line):
+        percentile_response_time = re.findall('.* percentile: +(.*)', line)[0]
+
+  tps_line = ', '.join(map(str, all_tps))
+  print tps_line
+  tps_percentile = _PercentileCalculator(all_tps)
+  print 'p50 tps %f' % tps_percentile['p50']
+  print 'p90 tps %f' % tps_percentile['p90']
+  print 'p99 tps %f' % tps_percentile['p99']
+  print 'stddev %f' % tps_percentile['stddev']
+
+  print min_response_time
+  print max_response_time
+  print avg_response_time
+  print percentile_response_time
 
 
 class RDSMySQLBenchmark(object):
@@ -190,6 +288,8 @@ class GoogleCloudSQLBenchmark(object):
                           '--mysql-host=%s' % vm.db_instance_ip,
                           'run']
     prepare_cmd = ' '.join(prepare_cmd_tokens)
+    # Sysbench output is in stdout, but we also get stderr just in case
+    # something went wrong.
     stdout, stderr = vm.RobustRemoteCommand(prepare_cmd)
     prepare_duration = time.time() - prepare_start_time
     logging.info('It took %d seconds to finish the prepare step',
@@ -204,7 +304,35 @@ class GoogleCloudSQLBenchmark(object):
         LATENCY_UNIT,
         metadata))
 
-    # Runs the actual sysbench test and parse the results.
+    # Now run the sysbench OLTP test and parse the results.
+
+    # First step is to run the test long enough to cover the warmup period
+    # as requested by the caller.
+    if FLAGS.sysbench_warmup_seconds > 0:
+      run_cmd_tokens = ['sysbench',
+                        '--test=%s' % OLTP_SCRIPT_PATH,
+                        '--oltp_tables_count=%d' % FLAGS.oltp_tables_count,
+                        '--oltp-table-size=%d' % FLAGS.oltp_table_size,
+                        '--rand-init=%s' % RAND_INIT_ON,
+                        '--db-ps-mode=%s' % DISABLE,
+                        '--oltp-dist-type=%s' % UNIFORM,
+                        '--oltp-read-only=%s' % OFF,
+                        '--num-threads=%d' % FLAGS.sysbench_thread_count,
+                        '--percentile=%d' % FLAGS.sysbench_latency_percentile,
+                        '--report-interval=%d' % FLAGS.sysbench_report_interval,
+                        '--max-time=%d' % FLAGS.sysbench_warmup_seconds,
+                        '--mysql-user=%s' % MYSQL_ROOT_USER,
+                        '--mysql-password="%s"' % MYSQL_ROOT_PASSWORD,
+                        '--mysql-host=%s' % vm.db_instance_ip,
+                        'run']
+      run_cmd = ' '.join(run_cmd_tokens)
+      stdout, stderr = vm.RobustRemoteCommand(run_cmd)
+      logging.info('Warmup results: \n stdout is:\n%s\nstderr is\n%s',
+                   stdout, stderr)
+      logging.info('\n Parsing Sysbench Results...\n')
+      _ParseSysbenchOutput(stdout)
+
+    # Then we launch the actual test.
 
     return results
 
