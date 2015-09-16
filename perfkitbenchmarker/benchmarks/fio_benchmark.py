0b6f5f559725500d182f7f29bdab7da2a8f36ed6
Noah Lavine <nlavine@google.com>
2015-09-16 15:46:51 -0700

    Add --run_for_minutes to fio benchmark
    Let the user specify how long they want the benchmark to run. Collect
    samples every 10 minutes for the duration of the job, annotated with
    how far into the job they were collected.

--- perfkitbenchmarker/benchmarks/fio_benchmark.py
+++ perfkitbenchmarker/benchmarks/fio_benchmark.py
@@ -18,6 +18,7 @@ Man: http://manpages.ubuntu.com/manpages/natty/man1/fio.1.html
 Quick howto: http://www.bluestop.org/fio/HOWTO.txt
 """
 
+import datetime
 import json
 import logging
 import posixpath
@@ -90,11 +91,15 @@ flags.DEFINE_integer('working_set_size', None,
                      'The size of the working set, in GB. If not given, use '
                      'the full size of the device.',
                      lower_bound=0)
+flags.DEFINE_integer('run_for_minutes', 10,
+                     'Repeat the job scenario(s) for the given number of '
+                     'minutes. Only valid when using --generate_scenarios.',
+                     lower_bound=0)
 
 
 
 FLAGS_IGNORED_FOR_CUSTOM_JOBFILE = {
-    'generate_scenarios', 'io_depths'}
+    'generate_scenarios', 'io_depths', 'run_for_minutes'}
 
 
 IODEPTHS_REGEXP = re.compile(r'(\d+)(-(\d+))?$')
@@ -159,6 +164,9 @@ size={{size}}
 {% endfor %}
 {% endfor %}
 """
+
+MINUTES_PER_JOB = 10  # Must match the JOB_FILE_TEMPLATE above.
+SECONDS_PER_MINUTE = 60
 
 
 def GetIODepths(io_depths):
@@ -280,6 +288,10 @@ def Prepare(benchmark_spec):
       logging.warning('Fio job file specified. Ignoring options "%s"',
                       ', '.join(ignored_flags))
 
+  if FLAGS.run_for_minutes % MINUTES_PER_JOB != 0:
+    logging.warning('Runtime %s will be rounded up to the next multiple of %s '
+                    'minutes.' % (MINUTES_PER_JOB,))
+
   vm = benchmark_spec.vms[0]
   logging.info('FIO prepare on %s', vm)
   vm.Install('fio')
@@ -340,21 +352,46 @@ def Run(benchmark_spec):
 
   fio_command = 'sudo %s --output-format=json %s' % (fio.FIO_PATH,
                                                      REMOTE_JOB_FILE_PATH)
+
+  run_reps = FLAGS.run_for_minutes // MINUTES_PER_JOB
+  if FLAGS.run_for_minutes % MINUTES_PER_JOB != 0:
+    run_reps += 1
+    # We already warned the user about rounding during the prepare
+    # phase. No need to warn them again here.
+
+  disk = vm.scratch_disks[0]
+
   # TODO(user): This only gives results at the end of a job run
   #      so the program pauses here with no feedback to the user.
   #      This is a pretty lousy experience.
   logging.info('FIO Results:')
-  stdout, stderr = vm.RemoteCommand(fio_command, should_log=True)
 
-  disk = vm.scratch_disks[0]
-  return fio.ParseResults(
-      GetOrGenerateJobFileString(FLAGS.fio_jobfile,
-                                 disk,
-                                 FLAGS.against_device,
-                                 FLAGS.generate_scenarios,
-                                 GetIODepths(FLAGS.io_depths),
-                                 FLAGS.working_set_size),
-      json.loads(stdout))
+  samples = []
+  start_time = datetime.datetime.now()
+  for rep_num in xrange(run_reps):
+    logging.info('**** Repetition number %s of %s ****' % (rep_num, run_reps))
+    run_start = datetime.datetime.now()
+    stdout, stderr = vm.RemoteCommand(fio_command, should_log=True)
+
+    seconds_since_start = int(round((run_start - start_time).total_seconds()))
+    minutes_since_start = seconds_since_start // SECONDS_PER_MINUTE
+    if seconds_since_start % SECONDS_PER_MINUTE > (SECONDS_PER_MINUTE // 2):
+      minutes_since_start += 1
+    base_metadata = {
+        'repeat_number': rep_num,
+        'minutes_since_start': minutes_since_start
+    }
+    samples.extend(fio.ParseResults(
+        GetOrGenerateJobFileString(FLAGS.fio_jobfile,
+                                   disk,
+                                   FLAGS.against_device,
+                                   FLAGS.generate_scenarios,
+                                   GetIODepths(FLAGS.io_depths),
+                                   FLAGS.working_set_size),
+        json.loads(stdout),
+        base_metadata=base_metadata))
+
+  return samples
 
 
 def Cleanup(benchmark_spec):
