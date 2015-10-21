0388e7323452f7b2a94ac9593ffce07e35012e09
Noah Lavine <nlavine@google.com>
2015-10-21 14:23:29 -0700

    Add AWS disk integration tests

---
+++ tests/aws_disk_integration_test.py
@@ -0,0 +1,95 @@
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Integration tests for AWS scratch disks."""
+
+import os
+import unittest
+
+from perfkitbenchmarker import pkb
+from perfkitbenchmarker import test_util
+
+MOUNT_POINT = '/scratch'
+
+
+@unittest.skipUnless('PERFKIT_INTEGRATION' in os.environ,
+                     'PERFKIT_INTEGRATION not in environment')
+class AwsScratchDiskIntegrationTest(unittest.TestCase):
+  def setUp(self):
+    pkb.SetUpPKB()
+
+  def testEBSStandard(self):
+    test_util.testDiskMounts({
+        'vm_groups': {
+            'vm_group_1': {
+                'cloud': 'AWS',
+                'vm_spec': {
+                    'AWS': {
+                        'machine_type': 'm4.large',
+                        'zone': 'us-east-1a'
+                    }
+                },
+                'disk_spec': {
+                    'AWS': {
+                        'disk_type': 'standard',
+                        'disk_size': 2,
+                        'mount_point': MOUNT_POINT
+                    }
+                }
+            }
+        }
+    }, MOUNT_POINT)
+
+  def testEBSGP(self):
+    test_util.testDiskMounts({
+        'vm_groups': {
+            'vm_group_1': {
+                'cloud': 'AWS',
+                'vm_spec': {
+                    'AWS': {
+                        'machine_type': 'm4.large',
+                        'zone': 'us-east-1a'
+                    }
+                },
+                'disk_spec': {
+                    'AWS': {
+                        'disk_type': 'remote_ssd',
+                        'disk_size': 2,
+                        'mount_point': MOUNT_POINT
+                    }
+                }
+            }
+        }
+    }, MOUNT_POINT)
+
+  def testLocalSSD(self):
+    test_util.testDiskMounts({
+        'vm_groups': {
+            'vm_group_1': {
+                'cloud': 'AWS',
+                'vm_spec': {
+                    'AWS': {
+                        'machine_type': 'm3.medium',
+                        'zone': 'us-east-1a'
+                    }
+                },
+                'disk_spec': {
+                    'AWS': {
+                        'disk_type': 'local',
+                        'mount_point': MOUNT_POINT
+                    }
+                }
+            }
+        }
+    }, MOUNT_POINT)
