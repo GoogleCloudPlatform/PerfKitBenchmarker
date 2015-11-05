992a426910c1c6955db9467866d97aaedd3ff858
Skyler Schneider <skschneider@google.com>
2015-11-04 17:11:38 -0800

    Fix benchmark spec tests to pass benchmark name to constructor.

--- tests/benchmark_spec_test.py
+++ tests/benchmark_spec_test.py
@@ -90,7 +90,7 @@ class ConstructVmsTestCase(unittest.TestCase):
 
   def testSimpleConfig(self):
     config = configs.LoadConfig(SIMPLE_CONFIG, {}, NAME)
-    spec = benchmark_spec.BenchmarkSpec(config, UID)
+    spec = benchmark_spec.BenchmarkSpec(config, NAME, UID)
     spec.ConstructVirtualMachines()
 
     self.assertEqual(len(spec.vms), 1)
@@ -102,7 +102,7 @@ class ConstructVmsTestCase(unittest.TestCase):
 
   def testMultiCloud(self):
     config = configs.LoadConfig(MULTI_CLOUD_CONFIG, {}, NAME)
-    spec = benchmark_spec.BenchmarkSpec(config, UID)
+    spec = benchmark_spec.BenchmarkSpec(config, NAME, UID)
     spec.ConstructVirtualMachines()
 
     self.assertEqual(len(spec.vms), 2)
@@ -111,7 +111,7 @@ class ConstructVmsTestCase(unittest.TestCase):
 
   def testStaticVms(self):
     config = configs.LoadConfig(STATIC_VM_CONFIG, {}, NAME)
-    spec = benchmark_spec.BenchmarkSpec(config, UID)
+    spec = benchmark_spec.BenchmarkSpec(config, NAME, UID)
     spec.ConstructVirtualMachines()
 
     self.assertEqual(len(spec.vms), 4)
@@ -128,6 +128,6 @@ class ConstructVmsTestCase(unittest.TestCase):
 
   def testBadParameter(self):
     config = configs.LoadConfig(BAD_VM_PARAMETER_CONFIG, {}, NAME)
-    spec = benchmark_spec.BenchmarkSpec(config, UID)
+    spec = benchmark_spec.BenchmarkSpec(config, NAME, UID)
     with self.assertRaises(ValueError):
       spec.ConstructVirtualMachines()
