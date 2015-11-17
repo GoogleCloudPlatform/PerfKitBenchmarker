456110be093eac7bb5186e0ea456f3f735909e10
Skyler Schneider <skschneider@google.com>
2015-11-16 17:22:14 -0800

    Change Azure default image to parse value from 'azure vm image list'.

--- perfkitbenchmarker/providers/azure/azure_virtual_machine.py
+++ perfkitbenchmarker/providers/azure/azure_virtual_machine.py
@@ -26,6 +26,8 @@ operate on the VM: boot, shutdown, etc.
 """
 
 import json
+import re
+import threading
 
 from perfkitbenchmarker import disk
 from perfkitbenchmarker import errors
@@ -41,12 +43,57 @@ from perfkitbenchmarker.providers.azure import azure_network
 FLAGS = flags.FLAGS
 
 AZURE_PATH = 'azure'
-UBUNTU_IMAGE = ('b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-'
-                '14_04_3-LTS-amd64-server-20150908-en-us-30GB')
-CENTOS_IMAGE = ('0b11de9248dd4d87b18621318e037d37__RightImage-'
-                'CentOS-7.0-x64-v14.2.1')
-WINDOWS_IMAGE = ('a699494373c04fc0bc8f2bb1389d6106__Windows-Server'
-                 '-2012-R2-201505.01-en.us-127GB.vhd')
+
+# Dict mapping OS_TYPE string to re.RegexObject. The RegexObject matches the
+# names of images for that OS type while extracting integer components of the
+# version and date from the image name in order of decreasing priority.
+_default_image_patterns = {}
+# Lock-protected dict mapping OS_TYPE string to default image name string.
+_default_images_lock = threading.Lock()
+_default_images = None
+
+
+def _GetDefaultImagesFromAzure():
+  """Gets the default images for each OS type in _default_image_patterns.
+
+  Returns:
+    dict mapping OS_TYPE string to default image name string.
+
+  Raises:
+    errors.Error: If unable to get the default image for an OS_TYPE.
+  """
+  list_images_cmd = [AZURE_PATH, 'vm', 'image', 'list']
+  stdout, _ = vm_util.IssueRetryableCommand(list_images_cmd)
+  default_images = {}
+  for os_type, pattern in _default_image_patterns.iteritems():
+    images = {}
+    for match in pattern.finditer(stdout):
+      image_name_fields = tuple(None if g is None else int(g)
+                                for g in match.groups())
+      images[image_name_fields] = match.group(0)
+    if not images:
+      raise errors.Error(
+          'Unable to get Azure default {0} image. No image names match '
+          '{1}'.format(os_type, pattern.pattern))
+    default_images[os_type] = images[max(images)]
+  return default_images
+
+
+def _GetDefaultImage(os_type):
+  """Gets the default image name for a given VM operating system.
+
+  Args:
+    os_type: string. VM operating system.
+
+  Returns:
+    string. Name of the image.
+  """
+  global _default_images
+  if _default_images is None:
+    with _default_images_lock:
+      if _default_images is None:
+        _default_images = _GetDefaultImagesFromAzure()
+  return _default_images[os_type]
 
 
 class AzureService(resource.BaseResource):
@@ -90,12 +137,27 @@ class AzureService(resource.BaseResource):
     return True
 
 
+class AzureVirtualMachineMetaClass(virtual_machine.AutoRegisterVmMeta):
+  """Metaclass for AzureVirtualMachine.
+
+  Registers default image pattern for each operating system.
+  """
+
+  def __init__(cls, name, bases, dct):
+    super(AzureVirtualMachineMetaClass, cls).__init__(name, bases, dct)
+    if hasattr(cls, 'OS_TYPE'):
+      assert cls.OS_TYPE, cls
+      assert cls.DEFAULT_IMAGE_PATTERN, cls
+      _default_image_patterns[cls.OS_TYPE] = cls.DEFAULT_IMAGE_PATTERN
+
+
 class AzureVirtualMachine(virtual_machine.BaseVirtualMachine):
   """Object representing an Azure Virtual Machine."""
 
+  __metaclass__ = AzureVirtualMachineMetaClass
   CLOUD = 'Azure'
-  # Subclasses should override the default image.
-  DEFAULT_IMAGE = None
+  # Subclasses should override the default image pattern.
+  DEFAULT_IMAGE_PATTERN = None
 
   def __init__(self, vm_spec):
     """Initialize a Azure virtual machine.
@@ -111,7 +173,7 @@ class AzureVirtualMachine(virtual_machine.BaseVirtualMachine):
     disk_spec = disk.BaseDiskSpec()
     self.os_disk = azure_disk.AzureDisk(disk_spec, self.name)
     self.max_local_disks = 1
-    self.image = self.image or self.DEFAULT_IMAGE
+    self.image = self.image or _GetDefaultImage(self.OS_TYPE)
 
   def _CreateDependencies(self):
     """Create VM dependencies."""
@@ -216,18 +278,34 @@ class AzureVirtualMachine(virtual_machine.BaseVirtualMachine):
 
 class DebianBasedAzureVirtualMachine(AzureVirtualMachine,
                                      linux_virtual_machine.DebianMixin):
-  DEFAULT_IMAGE = UBUNTU_IMAGE
+
+  # Example: ('b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-'
+  #           '14_04_3-LTS-amd64-server-20150908-en-us-30GB')
+  DEFAULT_IMAGE_PATTERN = re.compile(
+      r'b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-'
+      r'([0-9]+)_([0-9]+)(?:_([0-9]+))?'
+      r'-LTS-amd64-server-([0-9]+)(?:[.]([0-9]+))?-en-us-30GB')
 
 
 class RhelBasedAzureVirtualMachine(AzureVirtualMachine,
                                    linux_virtual_machine.RhelMixin):
-  DEFAULT_IMAGE = CENTOS_IMAGE
+
+  # Example: ('0b11de9248dd4d87b18621318e037d37__RightImage-'
+  #           'CentOS-7.0-x64-v14.2.1')
+  DEFAULT_IMAGE_PATTERN = re.compile(
+      r'0b11de9248dd4d87b18621318e037d37__RightImage-'
+      r'CentOS-([0-9]+)[.]([0-9]+)-x64-'
+      r'v([0-9]+)(?:[.]([0-9]+))?(?:[.]([0-9]+))?(?:[.]([0-9]+))?')
 
 
 class WindowsAzureVirtualMachine(AzureVirtualMachine,
                                  windows_virtual_machine.WindowsMixin):
 
-  DEFAULT_IMAGE = WINDOWS_IMAGE
+  # Example: ('a699494373c04fc0bc8f2bb1389d6106__Windows-Server'
+  #           '-2012-R2-201505.01-en.us-127GB.vhd')
+  DEFAULT_IMAGE_PATTERN = re.compile(
+      r'a699494373c04fc0bc8f2bb1389d6106__Windows-Server'
+      r'-2012-R2-([0-9]+)(?:[.]([0-9]+))?-en.us-127GB.vhd')
 
   def __init__(self, vm_spec):
     super(WindowsAzureVirtualMachine, self).__init__(vm_spec)
