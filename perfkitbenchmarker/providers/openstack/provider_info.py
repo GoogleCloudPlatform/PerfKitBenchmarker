f541af8dbf0becf8909331d1c16c43e069827579
Kris Hildrum <hildrum@google.com>
2015-12-02 14:34:24 -0800

    Allow providers to selectively support benchmarks
    Responses to comments
    
    Add flag to control behavior for unknown benchmarks

---
+++ perfkitbenchmarker/providers/openstack/provider_info.py
@@ -0,0 +1,26 @@
+# Copyright 2015 PerfKitBenchmarker Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""" Provider info for Open Stack Platform
+
+"""
+
+from perfkitbenchmarker import provider_info
+from perfkitbenchmarker import benchmark_spec
+
+
+class OpenStackProviderInfo(provider_info.BaseProviderInfo):
+
+  UNSUPPORTED_BENCHMARKS = ['mysql_service']
+  CLOUD = benchmark_spec.OPENSTACK
