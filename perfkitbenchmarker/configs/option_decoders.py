9eace893d8d69ff05a2df7e3994a091b8918e36c
Mateusz Blaszkowski <mateusz.blaszkowski@intel.com>
2015-12-15 09:26:35 +0100

    Fix for the FloatDecoder used in Mesos provider.
    I must have done the merge improperly.
    
    Signed-off-by: Mateusz Blaszkowski <mateusz.blaszkowski@intel.com>

--- perfkitbenchmarker/configs/option_decoders.py
+++ perfkitbenchmarker/configs/option_decoders.py
@@ -187,33 +187,38 @@ class FloatDecoder(TypeVerifier):
     min: None or float. If provided, it specifies the minimum accepted value.
   """
 
-  def __init__(self, component, option, max=None, min=None, **kwargs):
-    super(FloatDecoder, self).__init__(component, option, (float,), **kwargs)
+  def __init__(self, option, max=None, min=None, **kwargs):
+    super(FloatDecoder, self).__init__(option, (float,), **kwargs)
     self.max = max
     self.min = min
 
-  def Decode(self, value):
-    """Verifies that the provided value is an int.
+  def Decode(self, value, component_full_name, flag_values):
+    """Verifies that the provided value is a float.
 
     Args:
       value: The value specified in the config.
+      component_full_name: string. Fully qualified name of the configurable
+          component containing the config option.
+      flag_values: flags.FlagValues. Runtime flag values to be propagated to
+          BaseSpec constructors.
 
     Returns:
-      int. The valid value.
+      float. The valid value.
 
     Raises:
       errors.Config.InvalidValue upon invalid input value.
     """
-    value = super(FloatDecoder, self).Decode(value)
+    value = super(FloatDecoder, self).Decode(value, component_full_name,
+                                           flag_values)
     if value is not None:
       if self.max and value > self.max:
         raise errors.Config.InvalidValue(
-            'Invalid {0} "{1}" value: "{2}". Value must be at most '
-            '{3}.'.format(self.component, self.option, value, self.max))
+            'Invalid {0}.{1} value: "{2}". Value must be at most '
+            '{3}.'.format(component_full_name, self.option, value, self.max))
       if self.min and value < self.min:
         raise errors.Config.InvalidValue(
-            'Invalid {0} "{1}" value: "{2}". Value must be at least '
-            '{3}.'.format(self.component, self.option, value, self.min))
+            'Invalid {0}.{1} value: "{2}". Value must be at least '
+            '{3}.'.format(component_full_name, self.option, value, self.min))
     return value
 
 
