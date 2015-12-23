77717ec0369e9efaa6f00b04cc4c386e3dd4bd20
Marcin Karkocha <marcin.karkocha@intel.com>
2015-12-23 04:48:24 -0500

    add device name

--- perfkitbenchmarker/providers/openstack/os_disk.py
+++ perfkitbenchmarker/providers/openstack/os_disk.py
@@ -92,7 +92,7 @@ class OpenStackDisk(disk.BaseDisk):
         self.attached_vm_id = vm.id
 
         result = self.__nclient.volumes.create_server_volume(vm.id,
-                                                             self._disk.id,)
+                                                             self._disk.id, self.device)
         self.attach_id = result.id
 
         volume = None
