93bc79740e46340d6e871a1225b5e1f66ab90336
Kris Hildrum <hildrum@google.com>
2016-01-29 13:51:14 -0800

    Eliminate caret as a special character

--- perfkitbenchmarker/vm_util.py
+++ perfkitbenchmarker/vm_util.py
@@ -733,7 +733,7 @@ def GenerateRandomWindowsPassword(password_length=PASSWORD_LENGTH):
   # special characters. This greatly limits the set of characters
   # that we can safely use. See
   # https://github.com/Azure/azure-xplat-cli/blob/master/lib/commands/arm/vm/vmOsProfile._js#L145
-  special_chars = '*!@#$%^+='
+  special_chars = '*!@#$%+='
   password = [
       random.choice(string.ascii_letters + string.digits + special_chars)
       for _ in range(password_length - 4)]
