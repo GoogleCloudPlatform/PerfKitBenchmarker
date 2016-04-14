ed4f7e05744d1d215eb3f7ca43879e954615099c
John B. Diamond <jdiamond@us.ibm.com>
2016-04-14 14:39:12 -0400

    First commit with SofLayer work in progress

---
+++ ReadMeSoftLayer.txt
@@ -0,0 +1,17 @@
+slcli setup
+
+Username []: jdiamond
+API Key or Password []:
+Endpoint (public|private|custom) [public]:
+Timeout [0]:
+:..............:..................................................................:
+:         name : value                                                            :
+:..............:..................................................................:
+:     Username : jdiamond                                                         :
+:      API Key : 3aeed4784e634fc8e2448ce9097548337760f79e749ce8154dfbfa5de9a1a2c5 :
+: Endpoint URL : https://api.softlayer.com/xmlrpc/v3.1/                           :
+:      Timeout : not set                                                          :
+:..............:..................................................................:
+Are you sure you want to write settings to "C:\Users\IBM_ADMIN\AppData\Roaming\softlayer"? [Y/n]: Y
+Configuration Updated Successfully
+
---
+++ perfkitbenchmarker/providers/softlayer/flags.py
@@ -0,0 +1,22 @@
+# Copyright 2015 PerfKitBenchmarker Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from perfkitbenchmarker import flags
+
+flags.DEFINE_string('aws_user_name', 'ubuntu',
+                    'This determines the user name that Perfkit will '
+                    'attempt to use. This must be changed in order to '
+                    'use any image other than ubuntu.')
+flags.DEFINE_integer('aws_provisioned_iops', None,
+                     'IOPS for Provisioned IOPS (SSD) volumes in AWS.')
