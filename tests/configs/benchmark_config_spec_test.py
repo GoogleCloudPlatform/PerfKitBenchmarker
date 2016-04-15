5aeb34faa84d8fd4d738cbb1f5d518f1cb0170f8
skschneider <skschneider@users.noreply.github.com>
2016-04-15 11:38:28 -0700

    Check VM group OS types for Linux/Windows mismatches. (#952)

--- tests/configs/benchmark_config_spec_test.py
+++ tests/configs/benchmark_config_spec_test.py
@@ -456,6 +456,21 @@ class BenchmarkConfigSpecTestCase(unittest.TestCase):
         '.disk_size value: "0.5" (of type "float"). Value must be one of the '
         'following types: NoneType, int.'))
 
+  def testMismatchedOsTypes(self):
+    self._kwargs['vm_groups'] = {
+        os_type + '_group': {'os_type': os_type, 'vm_spec': _GCP_AWS_VM_CONFIG}
+        for os_type in (os_types.DEBIAN, os_types.RHEL, os_types.WINDOWS)}
+    expected_os_types = os_types.JUJU, os_types.WINDOWS
+    with self.assertRaises(errors.Config.InvalidValue) as cm:
+      self._spec_class(_COMPONENT, expected_os_types=expected_os_types,
+                       flag_values=flags.FLAGS, **self._kwargs)
+    self.assertEqual(str(cm.exception), (
+        "VM groups in test_component may only have the following OS types: "
+        "'juju', 'windows'. The following VM group options are invalid:{sep}"
+        "test_component.vm_groups['debian_group'].os_type: 'debian'{sep}"
+        "test_component.vm_groups['rhel_group'].os_type: 'rhel'".format(
+            sep=os.linesep)))
+
   def testFlagOverridesPropagate(self):
     self._kwargs['flags'] = {'cloud': providers.AWS,
                              'ignore_package_requirements': True}
