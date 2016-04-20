53cea5c21ef3bb1c1a4f04d86cb99b6ad7b304c0
skschneider <skschneider@users.noreply.github.com>
2016-04-20 15:14:24 -0700

    Improve Azure CLI version check. (#967)
    Add or expand error messages for when `azure -v` fails or returns an
    incorrect version. Exit PKB unless the version is correct or the
    user specifies --azure_ignore_cli_version.

--- perfkitbenchmarker/providers/azure/__init__.py
+++ perfkitbenchmarker/providers/azure/__init__.py
@@ -11,3 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+EXPECTED_CLI_VERSION = '0.9.9'
