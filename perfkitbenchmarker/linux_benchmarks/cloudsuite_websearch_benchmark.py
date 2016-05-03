46e61df6f724e669faaaad561f0e9689408a5010
Javier Picorel <javier.picorel@epfl.ch>
2016-05-03 17:20:14 +0200

    The commit applies the following changes: - Replace the old CloudSuite WebSearch benchmark with the new Docker version. - Includes CloudSuite as one of the benchmarks in the README file.

--- perfkitbenchmarker/linux_benchmarks/cloudsuite_websearch_benchmark.py
+++ perfkitbenchmarker/linux_benchmarks/cloudsuite_websearch_benchmark.py
@@ -12,197 +12,129 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Runs CloudSuite Web Search benchmark.
+"""Runs the Web Search benchmark of Cloudsuite.
 
-Docs:
-http://parsa.epfl.ch/cloudsuite/
-
-Runs CloudSuite Web Search to collect the statistics that show
-the operations completed per second and the minimum, maximum,
-average, 90th, and 99th response times.
+More info: http://cloudsuite.ch/websearch/
 """
 
-import posixpath
 import re
 import time
 
 from perfkitbenchmarker import configs
-from perfkitbenchmarker import vm_util
 from perfkitbenchmarker import flags
 from perfkitbenchmarker import sample
 
-from perfkitbenchmarker.linux_packages import solr as solr
-from perfkitbenchmarker.linux_packages import faban
-from perfkitbenchmarker.linux_packages.ant import ANT_HOME_DIR
-
 FLAGS = flags.FLAGS
+
+flags.DEFINE_string('cs_websearch_client_heap_size',
+                    '2g',
+                    'Java heap size for Faban client in the usual java format.'
+                    ' Default: 2g.')
+
+flags.DEFINE_string('cs_websearch_server_heap_size',
+                    '3g',
+                    'Java heap size for Solr server in the usual java format.'
+                    ' Default: 3g.')
+flags.DEFINE_enum('cs_websearch_query_distr',
+                  'Random',
+                  ['Random', 'Ziphian'],
+                  'Distribution of query terms. '
+                  'Random and Ziphian distributions are available. '
+                  'Default: Random.')
+flags.DEFINE_integer('cs_websearch_num_clients',
+                     1,
+                     'Number of client machines.',
+                     lower_bound=1)
+flags.DEFINE_integer('cs_websearch_ramp_up',
+                     90,
+                     'Benchmark ramp up time in seconds.',
+                     lower_bound=1)
+flags.DEFINE_integer('cs_websearch_ramp_down',
+                     60,
+                     'Benchmark ramp down time in seconds.',
+                     lower_bound=1)
+flags.DEFINE_integer('cs_websearch_steady_state',
+                     60,
+                     'Benchmark steady state time in seconds.',
+                     lower_bound=1)
+flags.DEFINE_integer('cs_websearch_scale',
+                     50,
+                     'Number of simulated web search users.',
+                     lower_bound=1)
 
 BENCHMARK_NAME = 'cloudsuite_websearch'
 BENCHMARK_CONFIG = """
 cloudsuite_websearch:
-  description: Run CloudSuite Web Search
+  description: >
+    Run Cloudsuite Web Search benchmark. Specify the number of worker
+    VMs with --num_vms.
   vm_groups:
-    workers:
+    servers:
       vm_spec: *default_single_core
       disk_spec: *default_500_gb
     clients:
       vm_spec: *default_single_core
+      vm_count: 1
 """
 
-CLOUDSUITE_WEB_SEARCH_DIR = posixpath.join(vm_util.VM_TMP_DIR,
-                                           'cloudsuite-web-search')
-
-FABAN_OUTPUT_DIR = posixpath.join(vm_util.VM_TMP_DIR, 'outputFaban')
-
-PACKAGES_URL = 'http://lsi-www.epfl.ch/parsa'
-INDEX_URL = posixpath.join(PACKAGES_URL, 'index')
-SOLR_CONFIG_URL = posixpath.join(PACKAGES_URL, 'solrconfig.xml')
-SCHEMA_URL = posixpath.join(PACKAGES_URL, 'schema.xml')
-SEARCH_DRIVER_URL = posixpath.join(PACKAGES_URL, 'search.tar.gz')
-
-SOLR_PORT = 8983
-
-CLASSPATH = ('$FABAN_HOME/lib/fabanagents.jar:$FABAN_HOME/lib/fabancommon.jar:'
-             '$FABAN_HOME/lib/fabandriver.jar:$JAVA_HOME/lib/tools.jar:'
-             '$FABAN_HOME/search/build/lib/search.jar')
-
-flags.DEFINE_string('cs_websearch_client_heap_size', '2g',
-                    'Java heap size for Faban client in the usual java format.'
-                    ' Default: 2g.')
-flags.DEFINE_string('cs_websearch_server_heap_size', '3g',
-                    'Java heap size for Solr server in the usual java format.'
-                    ' Default: 3g.')
-flags.DEFINE_enum('cs_websearch_query_distr', 'Random', ['Random', 'Ziphian'],
-                  'Distribution of query terms. '
-                  'Random and Ziphian distributions are available. '
-                  'Default: Random.')
-flags.DEFINE_integer('cs_websearch_num_clients', 1,
-                     'Number of client machines.', lower_bound=1)
-flags.DEFINE_integer('cs_websearch_ramp_up', 90,
-                     'Benchmark ramp up time in seconds.', lower_bound=1)
-flags.DEFINE_integer('cs_websearch_steady_state', 60,
-                     'Benchmark steady state time in seconds.', lower_bound=1)
-flags.DEFINE_integer('cs_websearch_scale', 50,
-                     'Number of simulated web search users.',
-                     lower_bound=1)
-
-
-def CheckPrerequisites():
-  """Verifies that the required resources are present.
-  Raises:
-      perfkitbenchmarker.data.ResourceNotFound: On missing resource.
-  """
-  if FLAGS.num_vms < 1:
-    raise ValueError('Cloudsuite Web Search requires at least 1 client VM.')
-
-  def _CheckHeapSize(heap_size_str):
-    m = re.match(r'(\d+)([mg])', heap_size_str)
-    if not m:
-      raise ValueError('Invalid heap size: {0}'.format(heap_size_str))
-
-  _CheckHeapSize(FLAGS.cs_websearch_server_heap_size)
-  _CheckHeapSize(FLAGS.cs_websearch_client_heap_size)
+DISK_PATH = '/scratch'
 
 
 def GetConfig(user_config):
   config = configs.LoadConfig(BENCHMARK_CONFIG, user_config, BENCHMARK_NAME)
-  num_clients = FLAGS.cs_websearch_num_clients
-  config['vm_groups']['clients']['vm_count'] = num_clients
+  if FLAGS['num_vms'].present:
+    config['vm_groups']['clients']['vm_count'] = FLAGS.num_vms
   return config
 
 
-def _PrepareSolr(solr_nodes):
-  """Starts and configures SolrCloud."""
-  basic_configs_dir = posixpath.join(solr.SOLR_HOME_DIR, 'server/solr/'
-                                     'configsets/basic_configs/conf')
-  server_heap_size = FLAGS.cs_websearch_server_heap_size
-  for vm in solr_nodes:
-    vm.Install('solr')
-    vm.RemoteCommand('cd {0} && wget {1}'.format(
-        basic_configs_dir,
-        SCHEMA_URL))
-    vm.RemoteCommand('cd {0} && wget {1}'.format(
-        basic_configs_dir,
-        SOLR_CONFIG_URL))
-    if vm == solr_nodes[0]:
-      solr.StartWithZookeeper(vm, SOLR_PORT, server_heap_size)
-    else:
-      solr.Start(vm, SOLR_PORT, vm, SOLR_PORT + 1000, server_heap_size)
-  solr.CreateCollection(solr_nodes[0], 'cloudsuite_web_search',
-                        len(solr_nodes), SOLR_PORT)
-
-
-def _BuildIndex(solr_nodes):
-  """Downloads Solr index and set it up."""
-  for vm in solr_nodes:
-    vm.RemoteCommand('cd {0} && '
-                     'bin/solr stop -p {1}'.format(
-                         solr.SOLR_HOME_DIR, SOLR_PORT))
-
-  def DownloadIndex(vm):
-    solr_core_dir = posixpath.join(vm.GetScratchDir(), 'solr_cores')
-    vm.RobustRemoteCommand('cd {0} && '
-                           'wget -O - {1} | '
-                           'tar zxvf - -C {2}'.format(
-                               solr_core_dir, INDEX_URL,
-                               'cloudsuite_web_search*'))
-
-  vm_util.RunThreaded(DownloadIndex, solr_nodes, len(solr_nodes))
-  server_heap_size = FLAGS.cs_websearch_server_heap_size
-  for vm in solr_nodes:
-    if vm == solr_nodes[0]:
-      solr.StartWithZookeeper(vm, SOLR_PORT, server_heap_size, False)
-    else:
-      solr.Start(vm, SOLR_PORT, solr_nodes[0], SOLR_PORT + 1000,
-                 server_heap_size, False)
-
-
-def _PrepareClient(clients):
-  """Prepares client machine in the benchmark."""
-  client_master = clients[0]
-  for client in clients:
-    client.Install('faban')
-    client.RemoteCommand('cd {0} && '
-                         'wget -O - {1} | '
-                         'tar -xzf -'.format(
-                             faban.FABAN_HOME_DIR, SEARCH_DRIVER_URL))
-    client.RemoteCommand('cd {0}/search && '
-                         'sed -i "/faban.home/c\\faban.home={0}" '
-                         'build.properties && '
-                         'sed -i "/ant.home/c\\ant.home='
-                         '{1}" build.properties && '
-                         'sed -i "/faban.url/c\\faban.url='
-                         'http://localhost:9980/" build.properties'.format(
-                             faban.FABAN_HOME_DIR, ANT_HOME_DIR))
-  faban.Start(client_master)
-  time.sleep(20)
+def _HasDocker(vm):
+  resp, _ = vm.RemoteCommand('command -v docker',
+                             ignore_failure=True,
+                             suppress_warning=True)
+  if resp.rstrip() == "":
+    return False
+  else:
+    return True
 
 
 def Prepare(benchmark_spec):
-  """Install cloudsuite web search and start the server on all machines.
+  """Install docker. Pull the required images from DockerHub.
+  Start Solr index node and client.
 
   Args:
     benchmark_spec: The benchmark specification. Contains all data that is
         required to run the benchmark.
   """
   vms = benchmark_spec.vms
-  servers = benchmark_spec.vm_groups['workers']
+  servers = benchmark_spec.vm_groups['servers'][0]
   clients = benchmark_spec.vm_groups['clients']
 
-  def PrepareVM(vm):
-    vm.Install('wget')
-    vm.RemoteCommand('mkdir -p {0}'.format(
-                     CLOUDSUITE_WEB_SEARCH_DIR))
+  for vm in vms:
+    if not _HasDocker(vm):
+      vm.Install('docker')
 
-  vm_util.RunThreaded(PrepareVM, vms)
+  server_cmd = ('sudo echo \'DOCKER_OPTS="-g %s"\''
+                '| sudo tee /etc/default/docker > /dev/null' % (DISK_PATH))
+  stdout, _ = servers.RemoteCommand(server_cmd, should_log=True)
 
-  _PrepareSolr(servers)
-  _PrepareClient(clients)
-  _BuildIndex(servers)
+  server_cmd = ('sudo service docker restart')
+  stdout, _ = servers.RemoteCommand(server_cmd, should_log=True)
+
+  for vm in clients:
+    vm.RemoteCommand('sudo docker pull cloudsuite/web-search:client')
+
+  servers.RemoteCommand('sudo docker pull cloudsuite/web-search:server')
+
+  server_cmd = ('sudo docker run -d --net host '
+                '--name server cloudsuite/web-search:server %s 1' %
+                (FLAGS.cs_websearch_server_heap_size))
+  stdout, _ = servers.RemoteCommand(server_cmd, should_log=True)
+
+  time.sleep(60)
 
 
 def Run(benchmark_spec):
-  """Run cloudsuite web search on the target vm.
+  """Run the Web Search benchmark.
 
   Args:
     benchmark_spec: The benchmark specification. Contains all data that is
@@ -211,120 +143,58 @@ def Run(benchmark_spec):
   Returns:
     A list of sample.Sample objects.
   """
-  clients = benchmark_spec.vm_groups['clients']
-  client_master = clients[0]
-  distribution = FLAGS.cs_websearch_query_distr
-  terms_file = None
-  search_driver = None
-  if distribution == 'Random':
-    terms_file = 'terms_random'
-    search_driver = 'Random.java'
-  elif distribution == 'Ziphian':
-    terms_file = 'terms_ordered'
-    search_driver = 'Ziphian.java'
-  else:
-    raise AssertionError('Unknown distribution: {0}'.format(distribution))
-  agents = ''
-  for client in clients:
-    client.RemoteCommand('cd {0}/search && '
-                         'cp distributions/{1} '
-                         'src/sample/searchdriver/SearchDriver.java'.format(
-                             faban.FABAN_HOME_DIR, search_driver))
-    target = None
-    if client != client_master:
-      target = 'deploy.jar'
-      agents += ' '
-    else:
-      target = 'deploy'
-    client.RemoteCommand('cd {0}/search && '
-                         'export JAVA_HOME={1} && '
-                         '{2}/bin/ant {3}'.format(
-                             faban.FABAN_HOME_DIR, faban.JAVA_HOME,
-                             ANT_HOME_DIR, target))
-    agents += client.ip_address + ':' + str(1)
-  client_master.RemoteCommand('cd {0}/search && '
-                              'sed -i "/<ipAddress1>/c\<ipAddress1>{1}'
-                              '</ipAddress1>" deploy/run.xml && '
-                              'sed -i "/<portNumber1>/c\<portNumber1>{3}'
-                              '</portNumber1>" deploy/run.xml && '
-                              'sed -i "/<outputDir>/c\<outputDir>{2}'
-                              '</outputDir>" deploy/run.xml && '
-                              'sed -i "/<termsFile>/c\<termsFile>{0}'
-                              '/search/src/sample/searchdriver/{4}'
-                              '</termsFile>" deploy/run.xml && '
-                              'sed -i "/<fa:scale>/c\<fa:scale>{7}'
-                              '</fa:scale>" deploy/run.xml && '
-                              'sed -i "/<agents>/c\<agents>{8}'
-                              '</agents>" deploy/run.xml && '
-                              'sed -i "/<fa:rampUp>/c\<fa:rampUp>{5}'
-                              '</fa:rampUp>" deploy/run.xml && '
-                              'sed -i "/<fa:rampDown>/c\<fa:rampDown>60'
-                              '</fa:rampDown>" deploy/run.xml && '
-                              'sed -i "/<fa:steadyState>/c\<fa:steadyState>{6}'
-                              '</fa:steadyState>" deploy/run.xml '.format(
-                                  faban.FABAN_HOME_DIR,
-                                  benchmark_spec.vms[0].ip_address,
-                                  FABAN_OUTPUT_DIR, SOLR_PORT, terms_file,
-                                  FLAGS.cs_websearch_ramp_up,
-                                  FLAGS.cs_websearch_steady_state,
-                                  FLAGS.cs_websearch_scale, agents))
-  agent_id = 1
-  client_heap_size = FLAGS.cs_websearch_client_heap_size
-  driver_dir = posixpath.join(faban.FABAN_HOME_DIR, 'search')
-  policy_path = posixpath.join(driver_dir, 'config/security/driver.policy')
-  for vm in clients:
-    if vm == client_master:
-      faban.StartRegistry(vm, CLASSPATH, policy_path)
-      faban.StartAgent(vm, CLASSPATH, driver_dir, 'SearchDriver', agent_id,
-                       client_heap_size, policy_path, client_master.ip_address)
-    else:
-      faban.StartAgent(vm, CLASSPATH, driver_dir, 'SearchDriver', agent_id,
-                       client_heap_size, policy_path, client_master.ip_address)
-    agent_id = agent_id + 1
-  benchmark_config = posixpath.join(faban.FABAN_HOME_DIR,
-                                    'search/deploy/run.xml')
-  faban.StartMaster(client_master, CLASSPATH, client_heap_size,
-                    policy_path, benchmark_config)
+  clients = benchmark_spec.vm_groups['clients'][0]
+  servers = benchmark_spec.vm_groups['servers'][0]
+  results = []
 
-  def ParseOutput(client):
-    stdout, _ = client.RemoteCommand('cd {0} && '
-                                     'cd `ls -Art | tail -n 1` && '
-                                     'cat summary.xml'.format(
-                                         FABAN_OUTPUT_DIR))
-    ops_per_sec = re.findall(r'\<metric unit="ops/sec"\>(\d+\.?\d*)', stdout)
-    sum_ops_per_sec = 0.0
-    for value in ops_per_sec:
-        sum_ops_per_sec += float(value)
-    p90 = re.findall(r'\<p90th\>(\d+\.?\d*)', stdout)
-    sum_p90 = 0.0
-    for value in p90:
-        sum_p90 += float(value)
-    p99 = re.findall(r'\<p99th\>(\d+\.?\d*)', stdout)
-    sum_p99 = 0.0
-    for value in p99:
-        sum_p99 += float(value)
-    return sum_ops_per_sec, sum_p90, sum_p99
+  benchmark_cmd = ('sudo docker run --rm --net host --name client '
+                   'cloudsuite/web-search:client %s %d %d %d %d ' %
+                   (servers.internal_ip,
+                    FLAGS.cs_websearch_scale,
+                    FLAGS.cs_websearch_ramp_up,
+                    FLAGS.cs_websearch_steady_state,
+                    FLAGS.cs_websearch_ramp_down))
+  stdout, _ = clients.RemoteCommand(benchmark_cmd, should_log=True)
+
+  ops_per_sec = re.findall(r'\<metric unit="ops/sec"\>(\d+\.?\d*)', stdout)
+  sum_ops_per_sec = 0.0
+  for value in ops_per_sec:
+     sum_ops_per_sec += float(value)
+  p90 = re.findall(r'\<p90th\>(\d+\.?\d*)', stdout)
+  sum_p90 = 0.0
+  for value in p90:
+     sum_p90 += float(value)
+  p99 = re.findall(r'\<p99th\>(\d+\.?\d*)', stdout)
+  sum_p99 = 0.0
+  for value in p99:
+     sum_p99 += float(value)
 
   results = []
-  sum_ops_per_sec, sum_p90, sum_p99 = ParseOutput(client_master)
   results.append(sample.Sample('Operations per second', sum_ops_per_sec,
                                'ops/s'))
   results.append(sample.Sample('90th percentile latency', sum_p90, 's'))
   results.append(sample.Sample('99th percentile latency', sum_p99, 's'))
-  faban.StopRegistry(client_master)
   return results
 
 
 def Cleanup(benchmark_spec):
-  """Cleanup CloudSuite Web Search on the target vm (by uninstalling).
+  """Stop and remove docker containers. Remove images.
 
   Args:
     benchmark_spec: The benchmark specification. Contains all data that is
         required to run the benchmark.
   """
-  servers = benchmark_spec.vm_groups['workers']
+  servers = benchmark_spec.vm_groups['servers'][0]
   clients = benchmark_spec.vm_groups['clients']
-  client_master = clients[0]
-  faban.Stop(client_master)
-  for vm in servers:
-    solr.Stop(vm, SOLR_PORT)
+
+  for vm in clients:
+    vm.RemoteCommand('sudo docker stop client')
+    vm.RemoteCommand('sudo docker rm client')
+
+  servers.RemoteCommand('sudo docker stop server')
+  servers.RemoteCommand('sudo docker rm server')
+
+  for vm in clients:
+    vm.RemoteCommand('sudo docker rmi cloudsuite/web-search:client')
+
+  servers.RemoteCommand('sudo docker rmi cloudsuite/web-search:server')
