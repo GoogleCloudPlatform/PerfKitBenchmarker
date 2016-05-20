2db39413b8de146e764ecdb14197b0fb1c053f6d
Mario Paulo Drumond <drumondmp@gmail.com>
2016-05-21 01:44:19 +0200

    Adding cloudsuite graph analytics benchmark (#986)
    * Adding cloudsuite graph analytics benchmark

--- perfkitbenchmarker/benchmark_sets.py
+++ perfkitbenchmarker/benchmark_sets.py
@@ -143,7 +143,8 @@ BENCHMARK_SETS = {
         MESSAGE: 'CloudSuite benchmark set.',
         BENCHMARK_LIST: ['cloudsuite_in_memory_analytics',
                          'cloudsuite_data_caching',
-                         'cloudsuite_media_streaming']
+                         'cloudsuite_media_streaming',
+                         'cloudsuite_graph_analytics']
     }
 }
 
