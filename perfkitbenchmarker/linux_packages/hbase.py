34545816458cb7e49ce2b80aefb4c23bbe5e4181
Eric Hankland <ehankland@google.com>
2016-05-25 16:22:40 -0700

    Fix cloud bigtable and hbase

--- perfkitbenchmarker/linux_packages/hbase.py
+++ perfkitbenchmarker/linux_packages/hbase.py
@@ -26,7 +26,7 @@ from perfkitbenchmarker import vm_util
 from perfkitbenchmarker.linux_packages import hadoop
 
 
-HBASE_VERSION = '1.1.4'
+HBASE_VERSION = '1.1.5'
 HBASE_URL = ('http://www.us.apache.org/dist/hbase/{0}/'
              'hbase-{0}-bin.tar.gz').format(HBASE_VERSION)
 
