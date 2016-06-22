3af758108d06ceef37b713aae408b7c439d285d3
yuyanting_google <yuyanting@google.com>
2016-06-22 08:43:09 -0700

    Fix hbase_ycsb config directory.

--- perfkitbenchmarker/linux_benchmarks/hbase_ycsb_benchmark.py
+++ perfkitbenchmarker/linux_benchmarks/hbase_ycsb_benchmark.py
@@ -200,7 +200,7 @@ def Prepare(benchmark_spec):
       posixpath.join(hbase.HBASE_CONF_DIR, HBASE_SITE))
 
   def PushHBaseSite(vm):
-    conf_dir = posixpath.join(ycsb.YCSB_DIR, 'hbase-binding', 'conf')
+    conf_dir = posixpath.join(ycsb.YCSB_DIR, 'hbase10-binding', 'conf')
     vm.RemoteCommand('mkdir -p {}'.format(conf_dir))
     vm.PushFile(
         os.path.join(vm_util.GetTempDir(), HBASE_SITE),
