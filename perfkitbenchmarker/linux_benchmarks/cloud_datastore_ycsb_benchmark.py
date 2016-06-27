41ed64b93b5f0b6cd170b2cb0a2a7dc314084d91
Kexi Long <kexi@google.com>
2016-06-27 21:11:34 +0000

    Addressed style problems and added PyDoc for module

--- perfkitbenchmarker/linux_benchmarks/cloud_datastore_ycsb_benchmark.py
+++ perfkitbenchmarker/linux_benchmarks/cloud_datastore_ycsb_benchmark.py
@@ -1,4 +1,32 @@
+# Copyright 2015 PerfKitBenchmarker Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Run YCSB benchmark against Google Cloud Datastore
+
+Before running this benchmark, you have to download your P12
+service account private key file to local machine, and pass the path
+via 'google_datastore_keyfile' parameters to PKB.
+
+Service Account email associated with the key file is also needed to
+pass to PKB.
+
+By default, this benchmark provision 1 single-CPU VM and spawn 1 thread
+to test Datastore.
+"""
+
 import posixpath
+import logging
 
 from perfkitbenchmarker import configs
 from perfkitbenchmarker import flags
@@ -7,7 +35,7 @@ from perfkitbenchmarker.linux_packages import ycsb
 
 
 BENCHMARK_NAME = 'cloud_datastore_ycsb'
-BENCHMARK_CONFIG = '''
+BENCHMARK_CONFIG = """
 cloud_datastore_ycsb:
   description: >
       Run YCSB agains Google Cloud Datastore.
@@ -15,7 +43,7 @@ cloud_datastore_ycsb:
   vm_groups:
     default:
       vm_spec: *default_single_core
-      vm_count: null'''
+      vm_count: 1"""
 
 YCSB_BINDING_TAR_URL = ('https://github.com/brianfrankcooper/YCSB/releases'
                         '/download/0.9.0/'
@@ -74,15 +102,13 @@ def Run(benchmark_spec):
     vms = benchmark_spec.vms
     executor = ycsb.YCSBExecutor('googledatastore')
     run_kwargs = {
-        'threads': 4,
         'googledatastore.datasetId': FLAGS.google_datastore_datasetId,
         'googledatastore.privateKeyFile': PRIVATE_KEYFILE_DIR,
         'googledatastore.serviceAccountEmail':
-        FLAGS.google_datastore_serviceAccount,
-            'googledatastore.debug': FLAGS.google_datastore_debug,
+            FLAGS.google_datastore_serviceAccount,
+        'googledatastore.debug': FLAGS.google_datastore_debug,
     }
     load_kwargs = run_kwargs.copy()
-    # TODO: support more flags, like thread, operationcount, recordcount, etc.
     if FLAGS['ycsb_preload_threads'].present:
         load_kwargs['threads'] = FLAGS['ycsb_preload_threads']
     samples = list(executor.LoadAndRun(vms,
@@ -92,7 +118,9 @@ def Run(benchmark_spec):
 
 
 def Cleanup(benchmark_spec):
-    # For now, we can only manually delete all the entries via GCP portal.
+    # TODO: support automatic cleanup.
+    logging.warning(
+        "For now, we can only manually delete all the entries via GCP portal.")
     return
 
 
