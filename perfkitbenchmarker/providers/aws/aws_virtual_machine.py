6fd4181b213be3bc54f5d465bd17f33058052dea
ehankland <ehankland@google.com>
2016-09-22 17:04:12 -0700

    Add support for AWS dedicated hosts (#1109)

--- perfkitbenchmarker/providers/aws/aws_virtual_machine.py
+++ perfkitbenchmarker/providers/aws/aws_virtual_machine.py
@@ -19,17 +19,21 @@ operate on the VM: boot, shutdown, etc.
 """
 
 import base64
+import collections
 import json
 import logging
+import uuid
 import threading
 
 from perfkitbenchmarker import disk
 from perfkitbenchmarker import errors
 from perfkitbenchmarker import flags
 from perfkitbenchmarker import linux_virtual_machine
+from perfkitbenchmarker import resource
 from perfkitbenchmarker import virtual_machine
 from perfkitbenchmarker import vm_util
 from perfkitbenchmarker import windows_virtual_machine
+from perfkitbenchmarker.configs import option_decoders
 from perfkitbenchmarker.providers.aws import aws_disk
 from perfkitbenchmarker.providers.aws import aws_network
 from perfkitbenchmarker.providers.aws import util
@@ -40,14 +44,6 @@ FLAGS = flags.FLAGS
 HVM = 'HVM'
 PV = 'PV'
 NON_HVM_PREFIXES = ['m1', 'c1', 't1', 'm2']
-US_EAST_1 = 'us-east-1'
-US_WEST_1 = 'us-west-1'
-US_WEST_2 = 'us-west-2'
-EU_WEST_1 = 'eu-west-1'
-AP_NORTHEAST_1 = 'ap-northeast-1'
-AP_SOUTHEAST_1 = 'ap-southeast-1'
-AP_SOUTHEAST_2 = 'ap-southeast-2'
-SA_EAST_1 = 'sa-east-1'
 PLACEMENT_GROUP_PREFIXES = frozenset(
     ['c3', 'c4', 'cc2', 'cg1', 'g2', 'cr1', 'r3', 'hi1', 'i2'])
 NUM_LOCAL_VOLUMES = {
@@ -69,6 +65,10 @@ INSTANCE_EXISTS_STATUSES = frozenset(
     ['pending', 'running', 'stopping', 'stopped'])
 INSTANCE_DELETED_STATUSES = frozenset(['shutting-down', 'terminated'])
 INSTANCE_KNOWN_STATUSES = INSTANCE_EXISTS_STATUSES | INSTANCE_DELETED_STATUSES
+HOST_EXISTS_STATES = frozenset(
+    ['available', 'under-assessment', 'permanent-failure'])
+HOST_RELEASED_STATES = frozenset(['released', 'released-permanent-failure'])
+KNOWN_HOST_STATES = HOST_EXISTS_STATES | HOST_RELEASED_STATES
 
 
 def GetBlockDeviceMap(machine_type):
@@ -97,6 +97,107 @@ def IsPlacementGroupCompatible(machine_type):
   return prefix in PLACEMENT_GROUP_PREFIXES
 
 
+class AwsDedicatedHost(resource.BaseResource):
+  """Object representing an AWS host.
+
+  Attributes:
+    region: The AWS region of the host.
+    zone: The AWS availability zone of the host.
+    machine_type: The machine type of VMs that may be created on the host.
+    client_token: A uuid that makes the creation request idempotent.
+    id: The host_id of the host.
+  """
+
+  def __init__(self, machine_type, zone):
+    super(AwsDedicatedHost, self).__init__()
+    self.machine_type = machine_type
+    self.zone = zone
+    self.region = util.GetRegionFromZone(self.zone)
+    self.client_token = str(uuid.uuid4())
+    self.id = None
+
+  def _Create(self):
+    create_cmd = util.AWS_PREFIX + [
+        'ec2',
+        'allocate-hosts',
+        '--region=%s' % self.region,
+        '--client-token=%s' % self.client_token,
+        '--instance-type=%s' % self.machine_type,
+        '--availability-zone=%s' % self.zone,
+        '--auto-placement=off',
+        '--quantity=1']
+    vm_util.IssueCommand(create_cmd)
+
+  def _Delete(self):
+    if self.id:
+      delete_cmd = util.AWS_PREFIX + [
+          'ec2',
+          'release-hosts',
+          '--region=%s' % self.region,
+          '--host-ids=%s' % self.id]
+      vm_util.IssueCommand(delete_cmd)
+
+  @vm_util.Retry()
+  def _Exists(self):
+    describe_cmd = util.AWS_PREFIX + [
+        'ec2',
+        'describe-hosts',
+        '--region=%s' % self.region,
+        '--filter=Name=client-token,Values=%s' % self.client_token]
+    stdout, _, _ = vm_util.IssueCommand(describe_cmd)
+    response = json.loads(stdout)
+    hosts = response['Hosts']
+    assert len(hosts) < 2, 'Too many hosts.'
+    if not hosts:
+      return False
+    host = hosts[0]
+    self.id = host['HostId']
+    state = host['State']
+    assert state in KNOWN_HOST_STATES, state
+    return state in HOST_EXISTS_STATES
+
+
+class AwsVmSpec(virtual_machine.BaseVmSpec):
+  """Object containing the information needed to create an AwsVirtualMachine.
+
+  Attributes:
+      use_dedicated_host: bool. Whether to create this VM on a dedicated host.
+  """
+
+  CLOUD = providers.AWS
+
+  @classmethod
+  def _ApplyFlags(cls, config_values, flag_values):
+    """Modifies config options based on runtime flag values.
+
+    Can be overridden by derived classes to add support for specific flags.
+
+    Args:
+      config_values: dict mapping config option names to provided values. May
+          be modified by this function.
+      flag_values: flags.FlagValues. Runtime flags that may override the
+          provided config values.
+    """
+    super(AwsVmSpec, cls)._ApplyFlags(config_values, flag_values)
+    if flag_values['aws_dedicated_hosts'].present:
+      config_values['use_dedicated_host'] = flag_values.aws_dedicated_hosts
+
+  @classmethod
+  def _GetOptionDecoderConstructions(cls):
+    """Gets decoder classes and constructor args for each configurable option.
+
+    Returns:
+      dict. Maps option name string to a (ConfigOptionDecoder class, dict) pair.
+          The pair specifies a decoder class and its __init__() keyword
+          arguments to construct in order to decode the named option.
+    """
+    result = super(AwsVmSpec, cls)._GetOptionDecoderConstructions()
+    result.update({
+        'use_dedicated_host': (option_decoders.BooleanDecoder,
+                               {'default': False})})
+    return result
+
+
 class AwsVirtualMachine(virtual_machine.BaseVirtualMachine):
   """Object representing an AWS Virtual Machine."""
 
@@ -107,6 +208,8 @@ class AwsVirtualMachine(virtual_machine.BaseVirtualMachine):
   _lock = threading.Lock()
   imported_keyfile_set = set()
   deleted_keyfile_set = set()
+  deleted_hosts = set()
+  host_map = collections.defaultdict(list)
 
   def __init__(self, vm_spec):
     """Initialize a AWS virtual machine.
@@ -122,6 +225,19 @@ class AwsVirtualMachine(virtual_machine.BaseVirtualMachine):
     self.user_data = None
     self.network = aws_network.AwsNetwork.GetNetwork(self)
     self.firewall = aws_network.AwsFirewall.GetFirewall()
+    self.use_dedicated_host = vm_spec.use_dedicated_host
+    self.host = None
+    self.id = None
+
+    if self.use_dedicated_host and util.IsRegion(self.zone):
+      raise ValueError(
+          'In order to use dedicated hosts, you must specify an availability '
+          'zone, not a region ("zone" was %s).' % self.zone)
+
+  @property
+  def host_list(self):
+    """Returns the list of hosts that are compatible with this VM."""
+    return self.host_map[(self.machine_type, self.zone)]
 
   @property
   def group_id(self):
@@ -223,16 +339,34 @@ class AwsVirtualMachine(virtual_machine.BaseVirtualMachine):
                                                      self.region)
     self.AllowRemoteAccessPorts()
 
+    if self.use_dedicated_host:
+      with self._lock:
+        if not self.host_list:
+          host = AwsDedicatedHost(self.machine_type, self.zone)
+          self.host_list.append(host)
+          host.Create()
+        self.host = self.host_list[-1]
+
   def _DeleteDependencies(self):
     """Delete VM dependencies."""
     self.DeleteKeyfile()
+    if self.host:
+      with self._lock:
+        if self.host in self.host_list:
+          self.host_list.remove(self.host)
+        if self.host not in self.deleted_hosts:
+          self.host.Delete()
+          self.deleted_hosts.add(self.host)
 
   def _Create(self):
     """Create a VM instance."""
     placement = []
     if not util.IsRegion(self.zone):
       placement.append('AvailabilityZone=%s' % self.zone)
-    if IsPlacementGroupCompatible(self.machine_type):
+    if self.use_dedicated_host:
+      placement.append('Tenancy=host,HostId=%s' % self.host.id)
+      num_hosts = len(self.host_list)
+    elif IsPlacementGroupCompatible(self.machine_type):
       placement.append('GroupName=%s' % self.network.placement_group.name)
     placement = ','.join(placement)
     block_device_map = GetBlockDeviceMap(self.machine_type)
@@ -252,18 +386,32 @@ class AwsVirtualMachine(virtual_machine.BaseVirtualMachine):
       create_cmd.append('--placement=%s' % placement)
     if self.user_data:
       create_cmd.append('--user-data=%s' % self.user_data)
-    stdout, _, _ = vm_util.IssueCommand(create_cmd)
+    stdout, stderr, _ = vm_util.IssueCommand(create_cmd)
+
+    if self.use_dedicated_host and 'InsufficientCapacityOnHost' in stderr:
+      logging.warning(
+          'Creation failed due to insufficient host capacity. A new host will '
+          'be created and instance creation will be retried.')
+      with self._lock:
+        if num_hosts == len(self.host_list):
+          host = AwsDedicatedHost(self.machine_type, self.zone)
+          self.host_list.append(host)
+          host.Create()
+        self.host = self.host_list[-1]
+      raise errors.Resource.RetryableCreationError()
+
     response = json.loads(stdout)
     self.id = response['Instances'][0]['InstanceId']
 
   def _Delete(self):
     """Delete a VM instance."""
-    delete_cmd = util.AWS_PREFIX + [
-        'ec2',
-        'terminate-instances',
-        '--region=%s' % self.region,
-        '--instance-ids=%s' % self.id]
-    vm_util.IssueCommand(delete_cmd)
+    if self.id:
+      delete_cmd = util.AWS_PREFIX + [
+          'ec2',
+          'terminate-instances',
+          '--region=%s' % self.region,
+          '--instance-ids=%s' % self.id]
+      vm_util.IssueCommand(delete_cmd)
 
   def _Exists(self):
     """Returns true if the VM exists."""
@@ -325,6 +473,16 @@ class AwsVirtualMachine(virtual_machine.BaseVirtualMachine):
   def AddMetadata(self, **kwargs):
     """Adds metadata to the VM."""
     util.AddTags(self.id, self.region, **kwargs)
+
+  def GetMachineTypeDict(self):
+    """Returns a dict containing properties that specify the machine type.
+
+    Returns:
+      dict mapping string property key to value.
+    """
+    result = super(AwsVirtualMachine, self).GetMachineTypeDict()
+    result['dedicated_host'] = self.use_dedicated_host
+    return result
 
 
 class DebianBasedAwsVirtualMachine(AwsVirtualMachine,
