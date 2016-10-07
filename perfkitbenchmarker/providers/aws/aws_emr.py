94118625b7ba65f23f48fecf1d219fa45f442985
Kris Hildrum <hildrum@google.com>
2016-10-06 23:15:10 -0700

    Fix bugs

--- perfkitbenchmarker/providers/aws/aws_emr.py
+++ perfkitbenchmarker/providers/aws/aws_emr.py
@@ -71,7 +71,7 @@ class AwsSecurityGroup(resource.BaseResource):
     vm_util.IssueCommand(cmd)
 
   def _Exists(self):
-    cmd = self.cmd_prefix + ['ec2', 'describe-security-group',
+    cmd = self.cmd_prefix + ['ec2', 'describe-security-groups',
                              '--group-id=' + self.group_id]
     _, _, retcode = vm_util.IssueCommand(cmd)
     # if the security group doesn't exist, the describe command gives an error.
