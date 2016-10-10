74887a33ce3c13a3963fb07d8b7bc264ffe2dbcc
Theodore DeRego <tedsta@google.com>
2016-10-10 11:42:36 -0700

    Changed naming of port variables in Firewall.AllowPort

--- perfkitbenchmarker/providers/aws/aws_network.py
+++ perfkitbenchmarker/providers/aws/aws_network.py
@@ -51,18 +51,20 @@ class AwsFirewall(network.BaseFirewall):
     self.firewall_set = set()
     self._lock = threading.Lock()
 
-  def AllowPort(self, vm, port, to_port=None):
+  def AllowPort(self, vm, start_port, end_port=None):
     """Opens a port on the firewall.
 
     Args:
       vm: The BaseVirtualMachine object to open the port for.
-      port: The local port to open.
+      start_port: The first local port to open in a range.
+      end_port: The last local port to open in a range. If None, only start_port
+        will be opened.
     """
     if vm.is_static:
       return
-    if to_port is None:
-      to_port = port
-    entry = (port, to_port, vm.group_id)
+    if end_port is None:
+      end_port = start_port
+    entry = (start_port, end_port, vm.group_id)
     if entry in self.firewall_set:
       return
     with self._lock:
@@ -73,7 +75,7 @@ class AwsFirewall(network.BaseFirewall):
           'authorize-security-group-ingress',
           '--region=%s' % vm.region,
           '--group-id=%s' % vm.group_id,
-          '--port=%s-%s' % (port, to_port),
+          '--port=%s-%s' % (start_port, end_port),
           '--cidr=0.0.0.0/0']
       util.IssueRetryableCommand(
           authorize_cmd + ['--protocol=tcp'])
