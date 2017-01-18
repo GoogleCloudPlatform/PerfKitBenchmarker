d0b19ffdf5e7781c043bb5ece0c8041c7bd84271
Besan A. Radwan <besan.aburadwan@rackspace.com>
2017-01-18 17:56:31 -0600

    This code aims to add the capability for PKB to publish directly to InfluxDB
    The publishing of data from PKB benchmarks can done either by specifiying a pre-existing Influx DB publisher or with none specified it will create a new one or re-use a pre-existing DB called perfkit. By default if no uri is specified it will publish the data to localhost.

--- perfkitbenchmarker/publisher.py
+++ perfkitbenchmarker/publisher.py
@@ -17,6 +17,7 @@
 import abc
 import copy
 import csv
+import httplib
 import io
 import itertools
 import json
@@ -26,6 +27,7 @@ import operator
 import pprint
 import sys
 import time
+import urllib
 import uuid
 
 from perfkitbenchmarker import disk
@@ -110,6 +112,14 @@ flags.DEFINE_multistring(
     'A colon separated key-value pair that will be added to the labels field '
     'of all samples as metadata. Multiple key-value pairs may be specified '
     'by separating each pair by commas.')
+
+flags.DEFINE_string(
+    'influx_uri', 'http://localhost:8086/',
+    'The Influx DB endpoint for queries address and port')
+
+flags.DEFINE_string(
+    'influx_db_name', 'perfkit',
+    'Name of Influx DB database that you wish to publish to or create')
 
 DEFAULT_JSON_OUTPUT_NAME = 'perfkitbenchmarker_results.json'
 DEFAULT_CREDENTIALS_JSON = 'credentials.json'
@@ -643,6 +653,81 @@ class ElasticsearchPublisher(SamplePublisher):
       if new_key != key:
         res[new_key] = res.pop(key)
     return res
+
+
+class InfluxDBPublisher(SamplePublisher):
+
+  def __init__(self, influx_uri=None, influx_db_name=None):
+    # set to default above in flags unless changed
+    self.influx_uri = influx_uri
+    self.influx_db_name = influx_db_name
+
+  def PublishSamples(self, samples):
+    successful_http_request_codes = [200, 202, 204]
+    sample = list(samples)
+    number_of_samples = 0
+    for sample in samples:
+      number_of_samples += 1
+      timestamp = int((10 ** 9) * sample['timestamp'])
+      measurement = 'perfkitbenchmarker'
+      tag_set_metadata = \
+          ', '.join(self.FormatSampleForInfluxDB(sample['metadata']))
+      tag_set = ','.join((tag_set_metadata, self.
+                          FormatSampleForInfluxDB({k: sample[k]
+                                                   for k in ('test',
+                                                             'official',
+                                                             'owner',
+                                                             'run_uri',
+                                                             'sample_uri')})))
+      field_set = '%s_%s=%s' % (self.FormatSampleForInfluxDB({k: sample[k]
+                                for k in ('metric', 'unit', 'value')}))
+      samples_constructed_body = (' '.join((','.join((measurement,
+                                           tag_set))),
+                                           (' '.join((field_set,
+                                                     timestamp))))) + "\n"
+
+    create_db_status, create_db_response = self. \
+        _createInfluxDB(self.influx_uri, self.influx_db_name)
+    if create_db_status in successful_http_request_codes:
+      logging.debug('Success!', self.influx_db_name, ' DB Created')
+    else:
+      logging.debug(create_db_status,
+                    'Request could not be completed due to: ',
+                    create_db_response)
+
+    logging.debug('writing samples to publisher: writing',
+                  number_of_samples, ' samples.')
+    self._writeData(self.influx_uri, self.influx_db_name,
+                    samples_constructed_body)
+    logging.debug(create_db_status,
+                  'Request could not be completed due to: ',
+                  create_db_response)
+
+  def FormatSampleForInfluxDB(self, sample):
+    for k, v in sample.iteritems():
+            return '%s=%s' % (k, v)
+
+  def _CreateDB(self, influx_uri, influx_db_name):
+    headers = {'Content-type': 'application/x-www-form-urlencoded',
+               'Accept': 'text/plain'}
+    body = urllib.urlencode({'q': 'CREATE DATABASE' + influx_db_name})
+    conn = httplib.HTTPConnection(influx_uri)
+    conn.request('POST', '/query?' + body, headers)
+    response = conn.getresponse()
+    response_status = response.status
+    response_response = response.response
+    return response_status, response_response
+
+  def _WriteData(self, influx_uri, influx_db_name, data):
+    body = data
+    headers = {"Content-type": "application/octet-stream"}
+    conn = httplib.HTTPConnection(influx_uri)
+    conn.request('POST', '/write?' + 'db=' + influx_db_name, body, headers)
+    response = conn.getresponse()
+    response_status = response.status
+    response_response = response.response
+    return response_status, response_response
+
 
 
 class SampleCollector(object):
