8ffbac37dc54c680eb94e69e1d078d8182c85b40
yuyanting_google <yuyanting@google.com>
2017-03-08 13:09:54 -0800

    Address comments

--- perfkitbenchmarker/providers/aws/aws_virtual_machine.py
+++ perfkitbenchmarker/providers/aws/aws_virtual_machine.py
@@ -296,7 +296,7 @@ class AwsVirtualMachine(virtual_machine.BaseVirtualMachine):
       raise ValueError(
           'In order to use dedicated hosts, you must specify an availability '
           'zone, not a region ("zone" was %s).' % self.zone)
-    if self.machine_type[:2].lower() == 'i3' and not FLAGS['image'].present:
+    if self.machine_type[:2].lower() == 'i3' and not self.image:
       # TODO(user): Remove this check when pkb defaults to ubuntu-1604.
       raise ValueError(
           'In order to use i3 instances, you must specify --image.')
