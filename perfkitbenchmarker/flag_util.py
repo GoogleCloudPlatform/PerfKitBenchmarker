dc3dffa97d8c4ef26350371a501297c3793daf60
Yilei (Dolee) Yang <yileiyang9@gmail.com>
2017-04-11 14:49:07 -0400

    Use snake_case gflags APIs. (#1328)

--- perfkitbenchmarker/flag_util.py
+++ perfkitbenchmarker/flag_util.py
@@ -99,7 +99,7 @@ class IntegerList(object):
           yield val
 
   def __str__(self):
-      return IntegerListSerializer().Serialize(self)
+      return IntegerListSerializer().serialize(self)
 
   def _CreateXrangeFromTuple(self, input_tuple):
     start = input_tuple[0]
@@ -137,7 +137,7 @@ class IntegerListParser(flags.ArgumentParser):
 
     self.on_nonincreasing = on_nonincreasing
 
-  def Parse(self, inp):
+  def parse(self, inp):
     """Parse an integer list.
 
     Args:
@@ -189,17 +189,18 @@ class IntegerListParser(flags.ArgumentParser):
 
     return IntegerList(result)
 
-  def Type(self):
+  def flag_type(self):
     return 'integer list'
 
 
 class IntegerListSerializer(flags.ArgumentSerializer):
+
   def _SerializeRange(self, val):
     if len(val) == 2:
       return '%s-%s' % (val[0], val[1])
     return '%s-%s-%s' % (val[0], val[1], val[2])
 
-  def Serialize(self, il):
+  def serialize(self, il):
     return ','.join([str(val) if isinstance(val, int) or isinstance(val, long)
                      else self._SerializeRange(val)
                      for val in il.groups])
@@ -263,7 +264,7 @@ class UnitsParser(flags.ArgumentParser):
       convertible_to: Either an individual unit specification or a series of
           unit specifications, where each unit specification is either a string
           (e.g. 'byte') or a units.Unit. The parser input must be convertible to
-          at least one of the specified Units, or the Parse() method will raise
+          at least one of the specified Units, or the parse() method will raise
           a ValueError.
     """
     if isinstance(convertible_to, (basestring, units.Unit)):
@@ -271,7 +272,7 @@ class UnitsParser(flags.ArgumentParser):
     else:
       self.convertible_to = [units.Unit(u) for u in convertible_to]
 
-  def Parse(self, inp):
+  def parse(self, inp):
     """Parse the input.
 
     Args:
@@ -311,7 +312,7 @@ class UnitsParser(flags.ArgumentParser):
 
 
 class UnitsSerializer(flags.ArgumentSerializer):
-  def Serialize(self, units):
+  def serialize(self, units):
     return str(units)
 
 
@@ -409,7 +410,7 @@ class YAMLParser(flags.ArgumentParser):
 
   syntactic_help = 'A YAML expression.'
 
-  def Parse(self, inp):
+  def parse(self, inp):
     """Parse the input.
 
     Args:
@@ -437,7 +438,7 @@ class YAMLParser(flags.ArgumentParser):
 
 class YAMLSerializer(flags.ArgumentSerializer):
 
-  def Serialize(self, val):
+  def serialize(self, val):
     return yaml.dump(val)
 
 
