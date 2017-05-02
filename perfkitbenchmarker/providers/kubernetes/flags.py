e5575057966cc455a3f8a3689e9608b1b539597c
ehankland <ehankland@google.com>
2017-05-02 15:22:22 -0700

    Fix up kubernetes provider (#1351)

--- perfkitbenchmarker/providers/kubernetes/flags.py
+++ perfkitbenchmarker/providers/kubernetes/flags.py
@@ -32,7 +32,7 @@ flags.DEFINE_list('ceph_monitors', [],
                   'Must be provided when Ceph scratch disk is required. '
                   'Example: "127.0.0.1:6789,192.168.1.1:6789"')
 
-flags.DEFINE_string('kubeconfig', '',
+flags.DEFINE_string('kubeconfig', None,
                     'Path to kubeconfig to be used by kubectl')
 
 flags.DEFINE_string('kubectl', 'kubectl',
@@ -46,8 +46,6 @@ flags.DEFINE_boolean('docker_in_privileged_mode', True,
                      'If set to True, will attempt to create Docker containers '
                      'in a privileged mode. Note that some benchmarks execute '
                      'commands which are only allowed in privileged mode.')
-
-flags.DEFINE_list('kubernetes_nodes', [],
-                  'IP addresses of Kubernetes Nodes. These need to be '
-                  'accessible from the machine running Perfkit '
-                  'benchmarker. Example: "10.20.30.40,10.20.30.41"')
+flags.DEFINE_boolean('kubernetes_anti_affinity', True,
+                     'If set to True, PKB pods will not be scheduled on the '
+                     'same nodes as other PKB pods.')
--- perfkitbenchmarker/providers/kubernetes/kubernetes_disk.py
+++ perfkitbenchmarker/providers/kubernetes/kubernetes_disk.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import abc
+import copy
 import logging
 import re
 
@@ -19,10 +21,14 @@ from perfkitbenchmarker import disk
 from perfkitbenchmarker import flags
 from perfkitbenchmarker import vm_util
 from perfkitbenchmarker import errors
+from perfkitbenchmarker import providers
 from perfkitbenchmarker.vm_util import OUTPUT_STDOUT as STDOUT,\
     OUTPUT_STDERR as STDERR, OUTPUT_EXIT_CODE as EXIT_CODE
+from perfkitbenchmarker.configs import option_decoders
+from perfkitbenchmarker.providers.gcp import gce_disk
 
 FLAGS = flags.FLAGS
+_K8S_VOLUME_REGISTRY = {}
 
 
 def CreateDisks(disk_specs, vm_name):
@@ -32,13 +38,37 @@ def CreateDisks(disk_specs, vm_name):
   """
   scratch_disks = []
   for disk_num, disk_spec in enumerate(disk_specs):
-    if disk_spec.disk_type == disk.LOCAL:
-      scratch_disk = LocalDisk(disk_num, disk_spec, vm_name)
-    else:
-      scratch_disk = CephDisk(disk_num, disk_spec, vm_name)
-    scratch_disk._Create()
+    disk_class = GetKubernetesDiskClass(disk_spec.disk_type)
+    scratch_disk = disk_class(disk_num, disk_spec, vm_name)
+    scratch_disk.Create()
     scratch_disks.append(scratch_disk)
   return scratch_disks
+
+
+class KubernetesDiskSpec(disk.BaseDiskSpec):
+
+  CLOUD = providers.KUBERNETES
+
+  @classmethod
+  def _GetOptionDecoderConstructions(cls):
+    result = super(KubernetesDiskSpec, cls)._GetOptionDecoderConstructions()
+    result.update({
+        'backing_store_disk_type': (option_decoders.StringDecoder,
+                                    {'default': None, 'none_ok': True}),
+    })
+    return result
+
+
+def GetKubernetesDiskClass(volume_type):
+  return _K8S_VOLUME_REGISTRY[volume_type]
+
+
+class AutoRegisterKubernetesDiskMeta(abc.ABCMeta):
+  """Metaclass that registers Kubernetes disks by volume type."""
+
+  def __init__(cls, name, bases, dct):
+    super(AutoRegisterKubernetesDiskMeta, cls).__init__(name, bases, dct)
+    _K8S_VOLUME_REGISTRY[cls.K8S_VOLUME_TYPE] = cls
 
 
 class KubernetesDisk(disk.BaseDisk):
@@ -46,9 +76,12 @@ class KubernetesDisk(disk.BaseDisk):
   Base class for Kubernetes Disks.
   """
 
-  def __init__(self, disk_spec):
+  __metaclass__ = AutoRegisterKubernetesDiskMeta
+  K8S_VOLUME_TYPE = None
+
+  def __init__(self, disk_num, disk_spec, name):
     super(KubernetesDisk, self).__init__(disk_spec)
-    self.mount_point = disk_spec.mount_point
+    self.name = '%s-%s' % (name, disk_num)
 
   def _Create(self):
     return
@@ -73,14 +106,12 @@ class KubernetesDisk(disk.BaseDisk):
     volume_mounts.append(volume_mount)
 
 
-class LocalDisk(KubernetesDisk):
+class EmptyDirDisk(KubernetesDisk):
   """
   Implementation of Kubernetes 'emptyDir' type of volume.
   """
 
-  def __init__(self, disk_num, disk_spec, name):
-    super(LocalDisk, self).__init__(disk_spec)
-    self.name = 'local-disk-%s-%s' % (name, disk_num)
+  K8S_VOLUME_TYPE = 'emptyDir'
 
   def GetDevicePath(self):
     """
@@ -103,9 +134,10 @@ class CephDisk(KubernetesDisk):
   Implementation of Kubernetes 'rbd' type of volume.
   """
 
+  K8S_VOLUME_TYPE = 'rbd'
+
   def __init__(self, disk_num, disk_spec, name):
-    super(CephDisk, self).__init__(disk_spec)
-    self.name = 'rbd-%s-%s' % (name, disk_num)
+    super(CephDisk, self).__init__(disk_num, disk_spec, name)
     self.ceph_secret = FLAGS.ceph_secret
 
   def _Create(self):
@@ -183,3 +215,44 @@ class CephDisk(KubernetesDisk):
 
   def GetDevicePath(self):
     return self.device_path
+
+
+class GcePersistentDisk(KubernetesDisk):
+  """GCE disk that can be used with K8s containers."""
+
+  K8S_VOLUME_TYPE = 'gcePersistentDisk'
+
+  def __init__(self, disk_num, spec, name):
+    super(GcePersistentDisk, self).__init__(disk_num, spec, name)
+    spec = copy.deepcopy(spec)
+    spec.disk_type = spec.backing_store_disk_type or gce_disk.PD_STANDARD
+    stdout, _, _ = vm_util.IssueCommand([
+        FLAGS.kubectl, '--kubeconfig=%s' % FLAGS.kubeconfig, 'get',
+        'nodes', '-o=jsonpath={.items[].spec.providerID}'])
+    try:
+      m = re.match('gce://(?P<project>[^/]*)/(?P<zone>[^/]*)/.*', stdout)
+      self.project = m.group('project')
+      self.zone = m.group('zone')
+    except:
+      logging.exception(
+          'Node ProviderID (%s) does not match expected GCE format.', stdout)
+      raise
+
+    self.pd = gce_disk.GceDisk(
+        spec, self.name, self.zone, self.project)
+
+  def _Create(self):
+    self.pd._Create()
+
+  def _Exists(self):
+    return self.pd._Exists()
+
+  def _Delete(self):
+    self.pd._Delete()
+
+  def AttachVolumeInfo(self, volumes):
+    gce_volume = {
+        "name": self.name,
+        "pdName": self.pd.name
+    }
+    volumes.append(gce_volume)
