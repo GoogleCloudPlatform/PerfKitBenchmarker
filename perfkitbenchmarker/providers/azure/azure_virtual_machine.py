be257f2e2171ea4ef3aef87f1600069797084163
ehankland <ehankland@google.com>
2017-06-06 11:05:01 -0700

    Update azure_virtual_machine.py

--- perfkitbenchmarker/providers/azure/azure_virtual_machine.py
+++ perfkitbenchmarker/providers/azure/azure_virtual_machine.py
@@ -179,7 +179,6 @@ class AzureVirtualMachine(virtual_machine.BaseVirtualMachine):
     self.image = vm_spec.image or self.IMAGE_URN
 
     disk_spec = disk.BaseDiskSpec('azure_os_disk')
-    disk_spec.disk_type = self.storage_account.storage_type
     self.os_disk = azure_disk.AzureDisk(disk_spec,
                                         self.name,
                                         self.machine_type,
@@ -200,7 +199,7 @@ class AzureVirtualMachine(virtual_machine.BaseVirtualMachine):
          '--size', self.machine_type,
          '--admin-username', self.user_name,
          '--availability-set', self.network.avail_set.name,
-         '--storage-sku', self.os_disk.disk_type,
+         '--storage-sku', self.storage_account.storage_type,
          '--name', self.name] +
         self.resource_group.args +
         self.nic.args)
