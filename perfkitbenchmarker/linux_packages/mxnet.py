78a3b3cbcd08978e988c63973ae4359fdfaa56ee
tohaowu <tohaowu@google.com>
2018-02-05 17:07:02 -0800

    Added MXNet support for CUDA Toolkit 9.0
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=184610088

--- perfkitbenchmarker/linux_packages/mxnet.py
+++ perfkitbenchmarker/linux_packages/mxnet.py
@@ -60,7 +60,10 @@ def Install(vm):
   vm.InstallPackages('libatlas-base-dev')
   if FLAGS.mx_device == 'gpu':
     vm.Install('cuda_toolkit')
-    vm.RemoteCommand('sudo pip install mxnet-cu80', should_log=True)
+    if FLAGS.cuda_toolkit_version == '8.0':
+      vm.RemoteCommand('sudo pip install mxnet-cu80', should_log=True)
+    elif FLAGS.cuda_toolkit_version == '9.0':
+      vm.RemoteCommand('sudo pip install mxnet-cu90', should_log=True)
   elif FLAGS.mx_device == 'cpu':
     vm.RemoteCommand('sudo pip install mxnet', should_log=True)
 
