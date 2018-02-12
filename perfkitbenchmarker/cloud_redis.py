f15cc343183cd1dfe85054ab55b5014b1c97fc7d
deitz <deitz@google.com>
2018-02-12 14:22:56 -0800

    ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=185435006

---
+++ perfkitbenchmarker/cloud_redis.py
@@ -0,0 +1,76 @@
+# Copyright 2018 PerfKitBenchmarker Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module containing class for cloud Redis."""
+
+import abc
+
+from perfkitbenchmarker import flags
+from perfkitbenchmarker import resource
+
+
+_CLOUD_REDIS_REGISTRY = {}
+FLAGS = flags.FLAGS
+
+# List of supported redis tiers
+STANDARD = 'STANDARD'
+BASIC = 'BASIC'
+
+# List of redis versions
+REDIS_3_2 = 'REDIS_3_2'
+REDIS_VERSIONS = [REDIS_3_2]
+
+
+def GetCloudRedisClass(cloud):
+  """Gets the cloud Redis class corresponding to 'cloud'.
+
+  Args:
+    cloud: String. name of cloud to get the class for.
+
+  Returns:
+    Implementation class corresponding to the argument cloud
+
+  Raises:
+    Exception: An invalid cloud was provided
+  """
+  if cloud not in _CLOUD_REDIS_REGISTRY:
+    raise Exception('No cloud redis found for {0}'.format(cloud))
+  return _CLOUD_REDIS_REGISTRY.get(cloud)
+
+
+class AutoRegisterCloudRedisMeta(abc.ABCMeta):
+  """Metaclass which allows Cloud Redis to register."""
+
+  def __init__(cls, name, bases, dct):
+    if hasattr(cls, 'CLOUD'):
+      if cls.CLOUD is None:
+        raise Exception('cloud Redis concrete subclasses must have a cloud '
+                        'attribute.')
+      else:
+        _CLOUD_REDIS_REGISTRY[cls.CLOUD] = cls
+    super(AutoRegisterCloudRedisMeta, cls).__init__(name, bases, dct)
+
+
+class BaseCloudRedis(resource.BaseResource):
+  """Object representing a cloud redis."""
+
+  __metaclass__ = AutoRegisterCloudRedisMeta
+
+  def __init__(self, cloud_redis_spec):
+    """Initialize the cloud redis object.
+
+    Args:
+      cloud_redis_spec: spec of the cloud redis.
+    """
+    super(BaseCloudRedis, self).__init__()
+    self.spec = cloud_redis_spec
