dc020f6b44d82e2f06dd858906d2f61b42ca566b
Steven Deitz <deitz@google.com>
2018-02-13 16:45:12 -0800

    Fixes for flake8.

--- perfkitbenchmarker/providers/azure/azure_managed_relational_db.py
+++ perfkitbenchmarker/providers/azure/azure_managed_relational_db.py
@@ -331,4 +331,3 @@ class AzureManagedRelationalDb(managed_relational_db.BaseManagedRelationalDb):
         self.spec.database_username,
         self.endpoint,
         self.spec.database_password)
-
