98ea364f4826a13744d683fe8cb615bf57cd8be8
saksena <saksena@google.com>
2018-05-02 13:01:47 -0700

    Adding runner utilities to execute multiple sql scripts on edw clusters.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=195137789

---
+++ perfkitbenchmarker/data/edw/script_driver.py
@@ -0,0 +1,60 @@
+"""Driver for running a script against a EDW cluster.
+
+Driver compiles the provider specific script execution command and returns the
+time taken to execute the script in seconds or -1 if the script fails.
+"""
+
+__author__ = 'p3rf@google.com'
+
+import logging
+from subprocess import call
+import time
+from absl import app
+from absl import flags
+import provider_specific_script_driver
+
+flags.DEFINE_string('script', None, 'SQL script which contains the query.')
+flags.DEFINE_string('logfile_suffix', 'log', 'Suffix to use for the output and '
+                                             'error files.')
+
+FLAGS = flags.FLAGS
+DRIVER_NAME = './script_runner.sh'
+
+
+def default_logfile_names(script, suffix):
+  """Method to return the names for output and error log files."""
+  suffix = script.split('.')[0] if suffix is None else suffix
+  output_logfile = '{}_out.txt'.format(suffix)
+  error_logfile = '{}_err.txt'.format(suffix)
+  return output_logfile, error_logfile
+
+
+def one_script(script, logfile_suffix):
+  """Method to execute a sql script on a EDW cluster.
+
+  Arguments:
+    script: SQL script which contains the query.
+    logfile_suffix: Suffix to use for the output and error files.
+
+  Returns:
+    Dictionary containing the name of the script and its execution time (-1 if
+    the script fails)
+  """
+  output, error = default_logfile_names(script, logfile_suffix)
+  cmd = provider_specific_script_driver.generate_provider_specific_cmd_list(
+      script, DRIVER_NAME, output, error)
+  start_time = time.time()
+  response_status = call(cmd)
+  execution_time = -1 if (response_status != 0) else round((time.time() -
+                                                            start_time), 2)
+  return {script: execution_time}
+
+
+def main(argv):
+  del argv
+  print one_script(FLAGS.script, FLAGS.logfile_suffix)
+
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO)
+  app.run(main)
