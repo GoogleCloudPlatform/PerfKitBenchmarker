c3e1aa95ceea0e67973be9709427bda0370997b6
Flint Dominic <31749352+flint-dominic@users.noreply.github.com>
2018-05-17 16:40:43 -0700

    Add files via upload
    added fixes

--- perfkitbenchmarker/providers/aws/aws_dynamodb.py
+++ perfkitbenchmarker/providers/aws/aws_dynamodb.py
@@ -14,7 +14,6 @@
 """Module containing class for AWS' dynamodb tables.
 
 Tables can be created and deleted.
-TODO: derive region from endpoint.
 """
 
 import json
@@ -25,24 +24,27 @@ from perfkitbenchmarker import flags
 from perfkitbenchmarker import vm_util
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string('aws_dynamodb_region',
-                    'us-west-1',
-                    'The config for the DynamoDB instance.')
+flags.DEFINE_string('aws_dynamodb_attributetype',
+                    'S',
+                    'The type of attribute, default to S, which is string.')
 
 
 class AwsDynamoDBInstance(resource.BaseResource):
 
-  def __init__(self, table_name, primary_key, throughput):
+  def __init__(self, region, table_name, primary_key, throughput):
     super(AwsDynamoDBInstance, self).__init__()
-    self.attributes = 'AttributeName=' + primary_key + ',AttributeType=S'
+    self.region = region
+    self.attributes = 'AttributeName=' + primary_key + \
+                      ',AttributeType=' + FLAGS.aws_dynamodb_attributetype
     self.table_name = table_name
-    self.primary_key = 'AttributeName=' + primary_key + ',KeyType=HASH'
+    self.primary_key = 'AttributeName=' + primary_key + \
+                       ',KeyType=HASH'
     self.throughput = throughput
 
   def _Create(self):
     """Creates the dynamodb table."""
     cmd = ['aws', 'dynamodb', 'create-table',
-           '--region', FLAGS.aws_dynamodb_region,
+           '--region', self.region,
            '--attribute-definitions', self.attributes,
            '--table-name', self.table_name,
            '--key-schema', self.primary_key,
@@ -52,66 +54,44 @@ class AwsDynamoDBInstance(resource.BaseResource):
   def _Delete(self):
     """Deletes the table."""
     cmd = ['aws', 'dynamodb', 'delete-table',
-           '--region', FLAGS.aws_dynamodb_region,
+           '--region', self.region,
            '--table-name', self.table_name]
     vm_util.IssueCommand(cmd)
 
   def _IsReady(self):
     """Check if table is ready."""
-    logging.info("Trying to get Table info for %s",
+    logging.info('Getting table ready status for %s',
                  self.table_name)
-    try:
-      cmd = ['aws', 'dynamodb', 'describe-table',
-             '--region', FLAGS.aws_dynamodb_region,
-             '--table-name', self.table_name]
-      stdout, stderr, retcode = vm_util.IssueCommand(cmd)
-      result = json.loads(stdout)
-      table_info = result['Table']
-      logging.info("isready table_info:", table_info)
-      table_status = table_info['TableStatus']
-      if table_status == 'ACTIVE':
-        logging.info("DynamoDB Table is up and running.")
-        return table_info
-    except errors.VirtualMachine.RemoteCommandError as e:
-      raise errors.Resource.RetryableCreationError(
-          "DynamoDB Table not up yet: %s." % str(e))
-    else:
-      raise errors.Resource.RetryableCreationError(
-          "DynamoDB not up yet. Status: %s" %
-          table_status)
+    cmd = ['aws', 'dynamodb', 'describe-table',
+           '--region', self.region,
+           '--table-name', self.table_name]
+    stdout, _, retcode = vm_util.IssueCommand(cmd)
+    result = json.loads(stdout)
+    return result['Table']['TableStatus'] == 'ACTIVE'
 
   def _Exists(self):
     """Returns true if the table exists."""
-    logging.info("Checking if table exists %s",
+    logging.info('Checking if table %s exists',
                  self.table_name)
-    try:
-      cmd = ['aws', 'dynamodb', 'describe-table',
-             '--region', FLAGS.aws_dynamodb_region,
-             '--table-name', self.table_name]
-      stdout, stderr, retcode = vm_util.IssueCommand(cmd)
-      result = json.loads(stdout)
-      table_info = result['Table']
-      table_status = table_info['TableStatus']
-      if table_status == 'ACTIVE':
-        logging.info("DynamoDB Table exists.")
-        return True
-      else:
-        return False
-    except errors.VirtualMachine.RemoteCommandError as e:
-      raise errors.Resource.RetryableCreationError(
-          "DynamoDB Table not up yet: %s." % str(e))
+    cmd = ['aws', 'dynamodb', 'describe-table',
+           '--region', self.region,
+           '--table-name', self.table_name]
+    stdout, _, retcode = vm_util.IssueCommand(cmd)
+    if retcode != 0:
+      return False
+    else:
+      return True
 
   def _DescribeTable(self):
     """Calls describe on table."""
     cmd = ['aws', 'dynamodb', 'describe-table',
-           '--region', FLAGS.aws_dynamodb_region,
+           '--region', self.region,
            '--table-name', self.table_name]
     stdout, stderr, retcode = vm_util.IssueCommand(cmd)
     if retcode != 0:
       logging.info('Could not find table %s, %s', self.table_name, stderr)
       return {}
-    result = json.loads(stdout)
-    for table_info in result['Table']:
+    for table_info in json.loads(stdout)['Table']:
       if table_info[3] == self.table_name:
         return table_info
     return {}
