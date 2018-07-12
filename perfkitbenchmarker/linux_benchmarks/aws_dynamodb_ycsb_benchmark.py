8d25700cb3399a782476d2d05992e42954454d7c
flint <flint@google.com>
2018-07-12 13:34:05 -0700

    added sortkey and LSI/GSI options to dynamodb.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=204354317

--- perfkitbenchmarker/linux_benchmarks/aws_dynamodb_ycsb_benchmark.py
+++ perfkitbenchmarker/linux_benchmarks/aws_dynamodb_ycsb_benchmark.py
@@ -1,4 +1,4 @@
-# Copyright 2016 PerfKitBenchmarker Authors. All rights reserved.
+# Copyright 2018 PerfKitBenchmarker Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
 """Run YCSB benchmark against AWS DynamoDB.
 This benchmark does not provision VMs for the corresponding DynamboDB database.
 The only VM group is client group that sends requests to specifiedDB.
-TODO: add RANGE option.
 TODO: add DAX option.
 TODO: add global table option.
 """
@@ -44,81 +43,89 @@ AWS_REMOTE_CRED_DIR = '/tmp/AWSCredentials.properties'
 flags.DEFINE_string('aws_dynamodb_ycsb_awscredentials_properties',
                     './AWSCredentials.properties',
                     'The AWS credential location. Defaults to PKB top folder')
-flags.DEFINE_enum('aws_dynamodb_ycsb_consistentReads',
-                  'false', ['false', 'true'],
-                  "Consistent reads cost 2x eventual reads. "
-                  "'false' is default which is eventual")
+flags.DEFINE_boolean('aws_dynamodb_ycsb_consistentReads',
+                     False,
+                     "Consistent reads cost 2x eventual reads. "
+                     "'false' is default which is eventual")
 
 
 def GetConfig(user_config):
-    config = configs.LoadConfig(BENCHMARK_CONFIG, user_config, BENCHMARK_NAME)
-    if FLAGS['ycsb_client_vms'].present:
-        config['vm_groups']['default']['vm_count'] = FLAGS.ycsb_client_vms
-    return config
+  config = configs.LoadConfig(BENCHMARK_CONFIG, user_config, BENCHMARK_NAME)
+  if FLAGS['ycsb_client_vms'].present:
+    config['vm_groups']['default']['vm_count'] = FLAGS.ycsb_client_vms
+  return config
 
 
 def CheckPrerequisites(benchmark_config):
-    """Verifies that the required resources are present.
-    Raises:
-    perfkitbenchmarker.data.ResourceNotFound: On missing resource.
-    """
-    ycsb.CheckPrerequisites()
+  """Verifies that the required resources are present.
+  Raises:
+  perfkitbenchmarker.data.ResourceNotFound: On missing resource.
+  """
+  ycsb.CheckPrerequisites()
 
 
 def Prepare(benchmark_spec):
-    """Install YCSB on the target vm.
-    Args:
-    benchmark_spec: The benchmark specification. Contains all data that is
-        required to run the benchmark.
-    """
-    benchmark_spec.always_call_cleanup = True
+  """Install YCSB on the target vm.
+  Args:
+  benchmark_spec: The benchmark specification. Contains all data that is
+      required to run the benchmark.
+  """
+  benchmark_spec.always_call_cleanup = True
 
-    benchmark_spec.dynamodb_instance = aws_dynamodb.AwsDynamoDBInstance(
-        table_name='pkb-{0}'.format(FLAGS.run_uri))
-    benchmark_spec.dynamodb_instance._Create()
+  benchmark_spec.dynamodb_instance = aws_dynamodb.AwsDynamoDBInstance(
+      table_name='pkb-{0}'.format(FLAGS.run_uri))
+  benchmark_spec.dynamodb_instance._Create()
 
-    vms = benchmark_spec.vms
-    # Install required packages.
-    vm_util.RunThreaded(_Install, vms)
-    benchmark_spec.executor = ycsb.YCSBExecutor('dynamodb')
+  vms = benchmark_spec.vms
+  # Install required packages.
+  vm_util.RunThreaded(_Install, vms)
+  benchmark_spec.executor = ycsb.YCSBExecutor('dynamodb')
 
 
 def Run(benchmark_spec):
-    """Run YCSB on the target vm.
-    Args:
-    benchmark_spec: The benchmark specification. Contains all data that is
-    required to run the benchmark.
-    Returns:
-    A list of sample.Sample objects.
-    """
-    vms = benchmark_spec.vms
+  """Run YCSB on the target vm.
+  Args:
+  benchmark_spec: The benchmark specification. Contains all data that is
+  required to run the benchmark.
+  Returns:
+  A list of sample.Sample objects.
+  """
+  vms = benchmark_spec.vms
 
-    run_kwargs = {
-        'dynamodb.awsCredentialsFile': AWS_REMOTE_CRED_DIR,
-        'dynamodb.primaryKey': FLAGS.aws_dynamodb_primarykey,
-        'dynamodb.endpoint': benchmark_spec.dynamodb_instance.GetEndPoint(),
-        'table': 'pkb-{0}'.format(FLAGS.run_uri),
-        'dynamodb.consistentReads': FLAGS.aws_dynamodb_ycsb_consistentReads,
-    }
-    load_kwargs = run_kwargs.copy()
-    if FLAGS['ycsb_preload_threads'].present:
-        load_kwargs['threads'] = FLAGS['ycsb_preload_threads']
-    samples = list(benchmark_spec.executor.LoadAndRun(
-        vms, load_kwargs=load_kwargs, run_kwargs=run_kwargs))
-    return samples
+  run_kwargs = {
+    'dynamodb.awsCredentialsFile': AWS_REMOTE_CRED_DIR,
+    'dynamodb.primaryKey': FLAGS.aws_dynamodb_primarykey,
+    'dynamodb.endpoint': benchmark_spec.dynamodb_instance.GetEndPoint(),
+    'table': 'pkb-{0}'.format(FLAGS.run_uri),
+  }
+  if FLAGS.aws_dynamodb_use_sort:
+    run_kwargs.update({'dynamodb.primaryKeyType': 'HASH_AND_RANGE',
+                       'dynamodb.hashKeyName': FLAGS.aws_dynamodb_primarykey,
+                       'dynamodb.primaryKey': FLAGS.aws_dynamodb_sortkey,})
+  if FLAGS.aws_dynamodb_ycsb_consistentReads:
+    run_kwargs.update({'dynamodb.consistentReads': 'true',})
+  load_kwargs = run_kwargs.copy()
+  if FLAGS['ycsb_preload_threads'].present:
+    load_kwargs['threads'] = FLAGS['ycsb_preload_threads']
+  samples = list(benchmark_spec.executor.LoadAndRun(
+      vms, load_kwargs=load_kwargs, run_kwargs=run_kwargs))
+  return samples
 
 
 def Cleanup(benchmark_spec):
-    """Cleanup YCSB on the target vm.
-    Args:
-    benchmark_spec: The benchmark specification. Contains all data that is
-    required to run the benchmark.
-    """
-    benchmark_spec.dynamodb_instance._Delete()
+  """Cleanup YCSB on the target vm.
+  Args:
+  benchmark_spec: The benchmark specification. Contains all data that is
+  required to run the benchmark.
+  """
+
+
+  benchmark_spec.dynamodb_instance._Delete()
 
 
 def _Install(vm):
-    """Install YCSB on client 'vm'."""
-    vm.Install('ycsb')
-    # copy AWS creds
-    vm.RemoteCopy(FLAGS.aws_dynamodb_ycsb_awscredentials_properties, AWS_REMOTE_CRED_DIR)
+  """Install YCSB on client 'vm'."""
+  vm.Install('ycsb')
+  # copy AWS creds
+  vm.RemoteCopy(FLAGS.aws_dynamodb_ycsb_awscredentials_properties,
+                AWS_REMOTE_CRED_DIR)
--- perfkitbenchmarker/providers/aws/aws_dynamodb.py
+++ perfkitbenchmarker/providers/aws/aws_dynamodb.py
@@ -19,15 +19,27 @@ Tables can be created and deleted.
 import json
 import logging
 
-from perfkitbenchmarker import resource
 from perfkitbenchmarker import flags
+from perfkitbenchmarker import resource
 from perfkitbenchmarker import vm_util
 from perfkitbenchmarker.providers.aws import util
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('aws_dynamodb_primarykey',
                     'primary_key',
-                    'The primaryKey of dynamodb table.')
+                    'The primaryKey of dynamodb table.'
+                    'This switches to sortkey if using sort.'
+                    'If testing GSI/LSI, use the range keyname'
+                    'of the index you want to test')
+flags.DEFINE_boolean('aws_dynamodb_use_sort',
+                     False,
+                     'determine whether to use sort key or not')
+flags.DEFINE_string('aws_dynamodb_sortkey',
+                    'sort_key',
+                    'The sortkey of dynamodb table.  '
+                    'This switches to primarykey if using sort.'
+                    'If testing GSI/LSI, use the primary keyname'
+                    'of the index you want to test')
 flags.DEFINE_enum('aws_dynamodb_attributetype',
                   'S', ['S', 'N', 'B'],
                   'The type of attribute, default to S (String).'
@@ -35,19 +47,105 @@ flags.DEFINE_enum('aws_dynamodb_attributetype',
 flags.DEFINE_string('aws_dynamodb_capacity',
                     'ReadCapacityUnits=5,WriteCapacityUnits=5',
                     'Set RCU/WCU for dynamodb table')
+flags.DEFINE_integer('aws_dynamodb_lsi_count',
+                     0, 'Set amount of Local Secondary Indexes. Only set 0-5')
+flags.register_validator('aws_dynamodb_lsi_count',
+                         lambda value: -1 < value < 6,
+                         message='--count must be from 0-5')
+flags.register_validator('aws_dynamodb_use_sort',
+                         lambda sort: sort or not FLAGS.aws_dynamodb_lsi_count,
+                         message='--aws_dynamodb_lsi_count requires sort key.')
+flags.DEFINE_integer('aws_dynamodb_gsi_count',
+                     0, 'Set amount of Global Secondary Indexes. Only set 0-5')
+flags.register_validator('aws_dynamodb_gsi_count',
+                         lambda value: -1 < value < 6,
+                         message='--count must be from 0-5')
+
+
+class _GetIndexes():
+  """Used to create secondary indexes."""
+
+  def __init__(self):
+    self.lsi_count = FLAGS.aws_dynamodb_lsi_count
+    self.gsi_count = FLAGS.aws_dynamodb_gsi_count
+
+  def CreateLocalSecondaryIndex(self):
+    """Used to create local secondary indexes."""
+    lsi_items = []
+    lsi_entry = []
+    attr_list = []
+    for lsi in range(0, self.lsi_count):
+      lsi_item = ('{{"IndexName": "lsiidx{0}",'
+                  '"KeySchema": [{{'
+                  '"AttributeName": "{1}",'
+                  '"KeyType": "HASH"}},{{'
+                  '"AttributeName": "lattr{2}",'
+                  '"KeyType": "RANGE"}}],'
+                  '"Projection": {{'
+                  '"ProjectionType": "KEYS_ONLY"}}}}'.format(
+                      str(lsi),
+                      FLAGS.aws_dynamodb_primarykey,
+                      str(lsi)))
+      lsi_entry.append(lsi_item)
+      attr_list.append('{{"AttributeName": "lattr{0}","AttributeType": "{1}"}}'
+                       .format(str(lsi), FLAGS.aws_dynamodb_attributetype))
+    lsi_items.append('[' + ','.join(lsi_entry) + ']')
+    lsi_items.append(','.join(attr_list))
+    return lsi_items
+
+  def CreateGlobalSecondaryIndex(self):
+    """Used to create global secondary indexes."""
+    gsi_items = []
+    gsi_entry = []
+    attr_list = []
+    for gsi in range(0, self.gsi_count):
+      gsi_item = ('{{"IndexName": "gsiidx{0}",'
+                  '"KeySchema": [{{'
+                  '"AttributeName": "gsikey{1}",'
+                  '"KeyType": "HASH"}},{{'
+                  '"AttributeName": "gattr{2}",'
+                  '"KeyType": "RANGE"}}],'
+                  '"Projection": {{'
+                  '"ProjectionType": "KEYS_ONLY"}},'
+                  '"ProvisionedThroughput": {{'
+                  '"ReadCapacityUnits": {3},'
+                  '"WriteCapacityUnits": {4}}}}}'.format(str(gsi),
+                                                         str(gsi),
+                                                         str(gsi),
+                                                         5, 5))
+      gsi_entry.append(gsi_item)
+      attr_list.append('{{"AttributeName": "gattr{0}","AttributeType": "{1}"}}'
+                       .format(str(gsi), FLAGS.aws_dynamodb_attributetype))
+      attr_list.append('{{"AttributeName": "gsikey{0}","AttributeType": "{1}"}}'
+                       .format(str(gsi), FLAGS.aws_dynamodb_attributetype))
+    gsi_items.append('[' + ','.join(gsi_entry) + ']')
+    gsi_items.append(','.join(attr_list))
+    return gsi_items
 
 
 class AwsDynamoDBInstance(resource.BaseResource):
+  """Class for working with DynamoDB."""
 
   def __init__(self, table_name):
     super(AwsDynamoDBInstance, self).__init__()
     self.zone = FLAGS.zones[0]
     self.region = util.GetRegionFromZone(self.zone)
-    self.primary_key = 'AttributeName={0},KeyType=HASH'.format(FLAGS.aws_dynamodb_primarykey)
-    self.attributes = 'AttributeName={0},AttributeType={1}'\
-                      .format(FLAGS.aws_dynamodb_primarykey, FLAGS.aws_dynamodb_attributetype)
+    self.primary_key = ('{{\"AttributeName\": \"{0}\",\"KeyType\": \"HASH\"}}'
+                        .format(FLAGS.aws_dynamodb_primarykey))
+    self.sort_key = ('{{\"AttributeName\": \"{0}\",\"KeyType\": \"RANGE\"}}'
+                     .format(FLAGS.aws_dynamodb_sortkey))
+    self.part_attributes = ('{{\"AttributeName\": \"{0}\",'
+                            '\"AttributeType\": \"{1}\"}}'
+                            .format(FLAGS.aws_dynamodb_primarykey,
+                                    FLAGS.aws_dynamodb_attributetype))
+    self.sort_attributes = ('{{\"AttributeName\": \"{0}\",'
+                            '\"AttributeType\": \"{1}\"}}'
+                            .format(FLAGS.aws_dynamodb_sortkey,
+                                    FLAGS.aws_dynamodb_attributetype))
     self.table_name = table_name
     self.throughput = FLAGS.aws_dynamodb_capacity
+    self.lsi_indexes = _GetIndexes().CreateLocalSecondaryIndex()
+    self.gsi_indexes = _GetIndexes().CreateGlobalSecondaryIndex()
 
   def _Create(self):
     """Creates the dynamodb table."""
@@ -55,11 +153,33 @@ class AwsDynamoDBInstance(resource.BaseResource):
         'dynamodb',
         'create-table',
         '--region', self.region,
-        '--attribute-definitions', self.attributes,
         '--table-name', self.table_name,
+        '--attribute-definitions', self.part_attributes,
         '--key-schema', self.primary_key,
         '--provisioned-throughput', self.throughput]
-    vm_util.IssueCommand(cmd)
+    if FLAGS.aws_dynamodb_lsi_count > 0 and FLAGS.aws_dynamodb_use_sort:
+      cmd[10] = (
+          '[' + self.part_attributes + ', ' + self.sort_attributes + ', ' +
+          self.lsi_indexes[1] + ']')
+      logging.info('adding to --attribute definitions')
+      cmd.append('--local-secondary-indexes')
+      cmd.append(self.lsi_indexes[0])
+      cmd[12] = ('[' + self.primary_key + ', ' + self.sort_key + ']')
+      logging.info('adding to --key-schema')
+    elif FLAGS.aws_dynamodb_use_sort:
+      cmd[10] = ('[' + self.part_attributes + ', ' + self.sort_attributes + ']')
+      logging.info('adding to --attribute definitions')
+      cmd[12] = ('[' + self.primary_key + ', ' + self.sort_key + ']')
+      logging.info('adding to --key-schema')
+    if FLAGS.aws_dynamodb_gsi_count > 0:
+      cmd[10] = cmd[10][:-1]
+      cmd[10] += (', ' + self.gsi_indexes[1] + ']')
+      logging.info('adding to --attribute definitions')
+      cmd.append('--global-secondary-indexes')
+      cmd.append(self.gsi_indexes[0])
+    _, stderror, retcode = vm_util.IssueCommand(cmd)
+    if retcode != 0:
+      logging.warn('Failed to create table! {0}'.format(stderror))
 
   def _Delete(self):
     """Deletes the dynamodb table."""
@@ -79,7 +199,7 @@ class AwsDynamoDBInstance(resource.BaseResource):
         'describe-table',
         '--region', self.region,
         '--table-name', self.table_name]
-    stdout, _, retcode = vm_util.IssueCommand(cmd)
+    stdout, _, _ = vm_util.IssueCommand(cmd)
     result = json.loads(stdout)
     return result['Table']['TableStatus'] == 'ACTIVE'
 
@@ -91,7 +211,7 @@ class AwsDynamoDBInstance(resource.BaseResource):
         'describe-table',
         '--region', self.region,
         '--table-name', self.table_name]
-    stdout, _, retcode = vm_util.IssueCommand(cmd)
+    _, _, retcode = vm_util.IssueCommand(cmd)
     if retcode != 0:
       return False
     else:
@@ -106,7 +226,8 @@ class AwsDynamoDBInstance(resource.BaseResource):
         '--table-name', self.table_name]
     stdout, stderr, retcode = vm_util.IssueCommand(cmd)
     if retcode != 0:
-      logging.info('Could not find table {0}, {1}'.format(self.table_name, stderr))
+      logging.info(
+          'Could not find table {0}, {1}'.format(self.table_name, stderr))
       return {}
     for table_info in json.loads(stdout)['Table']:
       if table_info[3] == self.table_name:
