855c6b67ea67116ad7d3021463dc551830e57d5e
chriswilkes <chriswilkes@google.com>
2018-08-17 10:22:28 -0700

    Create new flag `--hbase_version` to set HBase version as stable recently changed.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=209167499

--- CHANGES.next.md
+++ CHANGES.next.md
@@ -164,6 +164,8 @@
 - Added support for `--os_type` ubuntu1804. Removed ubuntu1710 for GCP.
 - Added flag `--iperf3_set_tcp_window_size` to let the user avoid setting the
   TCP Window size in Iperf3 Windows performance tests.
+- Added flags `--hbase_version` and `--hbase_use_stable` to set the HBase
+  version.  Also upped hadoop to version 2.8.4.
 
 ### Bug fixes and maintenance updates:
 - Moved GPU-related specs from GceVmSpec to BaseVmSpec
---
+++ tests/linux_packages/hbase_test.py
@@ -0,0 +1,63 @@
+# Copyright 2018 PerfKitBenchmarker Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for perfkitbenchmarker.linux_packages.hbase."""
+
+import unittest
+import urllib2
+import mock
+from perfkitbenchmarker.linux_packages import hbase
+from tests import mock_flags
+
+
+class HbaseTest(unittest.TestCase):
+
+  def setUp(self):
+    self.mock_flags = mock_flags.PatchTestCaseFlags(self)
+    self.mock_flags['hbase_use_stable'].parse(False)
+    self.mock_flags['hbase_version'].parse('1.3.2.1')
+    p = mock.patch.object(urllib2, 'urlopen')
+    self.mock_url_open = p.start()
+    self.addCleanup(p.stop)
+
+  def SetUrlOpenResponse(self, text, http_code=200):
+    response = mock.Mock()
+    self.mock_url_open.return_value = response
+    response.read.return_value = text
+    response.getcode.return_value = http_code
+
+  def testGetUrlStable(self):
+    self.mock_flags['hbase_use_stable'].parse(True)
+    self.SetUrlOpenResponse(MakeHbaseUrl('1.4.6'))
+    url = hbase._GetHBaseURL()
+    self.assertRegexpMatches(url, 'hbase-1.4.6-bin.tar.gz$')
+
+  def testGetUrlVersion(self):
+    self.SetUrlOpenResponse(MakeHbaseUrl('1.3.2.1'))
+    url = hbase._GetHBaseURL()
+    self.assertRegexpMatches(url, 'hbase-1.3.2.1-bin.tar.gz$')
+
+  def testGetHBaseURLBadResponse(self):
+    # response is for version 0.1.2 but wanted the default 1.3.2.1
+    self.SetUrlOpenResponse(MakeHbaseUrl('0.1.2'))
+    with self.assertRaisesRegexp(ValueError, r'0\.1\.2.*1\.3\.2\.1'):
+      hbase._GetHBaseURL()
+
+
+def MakeHbaseUrl(version):
+  return ('<a href="hbase-{version}-bin.tar.gz">'
+          'hbase-{version}-bin.tar.gz</a>').format(version=version)
+
+
+if __name__ == '__main__':
+  unittest.main()
