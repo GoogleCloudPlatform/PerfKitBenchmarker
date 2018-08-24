17a98ae239be7ffd027ff7d50f9a84c0a9e9aed7
nathante <nathante@google.com>
2018-08-24 13:49:02 -0700

    Add QPS to sysbench metrics
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=210150138

--- tests/linux_benchmarks/sysbench_benchmark_test.py
+++ tests/linux_benchmarks/sysbench_benchmark_test.py
@@ -37,8 +37,16 @@ class MySQLServiceBenchmarkTestCase(unittest.TestCase,
         self.contents, results, metadata)
     logging.info('results are, %s', results)
     expected_results = [
-        sample.Sample('tps_array', -1, 'tps', {'tps': [1012.86, 1006.64, 1022.3, 1016.16, 1009.03, 1016.99, 1010.0, 1018.0, 1002.01, 998.49, 959.52, 913.49, 936.98, 916.01, 957.96]}),
-        sample.Sample('latency_array', -1, 'ms', {'latency': [28.67, 64.47, 38.94, 44.98, 89.16, 29.72, 106.75, 46.63, 116.8, 41.85, 27.17, 104.84, 58.92, 75.82, 73.13]})]
+        sample.Sample('tps_array', -1, 'tps', {'tps': [
+            1012.86, 1006.64, 1022.3, 1016.16, 1009.03, 1016.99, 1010.0, 1018.0,
+            1002.01, 998.49, 959.52, 913.49, 936.98, 916.01, 957.96]}),
+        sample.Sample('latency_array', -1, 'ms', {'latency': [
+            28.67, 64.47, 38.94, 44.98, 89.16, 29.72, 106.75, 46.63, 116.8,
+            41.85, 27.17, 104.84, 58.92, 75.82, 73.13]}),
+        sample.Sample('qps_array', -1, 'qps', {'qps': [
+            20333.18, 20156.38, 20448.49, 20334.15, 20194.07, 20331.31,
+            20207.00, 20348.96, 20047.11, 19972.86, 19203.97, 18221.83,
+            18689.14, 18409.68, 19155.63]})]
     self.assertSampleListsEqualUpToTimestamp(results, expected_results)
 
 
