0683b3ed3b95e8a1042c8fb9733a4df9ac39379c
Hao Wu <tohaowu@google.com>
2018-09-14 11:46:58 -0700

    Fix t2t_benchmark for GPU/CPU output.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=213018282

--- tests/linux_benchmarks/t2t_benchmark_test.py
+++ tests/linux_benchmarks/t2t_benchmark_test.py
@@ -24,10 +24,10 @@ class Tensor2TensorBenchmarkTestCase(unittest.TestCase,
                                      test_util.SamplesTestMixin):
 
   @mock.patch('time.time', mock.MagicMock(return_value=0))
-  def testT2TOutput(self):
+  def testT2TTpuOutput(self):
     self.maxDiff = None
     path = os.path.join(
-        os.path.dirname(__file__), '..', 'data', 't2t_output.txt')
+        os.path.dirname(__file__), '..', 'data', 't2t_tpu_output.txt')
     with open(path) as fp:
       t2t_contents = fp.read()
 
@@ -160,6 +160,148 @@ class Tensor2TensorBenchmarkTestCase(unittest.TestCase,
             metadata={
                 'use_tpu': True,
                 'step': 1200
+            },
+            timestamp=0)
+    ]
+    self.assertEqual(samples, golden)
+
+  @mock.patch('time.time', mock.MagicMock(return_value=0))
+  def testT2TGpuOutput(self):
+    self.maxDiff = None
+    path = os.path.join(
+        os.path.dirname(__file__), '..', 'data', 't2t_gpu_output.txt')
+    with open(path) as fp:
+      t2t_contents = fp.read()
+
+    samples = t2t_benchmark._MakeSamplesFromOutput({
+        'use_tpu': False
+    }, t2t_contents)
+
+    golden = [
+        Sample(
+            metric='Global Steps Per Second',
+            value=3.04983,
+            unit='global_steps/sec',
+            metadata={
+                'index': 0,
+                'use_tpu': False
+            },
+            timestamp=0),
+        Sample(
+            metric='Global Steps Per Second',
+            value=4.12771,
+            unit='global_steps/sec',
+            metadata={
+                'index': 1,
+                'use_tpu': False
+            },
+            timestamp=0),
+        Sample(
+            metric='Global Steps Per Second',
+            value=4.11027,
+            unit='global_steps/sec',
+            metadata={
+                'index': 2,
+                'use_tpu': False
+            },
+            timestamp=0),
+        Sample(
+            metric='Global Steps Per Second',
+            value=4.10924,
+            unit='global_steps/sec',
+            metadata={
+                'index': 3,
+                'use_tpu': False
+            },
+            timestamp=0),
+        Sample(
+            metric='Global Steps Per Second',
+            value=4.12186,
+            unit='global_steps/sec',
+            metadata={
+                'index': 4,
+                'use_tpu': False
+            },
+            timestamp=0),
+        Sample(
+            metric='Global Steps Per Second',
+            value=4.08434,
+            unit='global_steps/sec',
+            metadata={
+                'index': 5,
+                'use_tpu': False
+            },
+            timestamp=0),
+        Sample(
+            metric='Global Steps Per Second',
+            value=4.10174,
+            unit='global_steps/sec',
+            metadata={
+                'index': 6,
+                'use_tpu': False
+            },
+            timestamp=0),
+        Sample(
+            metric='Global Steps Per Second',
+            value=4.11809,
+            unit='global_steps/sec',
+            metadata={
+                'index': 7,
+                'use_tpu': False
+            },
+            timestamp=0),
+        Sample(
+            metric='Global Steps Per Second',
+            value=4.10496,
+            unit='global_steps/sec',
+            metadata={
+                'index': 8,
+                'use_tpu': False
+            },
+            timestamp=0),
+        Sample(
+            metric='Eval Loss',
+            value=7.2263174,
+            unit='',
+            metadata={
+                'use_tpu': False,
+                'step': 1000
+            },
+            timestamp=0),
+        Sample(
+            metric='Accuracy',
+            value=13.972055999999998,
+            unit='%',
+            metadata={
+                'use_tpu': False,
+                'step': 1000
+            },
+            timestamp=0),
+        Sample(
+            metric='Accuracy Per Sequence',
+            value=0.0,
+            unit='%',
+            metadata={
+                'use_tpu': False,
+                'step': 1000
+            },
+            timestamp=0),
+        Sample(
+            metric='Negative Log Perplexity',
+            value=-7.2263174,
+            unit='perplexity',
+            metadata={
+                'use_tpu': False,
+                'step': 1000
+            },
+            timestamp=0),
+        Sample(
+            metric='Top 5 Accuracy',
+            value=24.800399000000002,
+            unit='%',
+            metadata={
+                'use_tpu': False,
+                'step': 1000
             },
             timestamp=0)
     ]
