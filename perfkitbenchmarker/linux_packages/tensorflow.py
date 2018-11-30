ac523e76b17390d5f5558e5a8b17d8215a5ae749
ferneyhough <ferneyhough@google.com>
2018-11-30 11:27:34 -0800

    Update Tensorflow CPU and GPU versions to 1.12. Update Tensorflow CNN benchmarks versions to support version 1.12. Add NCCL installation to Tensorflow installation when using GPUs. Add support for CUDA 10, but keep the default at CUDA 9 for now.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=223547237

--- perfkitbenchmarker/linux_packages/tensorflow.py
+++ perfkitbenchmarker/linux_packages/tensorflow.py
@@ -1,4 +1,4 @@
-# Copyright 2017 PerfKitBenchmarker Authors. All rights reserved.
+# Copyright 2018 PerfKitBenchmarker Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,23 +21,25 @@ from perfkitbenchmarker.linux_packages import cuda_toolkit
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('tf_cpu_pip_package',
-                    'https://anaconda.org/intel/tensorflow/1.10.0/download/'
-                    'tensorflow-1.10.0-cp27-cp27mu-linux_x86_64.whl',
+                    'https://anaconda.org/intel/tensorflow/1.12.0/download/'
+                    'tensorflow-1.12.0-cp27-cp27mu-linux_x86_64.whl',
                     'TensorFlow CPU pip package to install. By default, PKB '
                     'will install an Intel-optimized CPU build when using '
                     'CPUs.')
-flags.DEFINE_string('tf_gpu_pip_package', 'tensorflow-gpu==1.10',
+flags.DEFINE_string('tf_gpu_pip_package', 'tensorflow-gpu==1.12.0',
                     'TensorFlow GPU pip package to install. By default, PKB '
-                    'will install tensorflow-gpu==1.7 when using GPUs.')
+                    'will install tensorflow-gpu==1.12 when using GPUs.')
 flags.DEFINE_string(
     't2t_pip_package', 'tensor2tensor==1.7',
     'Tensor2Tensor pip package to install. By default, PKB '
     'will install tensor2tensor==1.7 .')
 flags.DEFINE_string('tf_cnn_benchmarks_branch',
-                    'cnn_tf_v1.10_compatible',
+                    'cnn_tf_v1.12_compatible',
                     'TensorFlow CNN branchmarks branch that is compatible with '
                     'A TensorFlow version.')
 
+NCCL_URL = 'https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb'
+NCCL_PACKAGE = 'nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb'
 
 def GetEnvironmentVars(vm):
   """Return a string containing TensorFlow-related environment variables.
@@ -87,6 +89,13 @@ def Install(vm):
   if has_gpu:
     vm.Install('cuda_toolkit')
     vm.Install('cudnn')
+
+    # TODO(ferneyhough): Move NCCL installation to its own package.
+    # Currently this is dependent on CUDA 9 being installed.
+    vm.RemoteCommand('wget %s' % NCCL_URL)
+    vm.RemoteCommand('sudo dpkg -i %s' % NCCL_PACKAGE)
+    vm.RemoteCommand('sudo apt install libnccl2=2.3.5-2+cuda9.0 '
+                     'libnccl-dev=2.3.5-2+cuda9.0')
 
   vm.Install('pip')
   vm.RemoteCommand('sudo pip install requests')
