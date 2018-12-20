4590c02a686243b0d90ffc9b15dc7efe53edb465
dlott <dlott@google.com>
2018-12-20 11:04:59 -0800

    Add module doc string.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=226362255

--- perfkitbenchmarker/providers/aws/s3.py
+++ perfkitbenchmarker/providers/aws/s3.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Contains classes/functions related to S3."""
+
 from perfkitbenchmarker import flags
 from perfkitbenchmarker import linux_packages
 from perfkitbenchmarker import object_storage_service
