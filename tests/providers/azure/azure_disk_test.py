0cecdf2c1efcb2e10d348f2d4f06e0f2f0e2908f
ferneyhough <ferneyhough@google.com>
2019-04-02 13:49:51 -0700

    Fix the device letter assignment for Azure disks. When going past 'z', previously the code was continuing to 'cc', but this fix makes it continue to 'aa', as expected by the linux kernel.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=241596236

--- tests/providers/azure/azure_disk_test.py
+++ tests/providers/azure/azure_disk_test.py
@@ -32,20 +32,20 @@ class AzureDiskGetDevicePathTest(pkb_common_test_case.PkbCommonTestCase):
     self.disk.lun = 23
     self.assertEqual('/dev/sdz', self.disk.GetDevicePath())
 
-  def test_get_device_path_eq_cc(self):
+  def test_get_device_path_eq_aa(self):
     self.disk.lun = 24
-    self.assertEqual('/dev/sdcc', self.disk.GetDevicePath())
+    self.assertEqual('/dev/sdaa', self.disk.GetDevicePath())
 
-  def test_get_device_path_eq_dc(self):
-    self.disk.lun = 48
-    self.assertEqual('/dev/sddc', self.disk.GetDevicePath())
+  def test_get_device_path_eq_ba(self):
+    self.disk.lun = 50
+    self.assertEqual('/dev/sdba', self.disk.GetDevicePath())
 
   def test_get_device_path_greatest_allowable_index(self):
-    self.disk.lun = 599
+    self.disk.lun = 699
     self.assertEqual('/dev/sdzz', self.disk.GetDevicePath())
 
   def test_get_device_path_index_too_large(self):
-    self.disk.lun = 600
+    self.disk.lun = 700
     with self.assertRaises(azure_disk.TooManyAzureDisksError):
       self.disk.GetDevicePath()
 
