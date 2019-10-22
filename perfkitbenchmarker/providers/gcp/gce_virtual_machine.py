882489b94ce4c5e7227cd08fda9af7578c500eba
pclay <pclay@google.com>
2019-10-22 16:55:16 -0700

    Add support for Container Optimized OS with GCP provider. Support is currently limited to the cluster_boot benchmark.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=276173046

--- perfkitbenchmarker/providers/gcp/gce_virtual_machine.py
+++ perfkitbenchmarker/providers/gcp/gce_virtual_machine.py
@@ -832,6 +832,12 @@ class Centos7BasedGceVirtualMachine(GceVirtualMachine,
     self.python_pip_package_config = 'python2-pip'
 
 
+class ContainerOptimizedOsBasedGceVirtualMachine(
+    GceVirtualMachine, linux_vm.ContainerOptimizedOsMixin):
+  DEFAULT_IMAGE_FAMILY = 'cos-stable'
+  DEFAULT_IMAGE_PROJECT = 'cos-cloud'
+
+
 class Ubuntu1404BasedGceVirtualMachine(GceVirtualMachine,
                                        linux_vm.Ubuntu1404Mixin):
   DEFAULT_IMAGE_FAMILY = 'ubuntu-1404-lts'
