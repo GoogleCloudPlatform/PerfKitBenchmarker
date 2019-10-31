47f1cd2704398db96cc00ec37c8bcd4f9547857a
Marco Micera <marco.micera@gmail.com>
2019-10-31 15:57:21 +0100

    Cassandra branch name fix

--- perfkitbenchmarker/linux_packages/cassandra.py
+++ perfkitbenchmarker/linux_packages/cassandra.py
@@ -41,7 +41,7 @@ from six.moves import range
 JNA_JAR_URL = ('https://maven.java.net/content/repositories/releases/'
                'net/java/dev/jna/jna/4.1.0/jna-4.1.0.jar')
 CASSANDRA_GIT_REPRO = 'https://github.com/apache/cassandra.git'
-CASSANDRA_VERSION = 'cassandra-2.1.10'
+CASSANDRA_VERSION = 'cassandra-2.1'
 CASSANDRA_YAML_TEMPLATE = 'cassandra/cassandra.yaml.j2'
 CASSANDRA_ENV_TEMPLATE = 'cassandra/cassandra-env.sh.j2'
 CASSANDRA_DIR = posixpath.join(INSTALL_DIR, 'cassandra')
