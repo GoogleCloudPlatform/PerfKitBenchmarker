75485d9379114bb2709a181ea648af1a678d8346
Lee, Keerock <keerock.lee@intel.com>
2020-01-10 15:15:16 -0800

    Add Clearlinux VirtualMachine

--- perfkitbenchmarker/providers/openstack/os_virtual_machine.py
+++ perfkitbenchmarker/providers/openstack/os_virtual_machine.py
@@ -406,3 +406,8 @@ class VersionlessRhel7BasedOpenStackVirtualMachine(
 class CentOs7BasedOpenStackVirtualMachine(OpenStackVirtualMachine,
                                           linux_virtual_machine.CentOs7Mixin):
   DEFAULT_IMAGE = 'centos7'
+
+
+class ClearBasedOpenStackVirtualMachine(OpenStackVirtualMachine,
+                                        linux_virtual_machine.ClearMixin):
+  DEFAULT_IMAGE = 'upstream-clear'
