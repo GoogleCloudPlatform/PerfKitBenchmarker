709fd45442bc15aa9c7d480d7080f5b2c9041796
Derek Phanekham <dphanekham@smu.edu>
2020-02-14 01:32:22 -0600

    remove white space

--- perfkitbenchmarker/linux_packages/iperf.py
+++ perfkitbenchmarker/linux_packages/iperf.py
@@ -31,7 +31,7 @@ def _Install(vm):
   vm.Install('build_tools')
   vm.Install('wget')
 
-  vm.RemoteCommand('wget -O %s/%s %s' % 
+  vm.RemoteCommand('wget -O %s/%s %s' %
                    (INSTALL_DIR, IPERF_TAR, IPERF_URL))
 
   vm.RemoteCommand('cd %s; tar xvf %s; cd %s; '
