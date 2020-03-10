GoogleCloudPlatform/PerfKitBenchmarker

commit 2b599617a28a68b3ba45a1f728b4e0ec42ba5330

Propagate Azure credentials to AKS when running as a service principal. This is consistent with propagating VM credentials in azure_credentials.py. This can be disabled by setting `--bootstrap_azure_service_principal=False`.
-------------
Created by MOE: https://github.com/google/moe
MOE_MIGRATED_REVID=296237588

perfkitbenchmarker/linux_packages/azure_credentials.py
@@ -15,23 +15,26 @@
 """Package for installing the Azure credentials."""
 
 import os
+
 from perfkitbenchmarker import object_storage_service
 
-AZURE_CREDENTIAL_LOCATION = '.azure'
-AZURE_CREDENTIAL_TOKENS_FILE = os.path.join(
-    AZURE_CREDENTIAL_LOCATION, 'accessTokens.json')
-AZURE_CREDENTIAL_PROFILE_FILE = os.path.join(
-    AZURE_CREDENTIAL_LOCATION, 'azureProfile.json')
+AZURE_CREDENTIAL_DIRECTORY = os.path.join('~', '.azure')
+AZURE_CREDENTIAL_TOKENS_FILE = 'accessTokens.json'
+AZURE_CREDENTIAL_PROFILE_FILE = 'azureProfile.json'
 
 
 def Install(vm):
   """Copies Azure credentials to the VM."""
-  vm.RemoteCommand('mkdir -p {0}'.format(AZURE_CREDENTIAL_LOCATION))
+  vm.RemoteCommand('mkdir -p {0}'.format(AZURE_CREDENTIAL_DIRECTORY))
   vm.PushFile(
-      object_storage_service.FindCredentialFile(
-          os.path.join('~', AZURE_CREDENTIAL_TOKENS_FILE)),
+      FindCredentialFile(AZURE_CREDENTIAL_TOKENS_FILE),
       AZURE_CREDENTIAL_TOKENS_FILE)
   vm.PushFile(
-      object_storage_service.FindCredentialFile(
-          os.path.join('~', AZURE_CREDENTIAL_PROFILE_FILE)),
+      FindCredentialFile(AZURE_CREDENTIAL_PROFILE_FILE),
       AZURE_CREDENTIAL_PROFILE_FILE)
+
+
+def FindCredentialFile(basename):
+  """Return the location of the credential file with the given name."""
+  return object_storage_service.FindCredentialFile(
+      os.path.join(AZURE_CREDENTIAL_DIRECTORY, basename))

perfkitbenchmarker/providers/azure/service_principal.py
@@ -15,10 +15,18 @@
 """Contains classes/functions related to Azure Service Principals."""
 
 import json
-import uuid
+import logging
+
+from perfkitbenchmarker import errors
+from perfkitbenchmarker import flags
 from perfkitbenchmarker import resource
 from perfkitbenchmarker import vm_util
+from perfkitbenchmarker.linux_packages import azure_credentials
 from perfkitbenchmarker.providers import azure
+
+FLAGS = flags.FLAGS
+
+SERVICE_PRINCIPAL_FILE = 'aksServicePrincipal.json'
 
 
 class ServicePrincipal(resource.BaseResource):
@@ -30,14 +38,49 @@ class ServicePrincipal(resource.BaseResource):
   def GetInstance(cls):
     """Returns the service principal instance."""
     if cls._instance is None:
-      cls._instance = cls()
+      if FLAGS.bootstrap_azure_service_principal:
+        cls._instance = cls.LoadFromFile()
+      else:
+        cls._instance = cls()
     return cls._instance
 
-  def __init__(self):
-    super(ServicePrincipal, self).__init__()
-    self.app_id = None
-    self.name = str(uuid.uuid4())
-    self.password = ''
+  @classmethod
+  def LoadFromFile(cls):
+    """Loads a service principal from a file."""
+    with open(azure_credentials.FindCredentialFile(azure_credentials.AZURE_CREDENTIAL_PROFILE_FILE), encoding='utf-8-sig') as profile_fp, \
+        open(azure_credentials.FindCredentialFile(azure_credentials.AZURE_CREDENTIAL_TOKENS_FILE)) as tokens_fp:
+      subscriptions = json.load(profile_fp)['subscriptions']
+      subscription = [sub for sub in subscriptions if sub['isDefault']][0]
+      subscription_type = subscription['user']['type']
+      if subscription_type != 'servicePrincipal':
+        # We are using user auth, and will probably have permission to create a
+        # service principal.
+        logging.info("Azure credentials are of type '%s'. "
+                     'Will try to create a new service principal.',
+                     subscription_type)
+        return cls()
+      # name and id are backwards
+      name = subscription['id']
+      app_id = subscription['user']['name']
+      for token in json.load(tokens_fp):
+        if token['servicePrincipalId'] == app_id:
+          logging.info("Azure credentials are of type 'servicePrincipal'. "
+                       'Will reuse them for benchmarking.')
+          return cls(
+              name, app_id, password=token['accessToken'], user_managed=True)
+      logging.warning('No access tokens found matching Azure defaultProfile '
+                      'Will try to create a new service principal.')
+      return cls()
+
+  def __init__(self, name=None, app_id=None, password=None, user_managed=False):
+    super(ServicePrincipal, self).__init__(user_managed)
+    # Service principles can be referred to by user provided name as long as
+    # they are prefixed by http:// or by a server generated appId.
+    # Prefer user provided ID for idempotence when talking to Active Directory.
+    # When talking to AKS or ACR, app_id is required.
+    self.name = 'http://' + (name or 'pkb-' + FLAGS.run_uri)
+    self.app_id = app_id
+    self.password = password
 
   def _Create(self):
     """Creates the service principal."""
@@ -45,25 +88,29 @@ class ServicePrincipal(resource.BaseResource):
         azure.AZURE_PATH, 'ad', 'sp', 'create-for-rbac', '--name', self.name,
         '--skip-assignment'
     ]
-    stdout, _ = vm_util.IssueCommand(cmd)
+    stdout, _, _ = vm_util.IssueCommand(cmd)
     response = json.loads(stdout)
     if response:
       self.app_id = response['appId']
       self.password = response['password']
+      if not self.app_id or not self.password:
+        raise errors.Resource.CreationError(
+            'Invalid creation response when creating service principal. '
+            'Expected appId and password. Received:\n' + stdout)
       return True
     return False
 
   def _Exists(self):
     """Returns True if the service principal exists."""
-    cmd = [
-        azure.AZURE_PATH, 'ad', 'sp', 'list', '--spn', 'http://' + self.name,
-    ]
-    stdout, _ = vm_util.IssueRetryableCommand(cmd)
-    return bool(json.loads(stdout))
+    # Use show rather than list, because list requires admin privileges.
+    cmd = [azure.AZURE_PATH, 'ad', 'sp', 'show', '--id', self.name]
+    try:
+      vm_util.IssueCommand(cmd, raise_on_failure=True)
+      return True
+    except errors.VmUtil.IssueCommandError:
+      return False
 
   def _Delete(self):
     """Deletes the service principal."""
-    cmd = [
-        azure.AZURE_PATH, 'ad', 'sp', 'delete', '--id', 'http://' + self.name,
-    ]
+    cmd = [azure.AZURE_PATH, 'ad', 'sp', 'delete', '--id', self.name]
     vm_util.IssueCommand(cmd)

perfkitbenchmarker/object_storage_service.py
@@ -285,6 +285,7 @@ class ObjectStorageService(
     return []
 
 
+# TODO(user): Move somewhere more generic
 def FindCredentialFile(default_location):
   """Return the path to the credential file."""
 

perfkitbenchmarker/providers/azure/azure_container_service.py
@@ -127,8 +127,10 @@ class AksCluster(container_service.KubernetesCluster):
     self.service_principal = service_principal.ServicePrincipal.GetInstance()
     self._deleted = False
 
-  # TODO(ferneyhough): Consider adding
-  # --kubernetes-version=FLAGS.container_cluster_version.
+  # Creating an AKS cluster with a fresh service principal usually fails due
+  # to a race condition. Active Directory knows the service principal exists,
+  # but AKS does not. (https://github.com/Azure/azure-cli/issues/9585)
+  @vm_util.Retry()
   def _Create(self):
     """Creates the AKS cluster."""
     vm_util.IssueCommand([
@@ -139,9 +141,11 @@ class AksCluster(container_service.KubernetesCluster):
         '--location', self.location,
         '--dns-name-prefix', 'pkb' + FLAGS.run_uri,
         '--ssh-key-value', vm_util.GetPublicKeyPath(),
-        # TODO(pclay): Debug ocassional invalid service principal error.
         '--service-principal', self.service_principal.app_id,
+        # TODO(pclay): avoid logging client secret
         '--client-secret', self.service_principal.password,
+        # TODO(pclay): Consider adding
+        # --kubernetes-version=FLAGS.container_cluster_version.
     ] + self.resource_group.args, timeout=1800)
 
   def _Exists(self):

perfkitbenchmarker/providers/azure/flags.py
@@ -81,3 +81,11 @@ flags.DEFINE_enum('azure_redis_size',
 
 flags.DEFINE_boolean('azure_low_priority_vms', False,
                      'Whether to set the priority to low for Azure VMs')
+
+flags.DEFINE_boolean('bootstrap_azure_service_principal', True,
+                     'Whether to use the current service principal credentials '
+                     'when passing a service principal to a service. This has '
+                     'no effect if the logged in user is not a service '
+                     'principal. This is useful, because service principals '
+                     "usually lack the 'User Authentication Admin' role that "
+                     'allows creation of new service principals.')

commit 3ecb6839335d71894a53bc64952e8472510cc150

SCP Azure credentials into ~/.azure, which was broken in 2b59961.
-------------
Created by MOE: https://github.com/google/moe
MOE_MIGRATED_REVID=299462787

perfkitbenchmarker/linux_packages/azure_credentials.py
@@ -19,22 +19,18 @@ import os
 from perfkitbenchmarker import object_storage_service
 
 AZURE_CREDENTIAL_DIRECTORY = os.path.join('~', '.azure')
-AZURE_CREDENTIAL_TOKENS_FILE = 'accessTokens.json'
-AZURE_CREDENTIAL_PROFILE_FILE = 'azureProfile.json'
+AZURE_CREDENTIAL_TOKENS_FILE = os.path.join(AZURE_CREDENTIAL_DIRECTORY,
+                                            'accessTokens.json')
+AZURE_CREDENTIAL_PROFILE_FILE = os.path.join(AZURE_CREDENTIAL_DIRECTORY,
+                                             'azureProfile.json')
 
 
 def Install(vm):
   """Copies Azure credentials to the VM."""
   vm.RemoteCommand('mkdir -p {0}'.format(AZURE_CREDENTIAL_DIRECTORY))
   vm.PushFile(
-      FindCredentialFile(AZURE_CREDENTIAL_TOKENS_FILE),
+      object_storage_service.FindCredentialFile(AZURE_CREDENTIAL_TOKENS_FILE),
       AZURE_CREDENTIAL_TOKENS_FILE)
   vm.PushFile(
-      FindCredentialFile(AZURE_CREDENTIAL_PROFILE_FILE),
+      object_storage_service.FindCredentialFile(AZURE_CREDENTIAL_PROFILE_FILE),
       AZURE_CREDENTIAL_PROFILE_FILE)
-
-
-def FindCredentialFile(basename):
-  """Return the location of the credential file with the given name."""
-  return object_storage_service.FindCredentialFile(
-      os.path.join(AZURE_CREDENTIAL_DIRECTORY, basename))

perfkitbenchmarker/providers/azure/service_principal.py
@@ -19,6 +19,7 @@ import logging
 
 from perfkitbenchmarker import errors
 from perfkitbenchmarker import flags
+from perfkitbenchmarker import object_storage_service
 from perfkitbenchmarker import resource
 from perfkitbenchmarker import vm_util
 from perfkitbenchmarker.linux_packages import azure_credentials
@@ -47,8 +48,8 @@ class ServicePrincipal(resource.BaseResource):
   @classmethod
   def LoadFromFile(cls):
     """Loads a service principal from a file."""
-    with open(azure_credentials.FindCredentialFile(azure_credentials.AZURE_CREDENTIAL_PROFILE_FILE), encoding='utf-8-sig') as profile_fp, \
-        open(azure_credentials.FindCredentialFile(azure_credentials.AZURE_CREDENTIAL_TOKENS_FILE)) as tokens_fp:
+    with open(object_storage_service.FindCredentialFile(azure_credentials.AZURE_CREDENTIAL_PROFILE_FILE), encoding='utf-8-sig') as profile_fp, \
+        open(object_storage_service.FindCredentialFile(azure_credentials.AZURE_CREDENTIAL_TOKENS_FILE)) as tokens_fp:
       subscriptions = json.load(profile_fp)['subscriptions']
       subscription = [sub for sub in subscriptions if sub['isDefault']][0]
       subscription_type = subscription['user']['type']