45f51848a665d4d77db1bdf0cf52b84f4a7b1cff
Lee, Keerock <keerock.lee@intel.com>
2020-03-11 01:10:01 -0700

    Add unittest for Floating IP network exception case

--- tests/providers/openstack/os_virtual_machine_test.py
+++ tests/providers/openstack/os_virtual_machine_test.py
@@ -6,6 +6,7 @@ import mock
 import json
 from perfkitbenchmarker import benchmark_spec
 from perfkitbenchmarker import configs
+from perfkitbenchmarker import errors
 from perfkitbenchmarker import flags
 from perfkitbenchmarker import linux_benchmarks
 from perfkitbenchmarker.configs import benchmark_config_spec
@@ -29,6 +30,7 @@ iperf:
 
 _network_true = {'router:external': True}
 _network_external = {'router:external': 'External'}
+_network_fail = {'router:external': 'Fail'}
 
 
 class BaseOpenStackNetworkTest(pkb_common_test_case.PkbCommonTestCase):
@@ -36,7 +38,21 @@ class BaseOpenStackNetworkTest(pkb_common_test_case.PkbCommonTestCase):
   def _CreateBenchmarkSpecFromYaml(self, yaml_string,
                                    benchmark_name=_BENCHMARK_NAME):
     config = configs.LoadConfig(yaml_string, {}, benchmark_name)
-    return self._CreateBenchmarkSpecFromConfigDict(config, benchmark_name)
+    spec = self._CreateBenchmarkSpecFromConfigDict(config, benchmark_name)
+    spec.disable_interrupt_moderation = False
+    spec.disable_rss = False
+    spec.zone = "test-zone"
+    spec.cidr = "192.164.1.0/24"
+    spec.machine_type = "Test_machine_type"
+    spec.gpu_count = "1"
+    spec.gpu_type = "test-gpu-type"
+    spec.image = "test-image"
+    spec.install_packages = "None"
+    spec.background_cpu_threads = "None"
+    spec.background_network_mbits_per_sec = "1"
+    spec.background_network_ip_type = "None"
+    spec.vm_metadata = {}
+    return spec
 
   def _CreateBenchmarkSpecFromConfigDict(self, config_dict, benchmark_name):
     config_spec = benchmark_config_spec.BenchmarkConfigSpec(
@@ -51,52 +67,34 @@ class BaseOpenStackNetworkTest(pkb_common_test_case.PkbCommonTestCase):
 class OpenStackVirtualMachineTest(BaseOpenStackNetworkTest):
 
 
-  @mock.patch.object(os_virtual_machine.OpenStackVirtualMachine, '_CheckNetworkExists', return_value=_network_true)
+  @mock.patch.object(os_virtual_machine.OpenStackVirtualMachine, '_CheckNetworkExists')
   def test_CheckFloatingIPNetworkExistsWithTrue(self, mock_CheckNetworkExists, flags=FLAGS):
     mock_CheckNetworkExists.return_value = _network_true
-
     flags.ignore_package_requirements = True
     spec = self._CreateBenchmarkSpecFromYaml(_CFG_DEFAULT_DEFAULT)
-    spec.disable_interrupt_moderation = False
-    spec.disable_rss = False
-    spec.zone = "test-zone"
-    spec.cidr = "192.164.1.0/24"
-    spec.machine_type = "Test_machine_type"
-    spec.gpu_count = "1"
-    spec.gpu_type = "test-gpu-type"
-    spec.image = "test-image"
-    spec.install_packages = "None"
-    spec.background_cpu_threads = "None"
-    spec.background_network_mbits_per_sec = "1"
-    spec.background_network_ip_type = "None"
-    spec.vm_metadata = {}
     openstackVM = os_virtual_machine.OpenStackVirtualMachine(spec)
     network = openstackVM._CheckFloatingIPNetworkExists('External')
     self.assertEqual(_network_true, network)
 
 
-  @mock.patch.object(os_virtual_machine.OpenStackVirtualMachine, '_CheckNetworkExists', return_value=_network_external)
+  @mock.patch.object(os_virtual_machine.OpenStackVirtualMachine, '_CheckNetworkExists')
   def test_CheckFloatingIPNetworkExistsWithExternal(self, mock_CheckNetworkExists, flags=FLAGS):
     mock_CheckNetworkExists.return_value = _network_external
-
     flags.ignore_package_requirements = True
     spec = self._CreateBenchmarkSpecFromYaml(_CFG_DEFAULT_DEFAULT)
-    spec.disable_interrupt_moderation = False
-    spec.disable_rss = False
-    spec.zone = "test-zone"
-    spec.cidr = "192.164.1.0/24"
-    spec.machine_type = "Test_machine_type"
-    spec.gpu_count = "1"
-    spec.gpu_type = "test-gpu-type"
-    spec.image = "test-image"
-    spec.install_packages = "None"
-    spec.background_cpu_threads = "None"
-    spec.background_network_mbits_per_sec = "1"
-    spec.background_network_ip_type = "None"
-    spec.vm_metadata = {}
     openstackVM = os_virtual_machine.OpenStackVirtualMachine(spec)
     network = openstackVM._CheckFloatingIPNetworkExists('External')
     self.assertEqual(_network_external, network)
+
+
+  @mock.patch.object(os_virtual_machine.OpenStackVirtualMachine, '_CheckNetworkExists')
+  def test_CheckFloatingIPNetworkExistsWithFail(self, mock_CheckNetworkExists, flags=FLAGS):
+    mock_CheckNetworkExists.return_value = _network_fail
+    flags.ignore_package_requirements = True
+    spec = self._CreateBenchmarkSpecFromYaml(_CFG_DEFAULT_DEFAULT)
+    openstackVM = os_virtual_machine.OpenStackVirtualMachine(spec)
+    with self.assertRaises(errors.Config.InvalidValue):
+      openstackVM._CheckFloatingIPNetworkExists('External')
 
 
 if __name__ == '__main__':
