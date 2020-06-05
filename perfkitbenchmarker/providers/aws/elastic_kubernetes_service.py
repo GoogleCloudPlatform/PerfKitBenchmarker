c1957cfcc599b898bb9ed7dff72435e5f6db5943
Patrick Clay <pclay@google.com>
2020-06-04 20:12:52 -0700

    Use comma separated list in --tags flag for EKS.
    PiperOrigin-RevId: 314854167

--- perfkitbenchmarker/providers/aws/elastic_kubernetes_service.py
+++ perfkitbenchmarker/providers/aws/elastic_kubernetes_service.py
@@ -81,7 +81,7 @@ class EksCluster(container_service.KubernetesCluster):
         'node-type': self.vm_config.machine_type,
         'node-volume-size': self.vm_config.boot_disk_size,
         'region': self.region,
-        'tags': ' '.join('{}={}'.format(k, v) for k, v in tags.items()),
+        'tags': ','.join('{}={}'.format(k, v) for k, v in tags.items()),
         'ssh-public-key':
             aws_virtual_machine.AwsKeyFileManager.GetKeyNameForRun(),
         'version': self.cluster_version,
