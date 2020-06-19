2c87ffa0847411c5a993cf152526458b65a906f2
Brandon Liu <liubrandon@google.com>
2020-06-19 16:10:33 -0700

    Switch hpcc sed delimiter to pipe.
    PiperOrigin-RevId: 317398455

--- perfkitbenchmarker/linux_packages/hpcc.py
+++ perfkitbenchmarker/linux_packages/hpcc.py
@@ -288,11 +288,11 @@ def _CompileHpccOpenblas(vm):
   vm.RemoteCommand(
       'cp %s/hpl/setup/%s %s' %
       (HPCC_DIR, HPCC_MAKEFILE_CBLAS, HPCC_MAKEFILE_PATH_OPEN_BLAS))
-  sed_cmd = ('sed -i -e "/^MP/d" -e "s/gcc/mpicc/" -e "s/g77/mpicc/" '
-             '-e "s/\\$(HOME)\\/netlib\\/ARCHIVES\\/Linux_PII/%s/" '
-             '-e "s/libcblas.*/libopenblas.a/" '
-             '-e "s/-funroll-loops/-funroll-loops -std=c99/" '
-             '-e "s/\\-lm/\\-lgfortran \\-lm/" %s' %
+  sed_cmd = ('sed -i -e "/^MP/d" -e "s|gcc|mpicc|" -e "s|g77|mpicc|" '
+             '-e "s|\\$(HOME)/netlib/ARCHIVES/Linux_PII|%s|" '
+             '-e "s|libcblas.*|libopenblas.a|" '
+             '-e "s|-funroll-loops|-funroll-loops -std=c99|" '
+             '-e "s|\\-lm|\\-lgfortran \\-lm|" %s' %
              (re.escape(openblas.OPENBLAS_DIR), HPCC_MAKEFILE_PATH_OPEN_BLAS))
   vm.RemoteCommand(sed_cmd)
   vm.RemoteCommand('cd %s; make arch=OPEN_BLAS' % HPCC_DIR)
@@ -303,11 +303,11 @@ def _CompileHpccAmdBlis(vm):
   vm.Install('amdblis')
   vm.RemoteCommand('cp %s/hpl/setup/%s %s' %
                    (HPCC_DIR, HPCC_MAKEFILE_CBLAS, HPCC_MAKEFILE_PATH_AMD_BLIS))
-  sed_cmd = ('sed -i -e "/^MP/d" -e "s/gcc/mpicc/" -e "s/g77/mpicc/" '
-             '-e "s/\\$(HOME)\\/netlib\\/ARCHIVES\\/Linux_PII/%s/" '
-             '-e "s/libcblas.*/lib\\/zen\\/libblis.a/" '
-             '-e "s/-funroll-loops/-funroll-loops -std=c99/" '
-             '-e "s/\\-lm/\\-lgfortran \\-lm/" %s' %
+  sed_cmd = ('sed -i -e "/^MP/d" -e "s|gcc|mpicc|" -e "s|g77|mpicc|" '
+             '-e "s|\\$(HOME)/netlib/ARCHIVES/Linux_PII|%s|" '
+             '-e "s|libcblas.*|lib/zen/libblis.a|" '
+             '-e "s|-funroll-loops|-funroll-loops -std=c99|" '
+             '-e "s|\\-lm|\\-lgfortran \\-lm|" %s' %
              (re.escape(amdblis.AMDBLIS_DIR), HPCC_MAKEFILE_PATH_AMD_BLIS))
   vm.RemoteCommand(sed_cmd)
   vm.RemoteCommand('cd %s; make arch=AMD_BLIS' % HPCC_DIR)
@@ -342,12 +342,12 @@ def _CompileHpccMKL(vm):
   mkl_ccflags = (' -Wl,--no-as-needed -ldl -lmpi -liomp5 -lpthread -lm '
                  '-DUSING_FFTW -DMKL_INT=long -DLONG_IS_64BITS')
   sed_cmd_mkl = (
-      'sed -i -e "/^MP/d" -e "s/gcc/mpicc/" -e "s/g77/mpicc/" '
-      '-e "s/\\$(HOME)\\/netlib\\/ARCHIVES\\/Linux_PII/'
-      '\\/opt\\/intel\\/mkl\\/lib\\/intel64/" '
-      '-e "s/\\$(LAdir)\\/libcblas.*/%s/" '
-      '-e "s/\\-lm/\\-lgfortran \\-lm/" '
-      '-e "/CCFLAGS / s/$/%s/" %s' %
+      'sed -i -e "/^MP/d" -e "s|gcc|mpicc|" -e "s|g77|mpicc|" '
+      '-e "s|\\$(HOME)/netlib/ARCHIVES/Linux_PII|'
+      '/opt/intel/mkl/lib/intel64|" '
+      '-e "s|\\$(LAdir)/libcblas.*|%s|" '
+      '-e "s|\\-lm|\\-lgfortran \\-lm|" '
+      '-e "/CCFLAGS / s|$|%s|" %s' %
       (re.escape(mkl_lalib), re.escape(mkl_ccflags), HPCC_MAKEFILE_PATH_MKL))
   vm.RemoteCommand(sed_cmd_mkl)
   vm.RemoteCommand('source /opt/intel/compilers_and_libraries/linux/bin/'
