29dbcfcf67cb439998ae641c6953ec75a16d6e76
David Lott <dlott@google.com>
2020-07-24 13:22:57 -0700

    Linter fixes
    PiperOrigin-RevId: 323060117

--- tests/benchmark_spec_test.py
+++ tests/benchmark_spec_test.py
@@ -14,12 +14,12 @@
 """Tests for perfkitbenchmarker.benchmark_spec."""
 
 import unittest
+from absl import flags
 import mock
 
 from perfkitbenchmarker import benchmark_spec
 from perfkitbenchmarker import configs
 from perfkitbenchmarker import context
-from perfkitbenchmarker import flags
 from perfkitbenchmarker import linux_benchmarks
 from perfkitbenchmarker import pkb  # pylint: disable=unused-import # noqa
 from perfkitbenchmarker import providers
