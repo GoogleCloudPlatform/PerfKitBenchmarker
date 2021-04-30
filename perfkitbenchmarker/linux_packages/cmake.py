1a453e55e87360256e19ff9d0cad00c310a50cf8
Chris Wilkes <chriswilkes@google.com>
2021-04-30 14:07:39 -0700

    Provide --cmake_kitware flag for debian installs of cmake from the Kitware site.
    PiperOrigin-RevId: 371402280

--- perfkitbenchmarker/linux_packages/cmake.py
+++ perfkitbenchmarker/linux_packages/cmake.py
@@ -1,4 +1,4 @@
-# Copyright 2016 PerfKitBenchmarker Authors. All rights reserved.
+# Copyright 2021 PerfKitBenchmarker Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,20 +11,52 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Module containing cmake installation and cleanup functions."""
+
+import logging
+from absl import flags
+from perfkitbenchmarker import os_types
+
+_CMAKE_KITWARE = flags.DEFINE_bool(
+    'cmake_kitware', False,
+    'Whether to install cmake from the Kitware repo. Default is to install '
+    'from the (possibily outdated) OS distro.')
+
+# Needed to configure the kitware debian repo
+_UBUNTU_VERSION_NAMES = {
+    os_types.UBUNTU1604: 'xenial',
+    os_types.UBUNTU1804: 'bionic',
+    os_types.UBUNTU2004: 'focal',
+}
+_KITWARE_KEY_URL = 'https://apt.kitware.com/keys/kitware-archive-latest.asc'
+_KITWARE_DEB_CONFIG = 'deb https://apt.kitware.com/ubuntu/ {version} main'
 
 
 def _Install(vm):
   """Installs the cmake package on the VM."""
   vm.InstallPackages('cmake')
+  text, _ = vm.RemoteCommand('cmake --version')
+  logging.info('Installed cmake version %s', text.splitlines()[0].split()[-1])
 
 
 def YumInstall(vm):
   """Installs the cmake package on the VM."""
+  if _CMAKE_KITWARE.value:
+    raise ValueError('Installing cmake from kitware only supported via apt.')
   _Install(vm)
 
 
 def AptInstall(vm):
   """Installs the cmake package on the VM."""
+  if _CMAKE_KITWARE.value:
+    _AddCmakeAptRepo(vm)
   _Install(vm)
+
+
+def _AddCmakeAptRepo(vm):
+  vm.Install('curl')
+  vm.RemoteCommand(f'curl --silent {_KITWARE_KEY_URL} | gpg --dearmor | '
+                   'sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null')
+  repo = _KITWARE_DEB_CONFIG.format(version=_UBUNTU_VERSION_NAMES[vm.OS_TYPE])
+  vm.RemoteCommand(f'sudo apt-add-repository "{repo}"')
+  vm.AptUpdate()
---
+++ tests/linux_packages/cmake_test.py
@@ -0,0 +1,47 @@
+"""Test for linux_packages/cmake.py."""
+
+import unittest
+from absl.testing import flagsaver
+import mock
+from perfkitbenchmarker.linux_packages import cmake
+from tests import pkb_common_test_case
+
+_CMAKE_VERSION = 'mock123'
+
+
+def MockVm(os_type):
+  vm = mock.Mock()
+  vm.OS_TYPE = os_type
+  # response to 'cmake --version'
+  vm.RemoteCommand.return_value = _CMAKE_VERSION, ''
+  return vm
+
+
+class CmakeTests(pkb_common_test_case.PkbCommonTestCase):
+
+  @flagsaver.flagsaver(cmake_kitware=True)
+  def testAptInstallViaKitware(self):
+    vm = MockVm('ubuntu1604')
+    cmake.AptInstall(vm)
+    expected_cmds = [
+        'curl --silent https://apt.kitware.com/keys/kitware-archive-latest.asc '
+        '| gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg '
+        '>/dev/null', 'sudo apt-add-repository '
+        '"deb https://apt.kitware.com/ubuntu/ xenial main"'
+    ]
+    vm.RemoteCommand.assert_has_calls([mock.call(cmd) for cmd in expected_cmds])
+
+  @flagsaver.flagsaver(cmake_kitware=True)
+  def testYumInstallViaKitware(self):
+    vm = MockVm('centos7')
+    with self.assertRaises(ValueError):
+      cmake.YumInstall(vm)
+
+  def testYumNonKitware(self):
+    vm = MockVm('centos7')
+    # no exceptions thrown
+    cmake.YumInstall(vm)
+
+
+if __name__ == '__main__':
+  unittest.main()
