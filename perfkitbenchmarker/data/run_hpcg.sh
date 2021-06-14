ed1a7d5dcd34b0ac73ddc650f772e0eae588914b
Hao Wu <tohaowu@google.com>
2021-06-14 10:47:06 -0700

    Add AWS OFI NCCL plugin and Azure NCCL RDMA SHARP plugins in HPCG test.
    PiperOrigin-RevId: 379309934

--- perfkitbenchmarker/data/run_hpcg.sh
+++ perfkitbenchmarker/data/run_hpcg.sh
@@ -21,7 +21,8 @@ DATETIME=`hostname`.`date +"%Y%m%d.%H%M%S"`
 # Just to get rid of warning on psg cluster node wo proper IB sw installed
 # Use mca btl_tcp_if_exclude to skip docker network in DLVM.
 MPIFLAGS="--mca btl tcp,self --mca btl_tcp_if_exclude docker0,lo"
+LD_LIBRARY_PATH_FLAGS="-x LD_LIBRARY_PATH=/usr/local/cuda/lib64:/opt/amazon/efa/lib64:/opt/amazon/openmpi/lib64:/opt/aws-ofi-nccl/lib:/usr/local/nccl-rdma-sharp-plugins/lib:$LD_LIBRARY_PATH"
 HPCG_BIN="hpcg"
 
 echo " ****** running HPCG 9-3-20 binary=$HPCG_BIN on $NUM_GPUS GPUs ***************************** "
-mpirun {{ ALLOW_RUN_AS_ROOT }} -np $NUM_GPUS $MPIFLAGS -hostfile HOSTFILE $HPCG_DIR/$HPCG_BIN | tee ./results/xhpcg-$DATETIME-output.txt
+mpirun {{ ALLOW_RUN_AS_ROOT }} -np $NUM_GPUS $MPIFLAGS -hostfile HOSTFILE $LD_LIBRARY_PATH_FLAGS $HPCG_DIR/$HPCG_BIN | tee ./results/xhpcg-$DATETIME-output.txt
