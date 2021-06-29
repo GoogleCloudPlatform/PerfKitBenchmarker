d6ccce61458e7bbb0562d214b18b7bcb9f2f1887
p3rf Team <p3rf@google.com>
2021-06-29 13:47:55 -0700

    GCP Cloud PubSub Benchmark.
    PiperOrigin-RevId: 382157553

--- perfkitbenchmarker/data/messaging_service/messaging_service_client.py
+++ perfkitbenchmarker/data/messaging_service/messaging_service_client.py
@@ -3,13 +3,22 @@
 This generic interface creates the base class that we need to benchmark
 different messaging services.
 """
-
+# pylint: disable=broad-except
 import abc
 import json
+import logging
+import random
+import string
+import time
 from typing import Any, Dict, List
 
 from absl import flags
 import numpy as np
+
+TIMEOUT = 10
+MESSAGE_CHARACTERS = string.ascii_letters + string.digits
+GET_TIME_IN_MILLISECONDS = lambda: time.time() * 1000
+UNIT_OF_TIME = 'milliseconds'
 
 FLAGS = flags.FLAGS
 
@@ -34,8 +43,8 @@ class MessagingServiceClient:
   own implementation (gcp_pubsub_client.py).
   """
 
-  def _GetSummaryStatistics(self, scenario: str, results: List[float],
-                            number_of_messages: int) -> Dict[str, Any]:
+  def _get_summary_statistics(self, scenario: str, results: List[float],
+                              number_of_messages: int) -> Dict[str, Any]:
     """Getting statistics based on results from the benchmark."""
     metrics_data = {}
     common_metadata = {}
@@ -46,27 +55,27 @@ class MessagingServiceClient:
 
     metrics_data[scenario + '_mean'] = {
         'value': latency_mean,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {'samples': results}
     }
     metrics_data[scenario + '_mean_without_cold_start'] = {
         'value': latency_mean_without_cold_start,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': common_metadata
     }
     metrics_data[scenario + '_p50'] = {
         'value': np.percentile(results, 50),
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': common_metadata
     }
     metrics_data[scenario + '_p99'] = {
         'value': np.percentile(results, 99),
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': common_metadata
     }
     metrics_data[scenario + '_p99_9'] = {
         'value': np.percentile(results, 99.9),
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': common_metadata
     }
     metrics_data[scenario + '_percentage_received'] = {
@@ -76,62 +85,62 @@ class MessagingServiceClient:
     }
     return metrics_data
 
-  @abc.abstractmethod
-  def PublishMessages(
-      self,
-      number_of_messages: int,
-      message_size: int) -> None:
-    """Publish messages on messaging service and measure latency.
+  def _generate_random_message(self, message_size: int) -> bytes:
+    message = ''.join(
+        random.choice(MESSAGE_CHARACTERS) for _ in range(message_size))
+    return message.encode('utf-8')
 
-    This function attempts to publish messages. It measures the latency
+  @abc.abstractmethod
+  def _publish_message(self, message_payload: bytes) -> Any:
+    """Publishes a single message to the messaging service.
+
+    Args:
+      message_payload: Message in bytes, created by '_generate_random_message'.
+      This message will be the one that we publish/pull from the messaging
+      service.
+
+    Returns:
+      Return response to publish a message from the provider. For GCP PubSub
+      we make a call to '.publish' that publishes the message and we return
+      the result from this call.
+    """
+
+  @abc.abstractmethod
+  def _pull_message(self) -> Any:
+    """Pulls a single message from the messaging service.
+
+    Returns:
+      Return response to pull a message from the provider. For GCP PubSub
+      we make a call to '.pull' that pulls a message and we return
+      the result from this call.
+    """
+
+  @abc.abstractmethod
+  def _acknowledges_received_message(self, response: Any) -> None:
+    """Acknowledges that the pulled message was received.
+
+    It tries to acknowledge the message that was pulled with _pull_message.
+
+    Args:
+      response: Response from _pull_message.
+    """
+
+  def measure_publish_and_pull_latency(self, number_of_messages: int,
+                                       message_size: int) -> Dict[str, Any]:
+    """Handle measurements of latency to publish and then pull messages.
+
+    This function publish messages, and then pull them. It measures the latency
     between a call to publish the message, and the message being successfully
     published. We wait for the publish message call to be completed (with a
     blocking call) before attempting to publish the next message. When the
     publish message call is completed the message was successfully published.
-
-    Args:
-      number_of_messages: Number of messages to publish.
-      message_size: Size of the messages that are being published.
-        It specifies the number of characters in those messages.
-    """
-
-  @abc.abstractmethod
-  def PullMessages(
-      self,
-      number_of_messages: int) -> None:
-    """Pull messages from messaging service and measure latency.
-
-    This function pulls messages from the messaging service. It measures the
-    latency between a call to pull the message, and the message being
-    successfully received on the client VM. We wait for a message to be pulled
-    before attempting to pull the next one (blocking call).
-
-    Args:
-      number_of_messages: Number of messages to pull.
-    """
-
-  @abc.abstractmethod
-  def ConsecutivePublishPullMessages(self, number_of_messages: int,
-                                     message_size: int):
-    """Publish and pull message, measuring end to end latency.
-
-    This function publishes a message and then it tries to pull it as soon as
-    it's available to be pulled. It measures the latency between a call to
-    publish the message and pulling the message - both operations are done
-    sequentially by the same client VM.
-
-    Args:
-      number_of_messages: Number of messages to publish and pull.
-      message_size: Size of the messages that are published/pulled. It specifies
-        the number of characters in those messages.
-    """
-
-  def MeasurePublishAndPullLatency(self, number_of_messages: int,
-                                   message_size: int) -> Dict[str, Any]:
-    """Handle measurements of latency to pull messages.
-
-    This function calls the cloud specific implementation to pull messages
-    and it creates a dictionary containing all the results.
+    After "number_of_messages" messages were published we try to pull them
+    from the messaging service. It measures the latency between a call to pull
+    the message, and the message being successfully received on the client VM.
+    We wait for a message to be pulled before attempting to pull the next one
+    (blocking call). If a publish (or pull) message fails we will keep trying to
+    publish 'number_of_messages' messages, we are generating a stat for success
+    rate that shows the percentage of requests that didn't failed.
 
     Args:
       number_of_messages: Number of messages to publish and pull.
@@ -146,36 +155,62 @@ class MessagingServiceClient:
           ...
         }
     """
+    publish_latencies = []
+    pull_latencies = []
+    acknowledge_latencies = []
 
     # publishing 'number_of_messages' messages
-    publish_latencies = self.PublishMessages(
-        number_of_messages,
-        message_size)
+    for _ in range(number_of_messages):
+      message_payload = self._generate_random_message(message_size)
+      start_time = GET_TIME_IN_MILLISECONDS()
+      # Publishing a message and waiting for completion
+      try:
+        self._publish_message(message_payload)
+        end_time = GET_TIME_IN_MILLISECONDS()
+        publish_latencies.append(end_time - start_time)
+      except Exception as e:
+        logging.warning('Failed to publish message: %r', e)
 
-    # pulling 'number_of_messages' messages
-    pull_latencies = self.PullMessages(number_of_messages)
+    # attempt to pull 'number_of_messages' messages
+    for _ in range(number_of_messages):
+      start_time = GET_TIME_IN_MILLISECONDS()
+      try:
+        response = self._pull_message()
+        end_time = GET_TIME_IN_MILLISECONDS()
+        pull_latencies.append(end_time - start_time)
+        self._acknowledges_received_message(response)
+        end_time = GET_TIME_IN_MILLISECONDS()
+        acknowledge_latencies.append(end_time - start_time)
+      except Exception as e:
+        logging.warning('Failed to pull and acknowledge message: %r', e)
 
-    # getting metrics and adding to 'data' Dict
-    publish_metrics = self._GetSummaryStatistics('publish_latency',
-                                                 publish_latencies,
-                                                 number_of_messages)
-    pull_metrics = self._GetSummaryStatistics('pull_latency', pull_latencies,
-                                              number_of_messages)
+    # getting metrics for publish, pull, and acknowledge latencies
+    publish_metrics = self._get_summary_statistics('publish_latency',
+                                                   publish_latencies,
+                                                   number_of_messages)
+    pull_metrics = self._get_summary_statistics('pull_latency', pull_latencies,
+                                                number_of_messages)
+
+    acknowledge_metrics = self._get_summary_statistics(
+        'pull_and_acknowledge_latency', acknowledge_latencies,
+        number_of_messages)
+
     # merging dictionaries
-    publish_metrics.update(pull_metrics)
+    metrics = {**publish_metrics, **pull_metrics, **acknowledge_metrics}
 
-    print(json.dumps(publish_metrics))
-    return publish_metrics
+    print(json.dumps(metrics))
+    return metrics
 
-  def MeasureEndToEndLatency(self, number_of_messages: int,
-                             message_size: int) -> Dict[str, Any]:
+  def measure_end_to_end_latency(self, number_of_messages: int,
+                                 message_size: int) -> Dict[str, Any]:
     """Measures end to end latency.
 
     Latency to publish and pull message.
-
-    This function calls the cloud specific implementation to publish and pull
-    messages - right after they're available to be pulled - and it creates a
-    dictionary containing all the results.
+    This function publishes a single message and then it tries to pull it as
+    soon as it's available to be pulled. It measures the latency between a call
+    to publish the message and pulling the message - both operations are done
+    sequentially by the same client VM. It uses the _publish_message and
+    _pull_message cloud specific implementations.
 
     Args:
       number_of_messages: Number of messages to publish and pull.
@@ -191,25 +226,43 @@ class MessagingServiceClient:
         }
     """
     # TODO(user): revisit if we really want to measure EndToEndLatency.
+    end_to_end_latencies = []
+    end_to_end_acknowledge_latencies = []
 
-    # publishing and pulling 'number_of_messages' messages
-    # and measuring their latency | run phase
-    end_to_end_latencies = self.ConsecutivePublishPullMessages(
-        number_of_messages,
-        message_size)
+    # publishing and then pulling a single message
+    for _ in range(number_of_messages):
+      message_payload = self._generate_random_message(message_size)
+      start_time = GET_TIME_IN_MILLISECONDS()
+      try:
+        # Publishing a message and waiting for completion
+        self._publish_message(message_payload)
+        # Pulling the message and waiting for completion
+        response = self._pull_message()
+        end_time = GET_TIME_IN_MILLISECONDS()
+        end_to_end_latencies.append(end_time - start_time)
+        self._acknowledges_received_message(response)
+        end_time = GET_TIME_IN_MILLISECONDS()
+        end_to_end_acknowledge_latencies.append(end_time - start_time)
+      except Exception as e:
+        logging.warning('Failed to publish and then pull message: %r', e)
+        raise Exception(e)
 
-    # getting metrics and adding to our 'data' Dict
-    end_to_end_metrics = self._GetSummaryStatistics('end_to_end_latency',
-                                                    end_to_end_latencies,
-                                                    number_of_messages)
+    # getting end_to_end metrics: (publish + pull), and (publish + pull + ack)
+    end_to_end_metrics = self._get_summary_statistics('end_to_end_latency',
+                                                      end_to_end_latencies,
+                                                      number_of_messages)
+    acknowledge_metrics = self._get_summary_statistics(
+        'end_to_end_acknowledge_latency', end_to_end_acknowledge_latencies,
+        number_of_messages)
 
-    print(json.dumps(end_to_end_metrics))
-    return end_to_end_metrics
+    # merging dictionaries
+    metrics = {**end_to_end_metrics, **acknowledge_metrics}
 
-  def RunPhase(self,
-               benchmark_scenario: str,
-               number_of_messages: int,
-               message_size: int) -> Dict[str, Any]:
+    print(json.dumps(metrics))
+    return metrics
+
+  def run_phase(self, benchmark_scenario: str, number_of_messages: int,
+                message_size: int) -> Dict[str, Any]:
     """Runs a given benchmark based on the benchmark_messaging_service Flag.
 
     Args:
@@ -228,6 +281,7 @@ class MessagingServiceClient:
         }
     """
     if benchmark_scenario == 'pull_latency':
-      return self.MeasurePublishAndPullLatency(number_of_messages, message_size)
+      return self.measure_publish_and_pull_latency(number_of_messages,
+                                                   message_size)
     elif benchmark_scenario == 'end_to_end_latency':
-      return self.MeasureEndToEndLatency(number_of_messages, message_size)
+      return self.measure_end_to_end_latency(number_of_messages, message_size)
--- tests/messaging_service_client_test.py
+++ tests/messaging_service_client_test.py
@@ -12,6 +12,7 @@ from perfkitbenchmarker.data.messaging_service.messaging_service_client import M
 FAKE_DATETIME = datetime.datetime(2021, 6, 14)
 NUMBER_OF_MESSAGES = 10
 MESSAGE_SIZE = 10
+UNIT_OF_TIME = 'milliseconds'
 
 METRICS = [
     0.20989608764648438, 0.2431643009185791, 0.14051604270935059,
@@ -23,29 +24,29 @@ METRICS = [
 AGGREGATE_PUBLISH_METRICS = {
     'publish_latency_mean': {
         'value': 0.11147687435150147,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {
             'samples': METRICS
         }
     },
     'publish_latency_mean_without_cold_start': {
         'value': 0.06490101814270019,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {}
     },
     'publish_latency_p50': {
         'value': 0.0983436107635498,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {}
     },
     'publish_latency_p99': {
         'value': 0.2401701617240906,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {}
     },
     'publish_latency_p99_9': {
         'value': 0.2428648869991303,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {}
     },
     'publish_latency_percentage_received': {
@@ -58,28 +59,28 @@ AGGREGATE_PUBLISH_METRICS = {
 AGGREGATE_PULL_METRICS = {
     'pull_latency_mean': {
         'value': 0.11147687435150147,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {
             'samples': METRICS}
     },
     'pull_latency_mean_without_cold_start': {
         'value': 0.06490101814270019,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {}
     },
     'pull_latency_p50': {
         'value': 0.0983436107635498,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {}
     },
     'pull_latency_p99': {
         'value': 0.2401701617240906,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {}
     },
     'pull_latency_p99_9': {
         'value': 0.2428648869991303,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {}
     },
     'pull_latency_percentage_received': {
@@ -92,29 +93,29 @@ AGGREGATE_PULL_METRICS = {
 AGGREGATE_END_TO_END_METRICS = {
     'end_to_end_latency_mean': {
         'value': 0.11147687435150147,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {
             'samples': METRICS
         }
     },
     'end_to_end_latency_mean_without_cold_start': {
         'value': 0.06490101814270019,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {}
     },
     'end_to_end_latency_p50': {
         'value': 0.0983436107635498,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {}
     },
     'end_to_end_latency_p99': {
         'value': 0.2401701617240906,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {}
     },
     'end_to_end_latency_p99_9': {
         'value': 0.2428648869991303,
-        'unit': 'seconds',
+        'unit': UNIT_OF_TIME,
         'metadata': {}
     },
     'end_to_end_latency_percentage_received': {
@@ -137,7 +138,7 @@ class MessagingServiceClientTest(parameterized.TestCase):
       ('pull', 'pull_latency', AGGREGATE_PULL_METRICS),
       ('end_to_end', 'end_to_end_latency', AGGREGATE_END_TO_END_METRICS))
   def testGetSummaryStatistics(self, scenario, expected_samples):
-    actual_samples = self.messaging_service._GetSummaryStatistics(
+    actual_samples = self.messaging_service._get_summary_statistics(
         scenario, METRICS, NUMBER_OF_MESSAGES)
 
     for expected_sample_key in expected_samples:
@@ -149,20 +150,28 @@ class MessagingServiceClientTest(parameterized.TestCase):
       elif expected_samples[expected_sample_key] != actual_samples[
           expected_sample_key]:
         sample_doesnt_match_message = (
-            f"Expected sample:\n{expected_sample_key}\ndoesn't match actual sample:"
+            f"Expected sample:\n{expected_samples[expected_sample_key]}\ndoesn't match actual sample:"
             f'\n{actual_samples[expected_sample_key]}')
         raise Exception(sample_doesnt_match_message)
 
-  @mock.patch.object(MessagingServiceClient, 'PublishMessages')
-  @mock.patch.object(MessagingServiceClient, 'PullMessages')
+  def testGenerateRandomMessage(self):
+    random_message = self.messaging_service._generate_random_message(
+        MESSAGE_SIZE)
+    self.assertLen(random_message, MESSAGE_SIZE)
+    self.assertIsInstance(random_message, bytes)
+
+  @mock.patch.object(MessagingServiceClient, '_publish_message')
+  @mock.patch.object(MessagingServiceClient, '_pull_message')
+  @mock.patch.object(MessagingServiceClient, '_acknowledges_received_message')
   @mock.patch.object(
       MessagingServiceClient,
-      '_GetSummaryStatistics',
+      '_get_summary_statistics',
       return_value={'mocked_dict': 'mocked_value'})
-  def testMeasurePublishAndPullLatency(self, publish_mock, pull_mock,
-                                       summary_statistics_mock):
+  def testMeasurePublishAndPullLatency(self, summary_statistics_mock,
+                                       acknowledge_message_mock, pull_mock,
+                                       publish_mock):
 
-    results = self.messaging_service.MeasurePublishAndPullLatency(
+    results = self.messaging_service.measure_publish_and_pull_latency(
         NUMBER_OF_MESSAGES, MESSAGE_SIZE)
 
     self.assertIsInstance(results, dict)
@@ -170,42 +179,87 @@ class MessagingServiceClientTest(parameterized.TestCase):
     # check if functions were called
     publish_mock.assert_called()
     pull_mock.assert_called()
+    acknowledge_message_mock.assert_called()
     summary_statistics_mock.assert_called()
+    self.assertEqual(publish_mock.call_count, NUMBER_OF_MESSAGES)
+    self.assertEqual(pull_mock.call_count, NUMBER_OF_MESSAGES)
+    self.assertEqual(acknowledge_message_mock.call_count, NUMBER_OF_MESSAGES)
 
-  @mock.patch.object(MessagingServiceClient, 'ConsecutivePublishPullMessages')
   @mock.patch.object(
       MessagingServiceClient,
-      '_GetSummaryStatistics',
+      '_publish_message',
+      side_effect=Exception('MockedException'))
+  @mock.patch.object(
+      MessagingServiceClient,
+      '_pull_message',
+      side_effect=Exception('MockedException'))
+  @mock.patch.object(
+      MessagingServiceClient,
+      '_acknowledges_received_message',
+      side_effect=Exception('MockedException'))
+  @mock.patch.object(
+      MessagingServiceClient,
+      '_get_summary_statistics',
       return_value={'mocked_dict': 'mocked_value'})
-  def testMeasureEndToEndLatency(self, publish_and_pull_mock,
-                                 summary_statistics_mock):
+  def testMeasurePublishAndPullLatencyException(self, summary_statistics_mock,
+                                                _, pull_mock, publish_mock):
 
-    results = self.messaging_service.MeasureEndToEndLatency(
+    results = self.messaging_service.measure_publish_and_pull_latency(
+        NUMBER_OF_MESSAGES, MESSAGE_SIZE)
+    self.assertEqual(results, {'mocked_dict': 'mocked_value'})
+
+    # check if functions were called
+    publish_mock.assert_called()
+    pull_mock.assert_called()
+    summary_statistics_mock.assert_called()
+
+  @mock.patch.object(MessagingServiceClient, '_publish_message')
+  @mock.patch.object(MessagingServiceClient, '_pull_message')
+  @mock.patch.object(MessagingServiceClient, '_acknowledges_received_message')
+  @mock.patch.object(
+      MessagingServiceClient,
+      '_get_summary_statistics',
+      return_value={'mocked_dict': 'mocked_value'})
+  def testMeasureEndToEndLatency(self, summary_statistics_mock,
+                                 acknowledge_message_mock, pull_mock,
+                                 publish_mock):
+
+    results = self.messaging_service.measure_end_to_end_latency(
         NUMBER_OF_MESSAGES, MESSAGE_SIZE)
 
     self.assertIsInstance(results, dict)
 
     # check if functions were called
-    publish_and_pull_mock.assert_called()
+    publish_mock.assert_called()
+    pull_mock.assert_called()
+    acknowledge_message_mock.assert_called()
     summary_statistics_mock.assert_called()
+    self.assertEqual(publish_mock.call_count, NUMBER_OF_MESSAGES)
+    self.assertEqual(pull_mock.call_count, NUMBER_OF_MESSAGES)
+    self.assertEqual(acknowledge_message_mock.call_count, NUMBER_OF_MESSAGES)
 
-  @mock.patch.object(MessagingServiceClient, 'MeasurePublishAndPullLatency')
+  @mock.patch.object(
+      MessagingServiceClient,
+      '_publish_message',
+      side_effect=Exception('MockedException'))
+  def testMeasureEndToEndLatencyException(self, _):
+    self.assertRaises(Exception,
+                      self.messaging_service.measure_end_to_end_latency,
+                      NUMBER_OF_MESSAGES, MESSAGE_SIZE)
+
+  @mock.patch.object(MessagingServiceClient, 'measure_publish_and_pull_latency')
   def testRunPhasePullScenario(self, pull_mock):
-
-    self.messaging_service.RunPhase(
+    self.messaging_service.run_phase(
         'pull_latency', NUMBER_OF_MESSAGES, MESSAGE_SIZE)
 
-    # check if functions were called
-    pull_mock.assert_called()
+    pull_mock.assert_called_with(NUMBER_OF_MESSAGES, MESSAGE_SIZE)
 
-  @mock.patch.object(MessagingServiceClient, 'MeasureEndToEndLatency')
+  @mock.patch.object(MessagingServiceClient, 'measure_end_to_end_latency')
   def testRunPhaseEndToEndScenario(self, end_to_end_mock):
-
-    self.messaging_service.RunPhase(
+    self.messaging_service.run_phase(
         'end_to_end_latency', NUMBER_OF_MESSAGES, MESSAGE_SIZE)
 
-    # check if functions were called
-    end_to_end_mock.assert_called()
+    end_to_end_mock.assert_called_with(NUMBER_OF_MESSAGES, MESSAGE_SIZE)
 
 
 if __name__ == '__main__':
