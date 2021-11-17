d47428503657a60a7834ccd9822023b58889c780
Rui Wang <ruwa@google.com>
2021-11-16 16:12:09 -0800

    Adding imulate local ssd flag to redis memtier benchmark.
    PiperOrigin-RevId: 410376089

--- perfkitbenchmarker/linux_benchmarks/redis_memtier_benchmark.py
+++ perfkitbenchmarker/linux_benchmarks/redis_memtier_benchmark.py
@@ -22,6 +22,7 @@ memtier_benchmark homepage: https://github.com/RedisLabs/memtier_benchmark
 """
 
 import datetime
+import logging
 from typing import Any, Dict, List
 
 from absl import flags
@@ -38,6 +39,9 @@ flags.DEFINE_string('redis_memtier_client_machine_type', None,
                     'If provided, overrides the memtier client machine type.')
 flags.DEFINE_string('redis_memtier_server_machine_type', None,
                     'If provided, overrides the redis server machine type.')
+REDIS_MEMTIER_SIMULATE_DISK = flags.DEFINE_bool(
+    'redis_memtier_simulate_disk', False, 'If true, simulate usage of '
+    'disks on the server by filling any scratch disks, if present. ')
 BENCHMARK_NAME = 'redis_memtier'
 BENCHMARK_CONFIG = """
 redis_memtier:
@@ -48,6 +52,7 @@ redis_memtier:
     servers:
       vm_spec: *default_dual_core
       vm_count: 1
+      disk_spec: *default_50_gb
     clients:
       vm_spec: *default_dual_core
       vm_count: 1
@@ -81,6 +86,8 @@ def GetConfig(user_config: Dict[str, Any]) -> Dict[str, Any]:
     for cloud in vm_spec:
       vm_spec[cloud]['machine_type'] = (
           FLAGS.redis_memtier_server_machine_type)
+  if not REDIS_MEMTIER_SIMULATE_DISK.value:
+    config['vm_groups']['servers'].pop('disk_spec')
   return config
 
 
@@ -102,6 +109,17 @@ def Prepare(bm_spec: _BenchmarkSpec) -> None:
   redis_server.Start(server_vm)
   for port in redis_server.GetRedisPorts():
     memtier.Load(server_vm, 'localhost', str(port))
+
+  if REDIS_MEMTIER_SIMULATE_DISK.value:
+    server_vm.InstallPackages('fio')
+    for disk in server_vm.scratch_disks:
+      cmd = ('sudo fio --name=global --direct=1 --ioengine=libaio --numjobs=1 '
+             '--refill_buffers --scramble_buffers=1 --allow_mounted_write=1 '
+             '--blocksize=128k --rw=write --iodepth=64 --size=100% '
+             f'--name=wipc --filename={disk.device_path}')
+      logging.info('Start filling %s. This may take up to 30min...',
+                   disk.device_path)
+      server_vm.RemoteCommand(cmd)
 
   bm_spec.redis_endpoint_ip = bm_spec.vm_groups['servers'][0].internal_ip
   vm_util.SetupSimulatedMaintenance(server_vm)
