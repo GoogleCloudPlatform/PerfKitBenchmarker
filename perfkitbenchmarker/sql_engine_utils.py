02f0940732949b270ca352e33489a550b3b64d69
Raymond Lam <chunla@google.com>
2022-02-08 13:56:29 -0800

    Increase query timeout to 300 minutes.
    PiperOrigin-RevId: 427285069

--- perfkitbenchmarker/sql_engine_utils.py
+++ perfkitbenchmarker/sql_engine_utils.py
@@ -103,7 +103,7 @@ class ISQLQueryTools(metaclass=abc.ABCMeta):
         query,
         database_name=database_name,
         suppress_stdout=suppress_stdout,
-        timeout=60*200)
+        timeout=60*300)
     end = timeit.default_timer()
     run_time = str(end - start)
     if error_:
