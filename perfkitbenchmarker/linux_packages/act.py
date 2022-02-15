d6e801acb107a7b26de19ae60363e05544af6bc1
Yanting Yu <yuyanting@google.com>
2022-02-15 10:52:51 -0800

    Fix act python interpreter.
    PiperOrigin-RevId: 428823663

--- perfkitbenchmarker/linux_packages/act.py
+++ perfkitbenchmarker/linux_packages/act.py
@@ -134,8 +134,9 @@ def RunAct(vm, load, index=None):
           ACT_DIR, config, output))
   # Shows 1,2,4,8,..,64.
   out, _ = vm.RemoteCommand(
-      'cd {0} ; ./analysis/act_latency.py -n 7 -e 1 -x -l ~/{1}; exit 0'.format(
-          ACT_DIR, output), ignore_failure=True)
+      'cd {0} ; python3 ./analysis/act_latency.py -n 7 -e 1 -x -l ~/{1}; '
+      'exit 0'.format(ACT_DIR, output),
+      ignore_failure=True)
   samples = ParseRunAct(out)
   last_output_block, _ = vm.RemoteCommand('tail -n 100 ~/{0}'.format(output))
 
