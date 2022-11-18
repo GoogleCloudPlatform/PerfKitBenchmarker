e476b6108a01fc0cebe3f62dae2f4a339bd99933
Rui Wang <ruwa@google.com>
2022-11-18 15:59:46 -0800

    Add a redundant disk type check for aws iops assignment.
    PiperOrigin-RevId: 489581130

--- perfkitbenchmarker/providers/aws/aws_virtual_machine.py
+++ perfkitbenchmarker/providers/aws/aws_virtual_machine.py
@@ -247,9 +247,11 @@ def GetBlockDeviceMap(vm):
         ebs_block['VolumeType'] = disk_spec.disk_type
         ebs_block['VolumeSize'] = disk_spec.disk_size
         ebs_block['DeleteOnTermination'] = True
-        if disk_spec.iops:
+        if disk_spec.iops and disk_spec.disk_type in [
+            aws_disk.IO1, aws_disk.IO2, aws_disk.GP3
+        ]:
           ebs_block['Iops'] = disk_spec.iops
-        if disk_spec.throughput:
+        if disk_spec.throughput and disk_spec.disk_type in [aws_disk.GP3]:
           ebs_block['Throughput'] = disk_spec.throughput
         mapping['Ebs'] = ebs_block
       mappings.append(mapping)
