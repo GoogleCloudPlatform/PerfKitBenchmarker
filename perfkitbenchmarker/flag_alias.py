7fe8274d8b8b179a7845ffb480aea07e63782dcd
Brandon Liu <liubrandon@google.com>
2023-02-14 14:15:21 -0800

    Refactor to flatten dict logic in flag_alias.py.
    PiperOrigin-RevId: 509635179

--- perfkitbenchmarker/flag_alias.py
+++ perfkitbenchmarker/flag_alias.py
@@ -55,22 +55,38 @@ ALL_TRANSLATIONS = [DISK_FLAGS_TO_TRANSLATE, RELATIONAL_DB_FLAGS_TO_TRANSLATE]
 PRIOR_ALIAS_REGEX = '(^-?-(?:no)?){0}(=.*|$)'
 
 
+def _FlattenTranslationsDicts(dicts: List[Dict[str, str]]) -> Dict[str, str]:
+  result = {}
+  for translation_dict in dicts:
+    result.update(translation_dict)
+  return result
+
+
 # pylint: disable=dangerous-default-value
 def AliasFlagsFromArgs(
     argv: List[str],
     alias_dict: List[Dict[str, str]] = ALL_TRANSLATIONS) -> List[str]:
   """Alias flags to support backwards compatibility."""
+  original_to_translation = _FlattenTranslationsDicts(alias_dict)
   new_argv = []
   for arg in argv:
-    for alias in alias_dict:
-      for key in alias:
-        regex = PRIOR_ALIAS_REGEX.format(key)
-        if re.match(regex, arg):
-          arg = re.sub(regex, r'\1{0}\2'.format(alias[key]), arg)
-          logging.warning(
-              'The flag %s is deprecated and will be removed in the future. '
-              'Translating to %s for now.', key, alias[key])
-    new_argv.append(arg)
+    result = arg
+    for original in original_to_translation:
+      regex = PRIOR_ALIAS_REGEX.format(original)
+      m = re.match(regex, arg)
+      if not m:
+        continue
+      translation = original_to_translation[original]
+      result = re.sub(regex, r'\1{0}\2'.format(translation), arg)
+      logging.warning(
+          (
+              'The flag %s is deprecated and will be removed in the future.'
+              ' Translating to %s for now.'
+          ),
+          original,
+          translation,
+      )
+    new_argv.append(result)
   return new_argv
 
 
@@ -82,17 +98,21 @@ def AliasFlagsFromYaml(
   """Alias flags to support backwards compatibility."""
   if not config:
     return config
+  original_to_translation = _FlattenTranslationsDicts(alias_dict)
   new_dict = {}
-  for original_flag in config:
-    key = original_flag
-    value = config[original_flag]
-    for alias in alias_dict:
-      for flag in alias:
-        if flag == original_flag:
-          key = alias[flag]
-          logging.warning(
+  for original, value in config.items():
+    if original in original_to_translation:
+      translation = original_to_translation[original]
+      new_dict[translation] = value
+      logging.warning(
+          (
               'The config flag %s is deprecated and will be '
               'removed in the future. '
-              'Translating to %s for now.', flag, alias[flag])
-    new_dict[key] = value
+              'Translating to %s for now.'
+          ),
+          original,
+          translation,
+      )
+    else:
+      new_dict[original] = value
   return new_dict
