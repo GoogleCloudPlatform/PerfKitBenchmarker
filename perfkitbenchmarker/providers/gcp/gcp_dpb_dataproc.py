e176e156c0500a29df9593d1b15687961c65e508
Diego Orellana <odiego@google.com>
2023-02-24 10:22:22 -0800

    Split Dataproc Serverless price list to its own file.
    PiperOrigin-RevId: 512102773

--- perfkitbenchmarker/providers/gcp/gcp_dpb_dataproc.py
+++ perfkitbenchmarker/providers/gcp/gcp_dpb_dataproc.py
@@ -32,6 +32,7 @@ from perfkitbenchmarker import flag_util
 from perfkitbenchmarker import providers
 from perfkitbenchmarker import vm_util
 from perfkitbenchmarker.linux_packages import aws_credentials
+from perfkitbenchmarker.providers.gcp import gcp_dpb_dataproc_serverless_prices
 from perfkitbenchmarker.providers.gcp import gcs
 from perfkitbenchmarker.providers.gcp import util
 
@@ -48,148 +49,6 @@ disk_to_hdfs_map = {
 DATAPROC_FLINK_INIT_SCRIPT = os.path.join('beam', 'flink-init.sh')
 DATAPROC_FLINK_PRESUBMIT_SCRIPT = os.path.join('beam', 'flink-presubmit.sh')
 DATAPROC_FLINK_TRIGGER_SCRIPT = os.path.join('beam', 'flink-trigger.sh')
-
-# Dataproc Serverless Prices
-# See https://cloud.google.com/dataproc-serverless/pricing
-DATAPROC_SERVERLESS_PRICES = {
-    'us-west1': {
-        'usd_per_milli_dcu_sec': 0.06 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.04 / 744 / 3600,
-    },
-    'us-west2': {
-        'usd_per_milli_dcu_sec': 0.072071 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
-    },
-    'us-west3': {
-        'usd_per_milli_dcu_sec': 0.072071 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
-    },
-    'us-west4': {
-        'usd_per_milli_dcu_sec': 0.067572 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
-    },
-    'us-east1': {
-        'usd_per_milli_dcu_sec': 0.06 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.04 / 744 / 3600,
-    },
-    'us-east4': {
-        'usd_per_milli_dcu_sec': 0.067572 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
-    },
-    'us-east5': {
-        'usd_per_milli_dcu_sec': 0.06 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.04 / 744 / 3600,
-    },
-    'us-central1': {
-        'usd_per_milli_dcu_sec': 0.06 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.04 / 744 / 3600,
-    },
-    'us-south1': {
-        'usd_per_milli_dcu_sec': 0.0708 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.0472 / 744 / 3600,
-    },
-    'europe-north1': {
-        'usd_per_milli_dcu_sec': 0.066062 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
-    },
-    'europe-west1': {
-        'usd_per_milli_dcu_sec': 0.066007 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.04 / 744 / 3600,
-    },
-    'europe-west2': {
-        'usd_per_milli_dcu_sec': 0.077307 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
-    },
-    'europe-west3': {
-        'usd_per_milli_dcu_sec': 0.077307 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
-    },
-    'europe-west4': {
-        'usd_per_milli_dcu_sec': 0.066057 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
-    },
-    'europe-west6': {
-        'usd_per_milli_dcu_sec': 0.083955 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.052 / 744 / 3600,
-    },
-    'europe-west8': {
-        'usd_per_milli_dcu_sec': 0.0696 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.0464 / 744 / 3600,
-    },
-    'europe-west9': {
-        'usd_per_milli_dcu_sec': 0.0696 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.0464 / 744 / 3600,
-    },
-    'europe-central2': {
-        'usd_per_milli_dcu_sec': 0.077307 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
-    },
-    'europe-southwest1': {
-        'usd_per_milli_dcu_sec': 0.0708 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.047 / 744 / 3600,
-    },
-    'asia-northeast1': {
-        'usd_per_milli_dcu_sec': 0.076976 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.052 / 744 / 3600,
-    },
-    'asia-northeast2': {
-        'usd_per_milli_dcu_sec': 0.076976 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.052 / 744 / 3600,
-    },
-    'asia-northeast3': {
-        'usd_per_milli_dcu_sec': 0.076976 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.052 / 744 / 3600,
-    },
-    'asia-east1': {
-        'usd_per_milli_dcu_sec': 0.069477 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.04 / 744 / 3600,
-    },
-    'asia-east2': {
-        'usd_per_milli_dcu_sec': 0.083955 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.05 / 744 / 3600,
-    },
-    'asia-south1': {
-        'usd_per_milli_dcu_sec': 0.072071 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
-    },
-    'asia-south2': {
-        'usd_per_milli_dcu_sec': 0.072071 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
-    },
-    'asia-southeast1': {
-        'usd_per_milli_dcu_sec': 0.074015 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
-    },
-    'asia-southeast2': {
-        'usd_per_milli_dcu_sec': 0.080674 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.052 / 744 / 3600,
-    },
-    'australia-southeast1': {
-        'usd_per_milli_dcu_sec': 0.085135 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.054 / 744 / 3600,
-    },
-    'australia-southeast2': {
-        'usd_per_milli_dcu_sec': 0.085135 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.054 / 744 / 3600,
-    },
-    'northamerica-northeast1': {
-        'usd_per_milli_dcu_sec': 0.066057 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
-    },
-    'northamerica-northeast2': {
-        'usd_per_milli_dcu_sec': 0.066057 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
-    },
-    'southamerica-east1': {
-        'usd_per_milli_dcu_sec': 0.095246 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.06 / 744 / 3600,
-    },
-    'southamerica-west1': {
-        'usd_per_milli_dcu_sec': 0.08581 / 1000 / 3600,
-        'usd_per_shuffle_storage_gb_sec': 0.057 / 744 / 3600,
-    },
-}
-# End of Dataproc Serverless Prices
 
 
 class MetricNotReadyError(Exception):
@@ -751,10 +610,16 @@ class GcpDpbDataprocServerless(GcpDpbBaseDataproc):
       return results
 
     # Pricing may vary based on regions. Only some regions available.
-    usd_per_milli_dcu_sec = DATAPROC_SERVERLESS_PRICES.get(
-        self.region, {}).get('usd_per_milli_dcu_sec')
-    usd_per_shuffle_storage_gb_sec = DATAPROC_SERVERLESS_PRICES.get(
-        self.region, {}).get('usd_per_shuffle_storage_gb_sec')
+    usd_per_milli_dcu_sec = (
+        gcp_dpb_dataproc_serverless_prices.DATAPROC_SERVERLESS_PRICES.get(
+            self.region, {}
+        ).get('usd_per_milli_dcu_sec')
+    )
+    usd_per_shuffle_storage_gb_sec = (
+        gcp_dpb_dataproc_serverless_prices.DATAPROC_SERVERLESS_PRICES.get(
+            self.region, {}
+        ).get('usd_per_shuffle_storage_gb_sec')
+    )
     if usd_per_milli_dcu_sec is None or usd_per_shuffle_storage_gb_sec is None:
       return None
     results = FetchBatchResults()
