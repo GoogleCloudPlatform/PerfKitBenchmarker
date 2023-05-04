fae7fbc839dd453f4a991c446d01afc2cfab4ada
p3rf Team <p3rf@google.com>
2023-05-04 14:38:31 -0700

    Retry failed createtable commands for CBT YCSB benchmark and don't ignore errors
    PiperOrigin-RevId: 529518016

--- perfkitbenchmarker/linux_benchmarks/cloud_bigtable_ycsb_benchmark.py
+++ perfkitbenchmarker/linux_benchmarks/cloud_bigtable_ycsb_benchmark.py
@@ -35,6 +35,7 @@ from perfkitbenchmarker import background_tasks
 from perfkitbenchmarker import benchmark_spec as bm_spec
 from perfkitbenchmarker import configs
 from perfkitbenchmarker import data
+from perfkitbenchmarker import errors
 from perfkitbenchmarker import sample
 from perfkitbenchmarker import virtual_machine
 from perfkitbenchmarker import vm_util
@@ -354,25 +355,7 @@ def Prepare(benchmark_spec: bm_spec.BenchmarkSpec) -> None:
   args = [((vm, instance), {}) for vm in vms]
   background_tasks.RunThreaded(_Install, args)
 
-  vm = benchmark_spec.vms[0]
-  splits = ','.join(
-      [
-          f'user{1000 + i * (9999 - 1000) // hbase_ycsb.TABLE_SPLIT_COUNT}'
-          for i in range(hbase_ycsb.TABLE_SPLIT_COUNT)
-      ]
-  )
-  command = [
-      google_cloud_cbt.CBT_BIN,
-      f'-project={FLAGS.project or _GetDefaultProject()}',
-      f'-instance={instance.name}',
-      f'-admin-endpoint={gcp_bigtable.ADMIN_ENDPOINT.value}:443',
-      'createtable',
-      _GetTableName(),
-      # Settings derived from data/hbase/create-ycsb-table.hbaseshell.j2
-      f'families={COLUMN_FAMILY}:maxversions=1',
-      f'splits={splits}',
-  ]
-  vm.RemoteCommand(' '.join(command), ignore_failure=True)
+  _CreateTable(benchmark_spec)
 
 
 def _GetYcsbExecutor(
@@ -488,6 +471,46 @@ def _GenerateLoadKwargs(instance: _Bigtable) -> Dict[str, str]:
     load_kwargs['threads'] = '1'
 
   return load_kwargs
+
+
+@vm_util.Retry()
+def _CreateTable(benchmark_spec: bm_spec.BenchmarkSpec):
+  """Creates a table when not using a static (ie user managed) table.
+
+  If the table is user-managed, this is a no-op.
+
+  Args:
+    benchmark_spec: The benchmark specification. Contains all data that is
+      required to run the benchmark.
+  """
+  instance: _Bigtable = benchmark_spec.non_relational_db
+  vm = benchmark_spec.vms[0]
+  splits = ','.join(
+      [
+          f'user{1000 + i * (9999 - 1000) // hbase_ycsb.TABLE_SPLIT_COUNT}'
+          for i in range(hbase_ycsb.TABLE_SPLIT_COUNT)
+      ]
+  )
+  command = [
+      google_cloud_cbt.CBT_BIN,
+      f'-project={FLAGS.project or _GetDefaultProject()}',
+      f'-instance={instance.name}',
+      f'-admin-endpoint={gcp_bigtable.ADMIN_ENDPOINT.value}:443',
+      'createtable',
+      _GetTableName(),
+      # Settings derived from data/hbase/create-ycsb-table.hbaseshell.j2
+      f'families={COLUMN_FAMILY}:maxversions=1',
+      f'splits={splits}',
+  ]
+
+  try:
+    vm.RemoteCommand(' '.join(command))
+  except errors.VirtualMachine.RemoteCommandError as e:
+    # Expected if --google_bigtable_static_table_name is set. We don't want to
+    # skip table creation in this case because it is convenient to have PKB
+    # create the table on the first run, but skip creation on any repeated runs.
+    if 'AlreadyExists' not in str(e):
+      raise
 
 
 @vm_util.Retry()
