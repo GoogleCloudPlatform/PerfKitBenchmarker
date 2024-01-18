62c2778e197b8641cf996951b19f2bc0e4806d22
p3rf Team <p3rf@google.com>
2024-01-18 11:16:34 -0800

    b/315504413 - Update SQL server update link
    PiperOrigin-RevId: 599566304

--- perfkitbenchmarker/data/relational_db_configs/sqlserver_ha_configs/update_sql_server.ps1
+++ perfkitbenchmarker/data/relational_db_configs/sqlserver_ha_configs/update_sql_server.ps1
@@ -1,6 +1,6 @@
 $scriptsFolder = 'c:\scripts'
-$url = 'https://download.microsoft.com/download/6/e/7/6e72dddf-dfa4-4889-bc3d-e5d3a0fd11ce/SQLServer2019-KB5030333-x64.exe'
-$dest = 'c:\scripts\SQLServer2019-KB5030333-x64.exe'
+$url = 'https://download.microsoft.com/download/6/e/7/6e72dddf-dfa4-4889-bc3d-e5d3a0fd11ce/SQLServer2019-KB5031908-x64.exe'
+$dest = 'c:\scripts\SQLServer2019-KB5031908-x64.exe'
 
 if (-not (Test-Path $scriptsFolder)) {
   New-Item -Path $scriptsFolder  -ItemType directory
@@ -9,4 +9,4 @@ if (-not (Test-Path $scriptsFolder)) {
 # Download the file
 (New-Object System.Net.WebClient).DownloadFile($url, $dest)
 
-c:\scripts\SQLServer2019-KB5030333-x64.exe /q /IAcceptSQLServerLicenseTerms /Action=Patch /InstanceName=MSSQLSERVER
+c:\scripts\SQLServer2019-KB5031908-x64.exe /q /IAcceptSQLServerLicenseTerms /Action=Patch /InstanceName=MSSQLSERVER
