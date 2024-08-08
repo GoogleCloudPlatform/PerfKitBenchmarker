7bf56004ab80957a1a29090420f0f3dc8a5da522
Andy Zhu <andytzhu@google.com>
2024-08-08 11:30:02 -0700

    Add flags dnnl_max_cpu_isa and benchmark_scenario to dlrm_intel_cpu_inference_benchmark.
    PiperOrigin-RevId: 660916109

--- perfkitbenchmarker/linux_benchmarks/dlrm_intel_cpu_inference_benchmark.py
+++ perfkitbenchmarker/linux_benchmarks/dlrm_intel_cpu_inference_benchmark.py
@@ -19,10 +19,33 @@ from typing import Any, Dict
 from absl import flags
 from perfkitbenchmarker import benchmark_spec
 from perfkitbenchmarker import configs
+from perfkitbenchmarker import errors
 from perfkitbenchmarker import regex_util
 from perfkitbenchmarker import sample
 
 FLAGS = flags.FLAGS
+
+_DNNL_MAX_CPU_ISA = flags.DEFINE_string(
+    'dnnl_max_cpu_isa',
+    'AVX512_CORE_AMX',
+    'This limits the processor features ONEDNN is able to detect. Refer to'
+    ' https://oneapi-src.github.io/oneDNN/v2/dev_guide_cpu_dispatcher_control.html'
+    ' for more details. The default options i AVX512_CORE_AMX. Other options'
+    ' include AVX512_CORE, AVX512_CORE_VNNI, and AVX512_CORE_BF16.',
+)
+_BENCHMARK_SCENARIO = flags.DEFINE_string(
+    'dlrm_intel_cpu_benchmark_scenario',
+    'offline',
+    'The benchmark scenario to run. The default value is offline. The other'
+    ' option is server.',
+)
+
+_SERVER_TARGET_QPS = flags.DEFINE_float(
+    'dlrm_intel_cpu_server_target_qps',
+    9750.0,
+    'The Target QPS for the server scenario. The default value is 9750.0.',
+)
+
 BENCHMARK_NAME = 'dlrm_intel_cpu_inference'
 BENCHMARK_CONFIG = """
 dlrm_intel_cpu_inference:
@@ -45,8 +68,6 @@ dlrm_intel_cpu_inference:
   flags:
     disable_smt: True
 """
-
-FLAGS = flags.FLAGS
 
 
 def GetConfig(user_config: Dict[str, Any]) -> Dict[str, Any]:
@@ -102,19 +123,21 @@ def Prepare(bm_spec: benchmark_spec.BenchmarkSpec) -> None:
       '-e CPUS_PER_INSTANCE=2 '
       '-e CPUS_FOR_LOADGEN=1 '
       '-e BATCH_SIZE=400 '
-      '-e DNNL_MAX_CPU_ISA=AVX512_CORE_AMX '  # AMX ignored if not supported
+      # AMX ignored if not supported
+      f'-e DNNL_MAX_CPU_ISA={_DNNL_MAX_CPU_ISA.value} '
       '--name=pkb-dlrm '
       'mlperf_inference_dlrm2:4.0'
   )
   # The target qps is set with the assumption running on EMR hosts.
   # Increase to make sure we always generate enough load.
   vm.RemoteCommand(
-      'docker exec pkb-dlrm '
-      'bash -c '
-      "'cd /opt/workdir/code/dlrm-v2-99.9/pytorch-cpu-int8; "
-      'ln -s /root/model/dlrm_int8.pt dlrm_int8.pt; '
-      'sed -i "s/dlrm.Offline.target_qps = 8600.0/'
-      'dlrm.Offline.target_qps = 16000.0/g" user_default.conf\''
+      "docker exec pkb-dlrm bash -c 'cd"
+      ' /opt/workdir/code/dlrm-v2-99.9/pytorch-cpu-int8; ln -s'
+      ' /root/model/dlrm_int8.pt dlrm_int8.pt; sed -i'
+      ' "s/dlrm.Offline.target_qps = 8600.0/dlrm.Offline.target_qps ='
+      ' 16000.0/g" user_default.conf; sed -i "s/dlrm.Server.target_qps ='
+      f' 8200.0/dlrm.Server.target_qps = {_SERVER_TARGET_QPS.value}/g"'
+      " user_default.conf'"
   )
 
 
@@ -123,7 +146,7 @@ def Run(bm_spec):
   vm = bm_spec.vms[0]
   cpus_for_benchmark = vm.NumCpusForBenchmark() // vm.numa_node_count // 2 * 2
   metadata = {
-      'scenario': 'offline',
+      'scenario': f'{_BENCHMARK_SCENARIO.value}',
       'num_sockets': vm.numa_node_count,
       'cpus_per_socket': cpus_for_benchmark,
       'cpus_per_process': cpus_for_benchmark,
@@ -135,14 +158,21 @@ def Run(bm_spec):
       'docker exec pkb-dlrm '
       'bash -c '
       "'cd /opt/workdir/code/dlrm-v2-99.9/pytorch-cpu-int8; "
-      'bash run_main.sh offline int8; '
-      'cat output/pytorch-cpu/dlrm/Offline/performance/'
+      f'bash run_main.sh {_BENCHMARK_SCENARIO.value} int8; '
+      f'cat output/pytorch-cpu/dlrm/{_BENCHMARK_SCENARIO.value.capitalize()}/performance/'
       "run_1/mlperf_log_summary.txt'"
   )
-  samples_per_sec = regex_util.ExtractFloat(
-      r'Samples per second: ' + f'({regex_util.FLOAT_REGEX})', stdout
-  )
   metadata['valid'] = 'Result is : VALID' in stdout
+  if not metadata['valid']:
+    raise errors.Benchmarks.RunError(
+        'Result is invalid. Please check the log for details.'
+    )
+  samples_per_sec_field = 'Samples per second: '
+  if _BENCHMARK_SCENARIO.value == 'server':
+    samples_per_sec_field = 'Completed samples per second    : '
+  samples_per_sec = regex_util.ExtractFloat(
+      samples_per_sec_field + f'({regex_util.FLOAT_REGEX})', stdout
+  )
   for percentile in ('50.00', '90.00', '95.00', '97.00', '99.00', '99.90'):
     latency = regex_util.ExtractFloat(
         percentile + r' percentile latency \(ns\)\s*: (\d+)', stdout
