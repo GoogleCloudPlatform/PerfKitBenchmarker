09d5ed15b49fc9512d84eafa14f794400102377b
Rui Wang <ruwa@google.com>
2024-10-07 10:10:30 -0700

    Minor linter cleanups to github pull request.
    PiperOrigin-RevId: 683227130

--- perfkitbenchmarker/linux_packages/openjdk_msft.py
+++ perfkitbenchmarker/linux_packages/openjdk_msft.py
@@ -1,9 +1,23 @@
-# linux_packages/openjdk_msft.py
+# Copyright 2024 PerfKitBenchmarker Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Module containing Microsoft Build of OpenJDK's installation."""
 
 
 def AptInstall(vm):
-  """
-  Installs Microsoft Build of OpenJDK on an Ubuntu-based VM.
+  """Installs Microsoft Build of OpenJDK on an Ubuntu-based VM.
 
   Args:
       vm: The VM instance where the OpenJDK should be installed.
@@ -24,8 +38,7 @@ def AptInstall(vm):
 
 
 def YumInstall(vm):
-  """
-  Installs Microsoft Build of OpenJDK on a RHEL/CentOS-based VM.
+  """Installs Microsoft Build of OpenJDK on a RHEL/CentOS-based VM.
 
   Args:
       vm: The VM instance where the OpenJDK should be installed.
