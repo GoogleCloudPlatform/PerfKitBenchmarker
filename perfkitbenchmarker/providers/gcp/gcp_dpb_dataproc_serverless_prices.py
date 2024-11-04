693fb1af7b868c925be92548ebf285d9e078f071
Diego Orellana <odiego@google.com>
2024-11-04 12:17:25 -0800

    Update Dataproc Serverless pricings.
    PiperOrigin-RevId: 693051391

--- perfkitbenchmarker/providers/gcp/gcp_dpb_dataproc_serverless_prices.py
+++ perfkitbenchmarker/providers/gcp/gcp_dpb_dataproc_serverless_prices.py
@@ -9,277 +9,277 @@ DATAPROC_SERVERLESS_PRICES = {
     'standard': {
         'us-west1': {
             'usd_per_milli_dcu_sec': 0.06 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.04 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.04 / 721 / 3600,
         },
         'us-west2': {
             'usd_per_milli_dcu_sec': 0.072071 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.048 / 721 / 3600,
         },
         'us-west3': {
             'usd_per_milli_dcu_sec': 0.072071 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.048 / 721 / 3600,
         },
         'us-west4': {
             'usd_per_milli_dcu_sec': 0.067572 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.044 / 721 / 3600,
         },
         'us-east1': {
             'usd_per_milli_dcu_sec': 0.06 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.04 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.04 / 721 / 3600,
         },
         'us-east4': {
             'usd_per_milli_dcu_sec': 0.067572 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.044 / 721 / 3600,
         },
         'us-east5': {
             'usd_per_milli_dcu_sec': 0.06 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.04 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.04 / 721 / 3600,
         },
         'us-central1': {
             'usd_per_milli_dcu_sec': 0.06 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.04 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.04 / 721 / 3600,
         },
         'us-south1': {
             'usd_per_milli_dcu_sec': 0.0708 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.0472 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.0472 / 721 / 3600,
         },
         'europe-north1': {
             'usd_per_milli_dcu_sec': 0.066062 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.044 / 721 / 3600,
         },
         'europe-west1': {
             'usd_per_milli_dcu_sec': 0.066007 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.04 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.04 / 721 / 3600,
         },
         'europe-west2': {
             'usd_per_milli_dcu_sec': 0.077307 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.048 / 721 / 3600,
         },
         'europe-west3': {
             'usd_per_milli_dcu_sec': 0.077307 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.048 / 721 / 3600,
         },
         'europe-west4': {
             'usd_per_milli_dcu_sec': 0.066057 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.044 / 721 / 3600,
         },
         'europe-west6': {
             'usd_per_milli_dcu_sec': 0.083955 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.052 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.052 / 721 / 3600,
         },
         'europe-west8': {
             'usd_per_milli_dcu_sec': 0.0696 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.0464 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.0464 / 721 / 3600,
         },
         'europe-west9': {
             'usd_per_milli_dcu_sec': 0.0696 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.0464 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.0464 / 721 / 3600,
         },
         'europe-central2': {
             'usd_per_milli_dcu_sec': 0.077307 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.048 / 721 / 3600,
         },
         'europe-southwest1': {
             'usd_per_milli_dcu_sec': 0.0708 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.047 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.047 / 721 / 3600,
         },
         'asia-northeast1': {
             'usd_per_milli_dcu_sec': 0.076976 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.052 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.052 / 721 / 3600,
         },
         'asia-northeast2': {
             'usd_per_milli_dcu_sec': 0.076976 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.052 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.052 / 721 / 3600,
         },
         'asia-northeast3': {
             'usd_per_milli_dcu_sec': 0.076976 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.052 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.052 / 721 / 3600,
         },
         'asia-east1': {
             'usd_per_milli_dcu_sec': 0.069477 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.04 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.04 / 721 / 3600,
         },
         'asia-east2': {
             'usd_per_milli_dcu_sec': 0.083955 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.05 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.05 / 721 / 3600,
         },
         'asia-south1': {
             'usd_per_milli_dcu_sec': 0.072071 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.048 / 721 / 3600,
         },
         'asia-south2': {
             'usd_per_milli_dcu_sec': 0.072071 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.048 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.048 / 721 / 3600,
         },
         'asia-southeast1': {
             'usd_per_milli_dcu_sec': 0.074015 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.044 / 721 / 3600,
         },
         'asia-southeast2': {
             'usd_per_milli_dcu_sec': 0.080674 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.052 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.052 / 721 / 3600,
         },
         'australia-southeast1': {
             'usd_per_milli_dcu_sec': 0.085135 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.054 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.054 / 721 / 3600,
         },
         'australia-southeast2': {
             'usd_per_milli_dcu_sec': 0.085135 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.054 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.054 / 721 / 3600,
         },
         'northamerica-northeast1': {
             'usd_per_milli_dcu_sec': 0.066057 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.044 / 721 / 3600,
         },
         'northamerica-northeast2': {
             'usd_per_milli_dcu_sec': 0.066057 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.044 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.044 / 721 / 3600,
         },
         'southamerica-east1': {
             'usd_per_milli_dcu_sec': 0.095246 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.06 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.06 / 721 / 3600,
         },
         'southamerica-west1': {
             'usd_per_milli_dcu_sec': 0.08581 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.057 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.057 / 721 / 3600,
         },
     },
     'premium': {
         'us-west1': {
             'usd_per_milli_dcu_sec': 0.089 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.1 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.1 / 721 / 3600,
         },
         'us-west2': {
             'usd_per_milli_dcu_sec': 0.106905 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.12 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.12 / 721 / 3600,
         },
         'us-west3': {
             'usd_per_milli_dcu_sec': 0.106905 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.12 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.12 / 721 / 3600,
         },
         'us-west4': {
             'usd_per_milli_dcu_sec': 0.100232 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.11 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.11 / 721 / 3600,
         },
         'us-east1': {
             'usd_per_milli_dcu_sec': 0.089 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.1 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.1 / 721 / 3600,
         },
         'us-east4': {
             'usd_per_milli_dcu_sec': 0.100232 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.11 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.11 / 721 / 3600,
         },
         'us-east5': {
             'usd_per_milli_dcu_sec': 0.089 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.1 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.1 / 721 / 3600,
         },
         'us-central1': {
             'usd_per_milli_dcu_sec': 0.089 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.1 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.1 / 721 / 3600,
         },
         'us-south1': {
             'usd_per_milli_dcu_sec': 0.10502 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.118 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.118 / 721 / 3600,
         },
         'europe-north1': {
             'usd_per_milli_dcu_sec': 0.097992 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.11 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.11 / 721 / 3600,
         },
         'europe-west1': {
             'usd_per_milli_dcu_sec': 0.09791 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.1 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.1 / 721 / 3600,
         },
         'europe-west2': {
             'usd_per_milli_dcu_sec': 0.114672 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.12 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.12 / 721 / 3600,
         },
         'europe-west3': {
             'usd_per_milli_dcu_sec': 0.114672 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.12 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.12 / 721 / 3600,
         },
         'europe-west4': {
             'usd_per_milli_dcu_sec': 0.097984 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.11 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.11 / 721 / 3600,
         },
         'europe-west6': {
             'usd_per_milli_dcu_sec': 0.124533 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.13 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.13 / 721 / 3600,
         },
         'europe-west8': {
             'usd_per_milli_dcu_sec': 0.10324 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.116 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.116 / 721 / 3600,
         },
         'europe-west9': {
             'usd_per_milli_dcu_sec': 0.10324 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.116 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.116 / 721 / 3600,
         },
         'europe-central2': {
             'usd_per_milli_dcu_sec': 0.114672 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.12 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.12 / 721 / 3600,
         },
         'europe-southwest1': {
             'usd_per_milli_dcu_sec': 0.10502 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.118 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.118 / 721 / 3600,
         },
         'asia-northeast1': {
             'usd_per_milli_dcu_sec': 0.114181 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.13 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.13 / 721 / 3600,
         },
         'asia-northeast2': {
             'usd_per_milli_dcu_sec': 0.114181 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.13 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.13 / 721 / 3600,
         },
         'asia-northeast3': {
             'usd_per_milli_dcu_sec': 0.114181 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.13 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.13 / 721 / 3600,
         },
         'asia-east1': {
             'usd_per_milli_dcu_sec': 0.103058 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.1 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.1 / 721 / 3600,
         },
         'asia-east2': {
             'usd_per_milli_dcu_sec': 0.124533 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.125 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.125 / 721 / 3600,
         },
         'asia-south1': {
             'usd_per_milli_dcu_sec': 0.106905 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.12 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.12 / 721 / 3600,
         },
         'asia-south2': {
             'usd_per_milli_dcu_sec': 0.106905 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.12 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.12 / 721 / 3600,
         },
         'asia-southeast1': {
             'usd_per_milli_dcu_sec': 0.10979 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.11 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.11 / 721 / 3600,
         },
         'asia-southeast2': {
             'usd_per_milli_dcu_sec': 0.119666 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.13 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.13 / 721 / 3600,
         },
         'australia-southeast1': {
             'usd_per_milli_dcu_sec': 0.126284 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.135 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.135 / 721 / 3600,
         },
         'australia-southeast2': {
             'usd_per_milli_dcu_sec': 0.126284 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.135 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.135 / 721 / 3600,
         },
         'northamerica-northeast1': {
             'usd_per_milli_dcu_sec': 0.097984 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.11 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.11 / 721 / 3600,
         },
         'northamerica-northeast2': {
             'usd_per_milli_dcu_sec': 0.097984 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.11 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.11 / 721 / 3600,
         },
         'southamerica-east1': {
             'usd_per_milli_dcu_sec': 0.141282 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.15 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.15 / 721 / 3600,
         },
         'southamerica-west1': {
             'usd_per_milli_dcu_sec': 0.127285 / 1000 / 3600,
-            'usd_per_shuffle_storage_gb_sec': 0.143 / 744 / 3600,
+            'usd_per_shuffle_storage_gb_sec': 0.143 / 721 / 3600,
         },
     },
 }
