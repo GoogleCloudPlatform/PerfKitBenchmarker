54442060e03c7ca455d8a9eb24d52d6e79e51484
Arushi Gaur <arushigaur@google.com>
2024-11-11 15:00:54 -0800

    Adding sysbench benchmark for unmanaged postgresql database
    PiperOrigin-RevId: 695493586

--- perfkitbenchmarker/linux_benchmarks/unmanaged_postgresql_sysbench_benchmark.py
+++ perfkitbenchmarker/linux_benchmarks/unmanaged_postgresql_sysbench_benchmark.py
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Sysbench Benchmark for unmanaged MySQL db on a VM.
+"""Sysbench Benchmark for unmanaged PostgreSQL db on a VM.
 
-This benchmark measures performance of Sysbench Databases on unmanaged MySQL.
+This benchmark measures performance of Sysbench Databases on unmanaged
+postgreSQL.
 """
 
 import copy
@@ -26,23 +27,33 @@ from perfkitbenchmarker import benchmark_spec as bm_spec
 from perfkitbenchmarker import configs
 from perfkitbenchmarker import errors
 from perfkitbenchmarker import sample
-from perfkitbenchmarker.linux_packages import mysql80
+from perfkitbenchmarker.linux_packages import postgresql16
 from perfkitbenchmarker.linux_packages import sysbench
 
 
 FLAGS = flags.FLAGS
 
 
-BENCHMARK_NAME = 'unmanaged_mysql_sysbench'
+BENCHMARK_NAME = 'unmanaged_postgresql_sysbench'
 BENCHMARK_CONFIG = """
-unmanaged_mysql_sysbench:
-  description: Mysql on a VM benchmarked using Sysbench.
+unmanaged_postgresql_sysbench:
+  description: PostgreSQL on a VM benchmarked using Sysbench.
   vm_groups:
-    server:
-      os_type: centos_stream9
+    client:
       vm_spec:
         GCP:
-          machine_type: c3-highmem-22
+          machine_type: c3-standard-22
+          zone: us-east1-b
+        AWS:
+          machine_type: m7i.4xlarge
+          zone: us-east-1a
+        Azure:
+          machine_type: Standard_D16s_v5
+          zone: eastus
+    server:
+      vm_spec:
+        GCP:
+          machine_type: c3-standard-22
           zone: us-east1-b
         AWS:
           machine_type: r7i.4xlarge
@@ -53,7 +64,7 @@ unmanaged_mysql_sysbench:
       disk_spec:
         GCP:
           disk_size: 500
-          disk_type: hyperdisk-balanced
+          disk_type: pd-ssd
           provisioned_iops: 160000
           provisioned_throughput: 2400
           num_striped_disks: 1
@@ -69,41 +80,13 @@ unmanaged_mysql_sysbench:
           provisioned_iops: 40000
           provisioned_throughput: 800
           num_striped_disks: 2
-    client:
-      os_type: centos_stream9
-      vm_spec:
-        GCP:
-          machine_type: c3-standard-22
-          zone: us-east1-b
-        AWS:
-          machine_type: m7i.4xlarge
-          zone: us-east-1a
-        Azure:
-          machine_type: Standard_D16s_v5
-          zone: eastus
   flags:
     sysbench_version: df89d34c410a2277e19f77e47e535d0890b2029b
     disk_fs_type: xfs
-    # for now we only have mysql supported
-    db_engine: mysql
-    sysbench_report_interval: 1
-    sysbench_ssl_mode: required
-    db_high_availability: True
-    sysbench_run_threads: 1,64,128,256,512,1024,2048
-    sysbench_run_seconds: 300
 """
 
-# There are 2 main customer scenarios:
-# 1: 100G data set and all of that fits in memory,
-# therefore only logging accesses disks.
-# 2: 100G data and only 8G fits in memory,
-# so both data access and logging access disks.
-# Percona claims this is consistent with the alternative approach of
-# increasing buffer pool and dataset to larger sizes
-DEFAULT_BUFFER_POOL_SIZE = 8
-
 # The database name is used to create a database on the server.
-_DATABASE_TYPE = 'mysql'
+_DATABASE_TYPE = 'pgsql'
 _DATABASE_NAME = 'sysbench'
 
 # test names
@@ -112,6 +95,12 @@ _OLTP_READ_WRITE = 'oltp_read_write'
 _OLTP_READ_ONLY = 'oltp_read_only'
 _OLTP_WRITE_ONLY = 'oltp_write_only'
 _OLTP = [_OLTP_READ_WRITE, _OLTP_READ_ONLY, _OLTP_WRITE_ONLY]
+
+SHARED_BUFFER_SIZE = flags.DEFINE_integer(
+    'postgresql_shared_buffer_size',
+    10,
+    'Size of the shared buffer in the postgresql cluster (in Gb).',
+)
 
 
 def GetConfig(user_config):
@@ -128,7 +117,9 @@ def GetConfig(user_config):
   # Force the scratch disk as database default dir (simpler code).
   disk_spec = config['vm_groups']['server']['disk_spec']
   for cloud in disk_spec:
-    disk_spec[cloud]['mount_point'] = '/var/lib/mysql'
+    disk_spec[cloud]['mount_point'] = postgresql16.GetOSDependentDefaults(
+        FLAGS.os_type
+    )['disk_mount_point']
   # Update machine type for server/client.
   if FLAGS.db_machine_type:
     vm_spec = config['vm_groups']['server']['vm_spec']
@@ -148,9 +139,43 @@ def GetConfig(user_config):
   return config
 
 
-def _GetPassword():
-  """Generate a password for this session."""
-  return FLAGS.run_uri + '_P3rfk1tbenchm4rker#'
+def Prepare(benchmark_spec: bm_spec.BenchmarkSpec):
+  """Prepare the servers and clients for the benchmark run.
+
+  Args:
+    benchmark_spec:
+  """
+  vms = benchmark_spec.vms
+  replica_servers = []
+  for vm in benchmark_spec.vm_groups:
+    if vm.startswith('replica'):
+      replica_servers += benchmark_spec.vm_groups[vm]
+  background_tasks.RunThreaded(postgresql16.ConfigureSystemSettings, vms)
+  background_tasks.RunThreaded(lambda vm: vm.Install('postgresql16'), vms)
+
+  primary_server = benchmark_spec.vm_groups['server'][0]
+  postgresql16.InitializeDatabase(primary_server)
+  postgresql16.ConfigureAndRestart(primary_server, FLAGS.run_uri)
+  for index, replica in enumerate(replica_servers):
+    postgresql16.SetupReplica(primary_server, replica, index, FLAGS.run_uri)
+  clients = benchmark_spec.vm_groups['client']
+  for client in clients:
+    client.db_driver = _DATABASE_TYPE
+    client.InstallPackages('git')
+    client.Install('sysbench')
+    if FLAGS.sysbench_testname == _TPCC:
+      client.RemoteCommand(
+          'cd /opt && sudo rm -fr sysbench-tpcc && '
+          f'sudo git clone {sysbench.SYSBENCH_TPCC_REPRO}'
+      )
+  loader_vm = benchmark_spec.vm_groups['client'][0]
+  sysbench_parameters = _GetSysbenchParameters(
+      primary_server.internal_ip,
+      postgresql16.GetPsqlUserPassword(FLAGS.run_uri),
+  )
+  cmd = sysbench.BuildLoadCommand(sysbench_parameters)
+  logging.info('%s load command: %s', FLAGS.sysbench_testname, cmd)
+  loader_vm.RemoteCommand(cmd)
 
 
 def _GetSysbenchParameters(primary_server_ip: str | None, password: str):
@@ -164,8 +189,8 @@ def _GetSysbenchParameters(primary_server_ip: str | None, password: str):
       db_password=password,
       db_name=_DATABASE_NAME,
       host_ip=primary_server_ip,
-      ssl_setting=sysbench.SYSBENCH_SSL_MODE.value,
   )
+  sysbench_parameters.port = 5432
   test = FLAGS.sysbench_testname
   if test in _OLTP:
     sysbench_parameters.built_in_test = True
@@ -173,9 +198,6 @@ def _GetSysbenchParameters(primary_server_ip: str | None, password: str):
     sysbench_parameters.db_ps_mode = 'disable'
     sysbench_parameters.skip_trx = True
     sysbench_parameters.table_size = FLAGS.sysbench_table_size
-    # oltp tests on mysql require ignoring errors.
-    # https://github.com/akopytov/sysbench/issues/253
-    sysbench_parameters.mysql_ignore_errors = 'all'
 
   elif test == _TPCC:
     sysbench_parameters.custom_lua_packages_path = '/opt/sysbench-tpcc/?.lua'
@@ -193,62 +215,6 @@ def _GetSysbenchParameters(primary_server_ip: str | None, password: str):
   return sysbench_parameters
 
 
-def GetBufferPoolSize():
-  """Get buffer pool size from flags."""
-  if FLAGS.innodb_buffer_pool_size:
-    return f'{FLAGS.innodb_buffer_pool_size}G'
-  return f'{DEFAULT_BUFFER_POOL_SIZE}G'
-
-
-def Prepare(benchmark_spec: bm_spec.BenchmarkSpec):
-  """Prepare the servers and clients for the benchmark run.
-
-  Args:
-    benchmark_spec:
-  """
-  vms = benchmark_spec.vms
-  background_tasks.RunThreaded(mysql80.ConfigureSystemSettings, vms)
-  background_tasks.RunThreaded(lambda vm: vm.Install('mysql80'), vms)
-
-  buffer_pool_size = GetBufferPoolSize()
-
-  primary_server = benchmark_spec.vm_groups['server'][0]
-  replica_servers = []
-  for vm in benchmark_spec.vm_groups:
-    if vm.startswith('replica'):
-      replica_servers += benchmark_spec.vm_groups[vm]
-
-  servers = [primary_server] + replica_servers
-  new_password = FLAGS.run_uri + '_P3rfk1tbenchm4rker#'
-  for index, server in enumerate(servers):
-    # mysql server ids needs to be positive integers.
-    mysql80.ConfigureAndRestart(server, buffer_pool_size, index + 1)
-    mysql80.UpdatePassword(server, new_password)
-    mysql80.CreateDatabase(server, new_password, _DATABASE_NAME)
-
-  assert primary_server.internal_ip
-  for replica in replica_servers:
-    mysql80.SetupReplica(replica, new_password, primary_server.internal_ip)
-
-  clients = benchmark_spec.vm_groups['client']
-  for client in clients:
-    client.db_driver = _DATABASE_TYPE
-    client.InstallPackages('git')
-    client.Install('sysbench')
-    if FLAGS.sysbench_testname == _TPCC:
-      client.RemoteCommand(
-          'cd /opt && sudo rm -fr sysbench-tpcc && '
-          f'sudo git clone {sysbench.SYSBENCH_TPCC_REPRO}'
-      )
-
-  loader_vm = benchmark_spec.vm_groups['client'][0]
-  sysbench_parameters = _GetSysbenchParameters(
-      primary_server.internal_ip, new_password)
-  cmd = sysbench.BuildLoadCommand(sysbench_parameters)
-  logging.info('%s load command: %s', FLAGS.sysbench_testname, cmd)
-  loader_vm.RemoteCommand(cmd)
-
-
 def Run(benchmark_spec: bm_spec.BenchmarkSpec) -> list[sample.Sample]:
   """Run the sysbench benchmark and publish results.
 
@@ -262,36 +228,38 @@ def Run(benchmark_spec: bm_spec.BenchmarkSpec) -> list[sample.Sample]:
   primary_server = benchmark_spec.vm_groups['server'][0]
   client = benchmark_spec.vm_groups['client'][0]
   sysbench_parameters = _GetSysbenchParameters(
-      primary_server.internal_ip, _GetPassword())
+      primary_server.internal_ip,
+      postgresql16.GetPsqlUserPassword(FLAGS.run_uri),
+  )
   results = []
-  # a map of trasactions metric name to current sample with max value
+  # a map of transaction metric name (tps/qps) to current sample with max value
   max_transactions = {}
   for thread_count in FLAGS.sysbench_run_threads:
     sysbench_parameters.threads = thread_count
-    cmd = sysbench.BuildRunCommand(sysbench_parameters)
-    logging.info('%s run command: %s', FLAGS.sysbench_testname, cmd)
-    try:
-      # TODO(arushigaur): Can be updated to use sysbench.Run to run sysbench..
-      stdout, _ = client.RemoteCommand(
-          cmd, timeout=2*FLAGS.sysbench_run_seconds,)
-    except errors.VirtualMachine.RemoteCommandError as e:
-      logging.exception('Failed to run sysbench command: %s', e)
+    stdout = sysbench.Run(client, sysbench_parameters)
+    if not stdout:
       continue
     metadata = sysbench.GetMetadata(sysbench_parameters)
     metadata.update({
-        'buffer_pool_size': GetBufferPoolSize(),
+        'shared_buffer_size': f'{SHARED_BUFFER_SIZE.value}GB',
     })
     results += sysbench.ParseSysbenchTimeSeries(stdout, metadata)
     results += sysbench.ParseSysbenchLatency([stdout], metadata)
     current_transactions = sysbench.ParseSysbenchTransactions(stdout, metadata)
     results += current_transactions
+    # max transactions stores the max tps/qps for all the thread counts.
+    # update the max tps/qps in max_transactions.
     for item in current_transactions:
       metric = item.metric
       metric_value = item.value
       current_max_sample = max_transactions.get(metric, None)
       if not current_max_sample or current_max_sample.value < metric_value:
         max_transactions[metric] = item
-  # find the max tps/qps amongst all thread counts and report as a new metric.
+  if not results:
+    raise errors.Benchmarks.RunError(
+        'None of the sysbench tests were successful.'
+    )
+  # report the max tps/qps as a new metric.
   for item in max_transactions.values():
     metadata = copy.deepcopy(item.metadata)
     metadata['searched_thread_counts'] = FLAGS.sysbench_run_threads
---
+++ perfkitbenchmarker/linux_packages/postgresql16.py
@@ -0,0 +1,312 @@
+# Copyright 2020 PerfKitBenchmarker Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module containing postgres installation functions."""
+
+import os
+
+from perfkitbenchmarker import data
+from perfkitbenchmarker import os_types
+
+
+SYSBENCH_PASSWORD = 'Syb3enCh#1'
+SHARED_BUFFERS_CONF = {
+    'SIZE_10GB': {
+        'shared_buffers': '10GB',
+        'effective_cache_size': '30GB',
+        'max_memory': '40G',
+        'nr_hugepages': '5632',
+    },
+    'SIZE_100GB': {
+        'shared_buffers': '100GB',
+        'effective_cache_size': '112.5GB',
+        'max_memory': '150G',
+        'nr_hugepages': '52736',
+    },
+}
+OS_DEPENDENT_DEFAULTS = {
+    'centos': {
+        'postgres_path': '/usr/pgsql-16',
+        'data_dir': '/var/lib/pgsql/16/data',
+        'conf_dir': '/var/lib/pgsql/16/data',
+        'disk_mount_point': '/var/lib/pgsql/16',
+        'postgres_service_name': 'postgresql-16'
+    },
+    'debian': {
+        'postgres_path': '/usr/lib/postgresql/16',
+        'data_dir': '/etc/postgresql/16/data/data',
+        'conf_dir': '/etc/postgresql/16/main',
+        'disk_mount_point': '/etc/postgresql/16/data',
+        'postgres_service_name': 'postgresql',
+    },
+    'amazonlinux': {
+        'postgres_path': '/usr/pgsql-16',
+        'data_dir': '/var/lib/pgsql/data',
+        'conf_dir': '/var/lib/pgsql/data',
+        'disk_mount_point': '/var/lib/pgsql',
+        'postgres_service_name': 'postgresql',
+    }
+}
+
+
+def ConfigureSystemSettings(vm):
+  """Tune OS for postgres."""
+  sysctl_append = 'sudo tee -a /etc/sysctl.conf'
+  sysctl_data = (
+      'vm.swappiness=1\nvm.dirty_ratio=15\nvm.dirty_background_ratio=5\n'
+      'net.core.somaxconn=65535\nnet.core.netdev_max_backlog=65535\n'
+      'net.ipv4.tcp_max_syn_backlog=65535\n'
+      'net.ipv4.ip_local_port_range=4000 65000\nnet.ipv4.tcp_tw_reuse=1\n'
+      'net.ipv4.tcp_fin_timeout=5'
+  )
+  vm.RemoteCommand(f'echo """{sysctl_data}""" | {sysctl_append}')
+  vm.RemoteCommand('sudo sysctl -p')
+
+  limits_append = 'sudo tee -a /etc/security/limits.conf'
+  vm.RemoteCommand(f'echo "*     soft    nofile  64000" | {limits_append}')
+  vm.RemoteCommand(f'echo "*     hard    nofile  64000" | {limits_append}')
+
+  vm.RemoteCommand(
+      'echo "session    required pam_limits.so" | sudo tee -a /etc/pam.d/login'
+  )
+  thp_append = 'sudo tee -a /usr/lib/systemd/system/disable-thp.service'
+  vm.RemoteCommand('sudo touch /usr/lib/systemd/system/disable-thp.service')
+  disable_huge_pages = f"""[Unit]
+    Description=Disable Transparent Huge Pages (THP)
+    DefaultDependencies=no
+    After=sysinit.target local-fs.target
+    Before={GetOSDependentDefaults(vm.OS_TYPE)["postgres_service_name"]}.service
+
+    [Service]
+    Type=oneshot
+    ExecStart=/bin/sh -c 'echo never | tee /sys/kernel/mm/transparent_hugepage/enabled > /dev/null'
+
+    [Install]
+    WantedBy=basic.target
+    """
+  vm.RemoteCommand(f'echo "{disable_huge_pages}" | {thp_append}')
+  vm.RemoteCommand(
+      'sudo chown root:root /usr/lib/systemd/system/disable-thp.service'
+  )
+  vm.RemoteCommand(
+      'sudo chmod 0600 /usr/lib/systemd/system/disable-thp.service'
+  )
+  vm.RemoteCommand('sudo systemctl daemon-reload')
+  vm.RemoteCommand(
+      'sudo systemctl enable disable-thp.service && sudo systemctl start'
+      ' disable-thp.service'
+  )
+  vm.Reboot()
+
+
+def YumInstall(vm):
+  """Installs the postgres package on the VM."""
+  if vm.OS_TYPE not in os_types.AMAZONLINUX_TYPES:
+    vm.RemoteCommand('sudo dnf config-manager --set-enabled crb')
+    vm.RemoteCommand('sudo dnf install -y epel-release epel-next-release')
+    vm.RemoteCommand(
+        'sudo yum install -y https://download.postgresql.org/pub/repos/yum/'
+        'reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm --skip-broken'
+    )
+    vm.RemoteCommand('sudo dnf -qy module disable postgresql')
+  else:
+    vm.RemoteCommand('sudo dnf update')
+  vm.RemoteCommand(
+      'sudo yum install -y postgresql16-server postgresql16'
+      ' postgresql16-contrib'
+  )
+
+
+def AptInstall(vm):
+  """Installs the postgres package on the VM."""
+  vm.RemoteCommand('sudo apt-get install -y postgresql-common')
+  vm.RemoteCommand(
+      'sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh -y'
+  )
+  vm.RemoteCommand('sudo apt-get update')
+  vm.RemoteCommand('sudo apt-get install -y postgresql-contrib-16')
+  vm.RemoteCommand('sudo apt-get -y install postgresql-16')
+
+
+def InitializeDatabase(vm):
+  """Initialize the database."""
+  postgres_path = GetOSDependentDefaults(vm.OS_TYPE)['postgres_path']
+  data_path = GetOSDependentDefaults(vm.OS_TYPE)['data_dir']
+  vm.RemoteCommand(
+      f'sudo mkdir -p {data_path} && sudo chown postgres:root'
+      f' {data_path}'
+  )
+  if vm.OS_TYPE in os_types.AMAZONLINUX_TYPES:
+    vm.RemoteCommand('sudo postgresql-setup --initdb')
+  else:
+    vm.RemoteCommand(
+        'sudo -u postgres'
+        f' {postgres_path}/bin/initdb -D'
+        f' {data_path}'
+    )
+
+
+def GetOSDependentDefaults(os_type: str) -> dict[str, str]:
+  """Returns the OS family."""
+  if os_type in os_types.CENTOS_TYPES:
+    return OS_DEPENDENT_DEFAULTS['centos']
+  elif os_type in os_types.AMAZONLINUX_TYPES:
+    return OS_DEPENDENT_DEFAULTS['amazonlinux']
+  else:
+    return OS_DEPENDENT_DEFAULTS['debian']
+
+
+def ConfigureAndRestart(vm, run_uri):
+  """Configure and restart postgres.
+
+  Args:
+    vm: virtual machine to configure postgres on.
+    run_uri: run uri to use for password generation.
+  """
+  conf_path = GetOSDependentDefaults(vm.OS_TYPE)['conf_dir']
+  data_path = GetOSDependentDefaults(vm.OS_TYPE)['data_dir']
+  conf_template_config = 'postgresql/postgresql-custom.conf.j2'
+  remote_temp_config = '/tmp/my.cnf'
+  postgres_conf_path = os.path.join(conf_path, 'postgresql-custom.conf')
+  pg_hba_conf_path = os.path.join(conf_path, 'pg_hba.conf')
+  database_queries_path = os.path.join(conf_path, 'queries.sql')
+  database_setup_queries = 'postgresql/database_setup_queries.sql.j2'
+  context = {
+      'listen_address': vm.internal_ip,
+      'shared_buffers': SHARED_BUFFERS_CONF['SIZE_10GB']['shared_buffers'],
+      'effective_cache_size': SHARED_BUFFERS_CONF['SIZE_10GB'][
+          'effective_cache_size'
+      ],
+      'data_directory': data_path,
+  }
+  vm.RenderTemplate(
+      data.ResourcePath(conf_template_config),
+      remote_temp_config,
+      context,
+  )
+  vm.RemoteCommand(f'sudo cp {remote_temp_config} {postgres_conf_path}')
+  vm.RemoteCommand(
+      'sudo echo -e "\ninclude = postgresql-custom.conf" | sudo tee -a'
+      f' {os.path.join(conf_path, "postgresql.conf")}'
+  )
+  vm.RenderTemplate(
+      data.ResourcePath('postgresql/pg_hba.conf.j2'),
+      '/tmp/pg_hba.conf',
+      {},
+  )
+  vm.RemoteCommand(f'sudo cp /tmp/pg_hba.conf {pg_hba_conf_path}')
+  vm.RenderTemplate(
+      data.ResourcePath(database_setup_queries),
+      '/tmp/queries.sql',
+      {
+          'repl_user_password': GetPsqlUserPassword(run_uri),
+          'sysbench_user_password': GetPsqlUserPassword(run_uri),
+          'pmm_user_password': GetPsqlUserPassword(run_uri),
+      },
+  )
+  vm.RemoteCommand(f'sudo cp /tmp/queries.sql {database_queries_path}')
+  vm.RemoteCommand(f'sudo chmod 755 {database_queries_path}')
+  vm.RemoteCommand(
+      'sudo sysctl -w'
+      f' vm.nr_hugepages={SHARED_BUFFERS_CONF["SIZE_10GB"]["nr_hugepages"]}'
+  )
+  vm.RemoteCommand(
+      'sudo sysctl -w vm.hugetlb_shm_group=$(getent group postgres | cut -d:'
+      ' -f3)'
+  )
+  vm.RemoteCommand('cat /proc/meminfo | grep -i "^hugepage"')
+  vm.RemoteCommand('sudo cat /proc/sys/vm/hugetlb_shm_group')
+  vm.RemoteCommand(
+      'sudo systemctl set-property'
+      f' {GetOSDependentDefaults(vm.OS_TYPE)["postgres_service_name"]}.service'
+      f' MemoryMax={SHARED_BUFFERS_CONF["SIZE_10GB"]["max_memory"]}'
+  )
+  vm.RemoteCommand('sudo sync; echo 3 | sudo tee -a /proc/sys/vm/drop_caches')
+  vm.RemoteCommand(
+      f'cat /etc/systemd/system.control/{GetOSDependentDefaults(vm.OS_TYPE)["postgres_service_name"]}.service.d/50-MemoryMax.conf'
+  )
+  vm.RemoteCommand(
+      'sudo su - postgres -c "openssl req -new -x509 -days 365 -nodes -text'
+      f' -out {data_path}/server.crt -keyout'
+      f' {data_path}/server.key -subj "/CN=`hostname`""'
+  )
+  vm.RemoteCommand(f'sudo chmod 755 {postgres_conf_path}')
+  vm.RemoteCommand(
+      'sudo systemctl restart'
+      f' {GetOSDependentDefaults(vm.OS_TYPE)["postgres_service_name"]}'
+  )
+  vm.RemoteCommand(
+      f'sudo su - postgres -c "psql -a -f {database_queries_path}"'
+  )
+
+
+def SetupReplica(primary_vm, replica_vm, replica_id, run_uri):
+  """Setup postgres replica."""
+  data_path = GetOSDependentDefaults(replica_vm.OS_TYPE)['data_dir']
+  replica_vm.RemoteCommand(f'sudo mkdir -p {data_path}')
+  replica_vm.RemoteCommand(f'sudo chown postgres:root {data_path}')
+  replica_vm.RemoteCommand(
+      'sudo su - postgres -c'
+      f' "PGPASSWORD="{GetPsqlUserPassword(run_uri)}"'
+      f' pg_basebackup -h {primary_vm.internal_ip} -U repl -p 5432 -D'
+      f' {data_path} -S slot{replica_id} -Fp -P -Xs -R"'
+  )
+  context = {
+      'listen_address': 'localhost',
+      'shared_buffers': SHARED_BUFFERS_CONF['SIZE_10GB']['shared_buffers'],
+      'effective_cache_size': SHARED_BUFFERS_CONF['SIZE_10GB'][
+          'effective_cache_size'
+      ],
+      'data_directory': data_path,
+  }
+  conf_path = GetOSDependentDefaults(replica_vm.OS_TYPE)['conf_dir']
+  conf_template_config = 'postgresql/postgresql-custom.conf.j2'
+  remote_temp_config = '/tmp/my.cnf'
+  postgres_conf_path = os.path.join(conf_path, 'postgresql-custom.conf')
+  replica_vm.RenderTemplate(
+      data.ResourcePath(conf_template_config),
+      remote_temp_config,
+      context,
+  )
+  replica_vm.RemoteCommand(f'sudo cp {remote_temp_config} {postgres_conf_path}')
+  # vm.RemoteCommand('sudo chmod 660 postgresql.conf ')
+  replica_vm.RemoteCommand(
+      'sudo sysctl -w'
+      f' vm.nr_hugepages={SHARED_BUFFERS_CONF["SIZE_10GB"]["nr_hugepages"]}'
+  )
+  replica_vm.RemoteCommand(
+      'sudo sysctl -w vm.hugetlb_shm_group=$(getent group postgres | cut -d:'
+      ' -f3)'
+  )
+  replica_vm.RemoteCommand('cat /proc/meminfo |grep -i "^hugepage"')
+  replica_vm.RemoteCommand('sudo cat /proc/sys/vm/hugetlb_shm_group')
+  replica_vm.RemoteCommand(
+      'sudo systemctl set-property'
+      f' {GetOSDependentDefaults(replica_vm.OS_TYPE)["postgres_service_name"]}.service'
+      f' MemoryMax={SHARED_BUFFERS_CONF["SIZE_10GB"]["max_memory"]}'
+  )
+  replica_vm.RemoteCommand(
+      'sudo sync; echo 3 | sudo tee -a /proc/sys/vm/drop_caches'
+  )
+  replica_vm.RemoteCommand(
+      f'cat /etc/systemd/system.control/{GetOSDependentDefaults(replica_vm.OS_TYPE)["postgres_service_name"]}.service.d/50-MemoryMax.conf'
+  )
+  replica_vm.RemoteCommand(
+      'sudo systemctl restart'
+      f' {GetOSDependentDefaults(replica_vm.OS_TYPE)["postgres_service_name"]}'
+  )
+
+
+def GetPsqlUserPassword(run_uri):
+  return run_uri + '_' + SYSBENCH_PASSWORD
