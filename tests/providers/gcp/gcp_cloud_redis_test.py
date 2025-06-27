576a963b9b2cd113fc0785cf8574cd9c3c829a6a
Brandon Liu <liubrandon@google.com>
2025-06-27 07:50:26 -0700

    Increase Cloud Redis creation timeout to match Valkey.
    PiperOrigin-RevId: 776582115

--- tests/providers/gcp/gcp_cloud_redis_test.py
+++ tests/providers/gcp/gcp_cloud_redis_test.py
@@ -45,7 +45,7 @@ class GcpCloudRedisTestCase(pkb_common_test_case.PkbCommonTestCase):
         util.GcloudCommand, 'Issue', return_value=('{}', '', 0)
     ) as gcloud:
       self.redis._Create()
-      gcloud.assert_called_once_with(timeout=1200)
+      gcloud.assert_called_once()
       self.assertTrue(self.redis._Exists())
 
   def testDelete(self):
@@ -53,7 +53,7 @@ class GcpCloudRedisTestCase(pkb_common_test_case.PkbCommonTestCase):
         util.GcloudCommand, 'Issue', return_value=('{}', '', 0)
     ) as gcloud:
       self.redis._Delete()
-      gcloud.assert_called_with(raise_on_failure=False, timeout=1200)
+      gcloud.assert_called_once()
 
   def testExistTrue(self):
     with mock.patch.object(
