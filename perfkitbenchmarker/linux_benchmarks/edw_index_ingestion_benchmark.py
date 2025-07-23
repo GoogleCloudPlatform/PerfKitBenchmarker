6bcf6fc53efde60a0c3384556801306d8c3b1b37
jguertin <157435133+jnguertin@users.noreply.github.com>
2025-07-23 04:02:18 +0000

    Added additional metadata collecttion, inter-thread synchronization.

--- perfkitbenchmarker/linux_benchmarks/edw_index_ingestion_benchmark.py
+++ perfkitbenchmarker/linux_benchmarks/edw_index_ingestion_benchmark.py
@@ -30,15 +30,18 @@ Example run command:
 --project=p3rf-bq-search \
 --zones=us-central1-c 
 """
+from multiprocessing.managers import ListProxy
 import time
 
 
 import logging
 import os
+from typing import Any
 from absl import flags
 from perfkitbenchmarker import configs
 from perfkitbenchmarker import edw_benchmark_results_aggregator as results_aggregator
 import multiprocessing
+from multiprocessing.synchronize import Event as SyncEvent
 from perfkitbenchmarker import edw_service
 from perfkitbenchmarker import sample
 
@@ -99,21 +102,25 @@ def Prepare(benchmark_spec):
 
 
 # TODO: Record metrics from these queries too
-def _execute_data_load(client_interface, concurrent_ingestion_query, load_time = 32):
+def _execute_data_load(client_interface: edw_service.EdwClientInterface, concurrent_ingestion_query:str, complete:SyncEvent, results:ListProxy[sample.Sample]):
   """Executes the data loading query."""
   logging.info('Starting data loading loop')
-  for i in range(load_time):
-    client_interface.ExecuteQuery(concurrent_ingestion_query) 
+  iter = 0
+  while (not complete.is_set()):
+     execution_time, metadata = client_interface.ExecuteQuery(concurrent_ingestion_query) 
+     metadata['load_iter'] = iter
+     iter += 1
+     results.append(sample.Sample('load_query_execution_time', execution_time, 'seconds', metadata))
   logging.info('Data loading loop completed')
 
 
-def _execute_index_queries(client_interface, search_query, results, num_queries=5, interval=0):
+def _execute_index_queries(client_interface: edw_service.EdwClientInterface, search_query:str, results:ListProxy[sample.Sample], num_queries=5, interval=0):
   """Executes the index search query in a separate process."""
   logging.info('Starting index search query loop')
   for i in range(num_queries):
     time.sleep(interval)
     logging.info('Running index search query iteration %d', i + 1)
-    execution_time, metadata = client_interface.ExecuteQuery(search_query) 
+    execution_time, metadata = client_interface.ExecuteQuery(search_query, print_results=True) 
     logging.info('Index search query iteration %d completed in execution time: %d', i + 1, execution_time)
     metadata['index_query_iter'] = i
     results.append(sample.Sample('query_execution_Time', execution_time, 'seconds', metadata))
@@ -122,25 +129,32 @@ def _execute_index_queries(client_interface, search_query, results, num_queries=
 
 def Run(benchmark_spec):
   """Run phase executes the sql scripts on edw cluster and collects duration."""
-  results = []
 
-  edw_service_instance = benchmark_spec.edw_service
-  client_interface = edw_service_instance.GetClientInterface()
+  results  = []
 
-  client_interface.ExecuteQuery('load_init_data_query') 
-  client_interface.ExecuteQuery('create_index_query') 
-  
-  # Create separate processes for concurrent data ingestion and index querying
-  data_load_process = multiprocessing.Process(
-      target=_execute_data_load,
-      args=(client_interface, 'concurrent_ingestion_query')
-  )
+  with multiprocessing.Manager() as manager:
+    results_proxy: ListProxy[sample.Sample] = manager.list()
+    edw_service_instance: edw_service.EdwService = benchmark_spec.edw_service
+    client_interface: edw_service.EdwClientInterface = edw_service_instance.GetClientInterface()
 
-  data_load_process.start()
+    client_interface.ExecuteQuery('load_init_data_query') 
+    client_interface.ExecuteQuery('create_index_query') 
+    
+    searches_finished = multiprocessing.Event()
 
-  _execute_index_queries(client_interface, 'search_query', results, num_queries=FLAGS.edw_index_ingestion_search_iterations)
-  data_load_process.join()
+    # Create separate processes for concurrent data ingestion and index querying
+    data_load_process = multiprocessing.Process(
+        target=_execute_data_load,
+        args=(client_interface, 'concurrent_ingestion_query', searches_finished, results_proxy)
+    )
 
+    data_load_process.start()
+    logging.info("Starting search query dispatch")
+    _execute_index_queries(client_interface, 'search_query', results_proxy, num_queries=FLAGS.edw_index_ingestion_search_iterations)
+    logging.info("Search queries completed. Ending data load.")
+    searches_finished.set()
+    data_load_process.join()
+    results = list(results_proxy)
 
   return results
 
