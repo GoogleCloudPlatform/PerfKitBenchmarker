6b39b35da0fb0c94b46243458001ec05a9821151
Zach Howell <howellz@google.com>
2025-11-21 11:09:51 -0800

    Wait even longer (40m) for AKS Automatic to start
    Waited 10m, then 25m in previous increase, now waiting 40m
    
    PiperOrigin-RevId: 835289917

--- perfkitbenchmarker/providers/azure/azure_kubernetes_service.py
+++ perfkitbenchmarker/providers/azure/azure_kubernetes_service.py
@@ -489,8 +489,8 @@ class AksAutomaticCluster(AksCluster):
   CLUSTER_TYPE = 'Auto'
 
   # Control how long to WaitUntilReady
-  READY_TIMEOUT = 25 * 60  # 25 minutes
-  POLL_INTERVAL = 20
+  READY_TIMEOUT = 40 * 60  # 40 minutes
+  POLL_INTERVAL = 40
 
   def _Create(self):
     """Creates the Automatic AKS cluster with tags."""
