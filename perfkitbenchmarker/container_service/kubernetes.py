c0bf845deb2d338c4bea9d399cd4d15b4f673689
p3rf Team <p3rf-github-robot@google.com>
2026-01-15 13:22:17 -0800

    Refactor kubectl methods out of container_service/kubernetes.py
    PiperOrigin-RevId: 856792439

--- perfkitbenchmarker/container_service/kubernetes.py
+++ perfkitbenchmarker/container_service/kubernetes.py
@@ -37,6 +37,12 @@ from perfkitbenchmarker.resources import kubernetes_inference_server
 import requests
 import yaml
 
+# Temporarily hoist kubectl related methods into this namespace
+# pylint: disable=unused-import
+from .kubectl import RETRYABLE_KUBECTL_ERRORS
+from .kubectl import RunKubectlCommand
+from .kubectl import RunRetryableKubectlCommand
+# pylint: enable=unused-import
 # Temporarily hoist k8s cluster commands into this namespace
 from .kubernetes_commands import KubernetesClusterCommands
 
@@ -64,86 +70,7 @@ spec:
     serviceName: {service_name}
     servicePort: 8080
 """
-RETRYABLE_KUBECTL_ERRORS = [
-    (
-        '"unable to decode an event from the watch stream: http2: client'
-        ' connection lost"'
-    ),
-    'read: connection reset by peer',
-    'Unable to connect to the server: dial tcp',
-    'Unable to connect to the server: net/http: TLS handshake timeout',
-    # These come up in kubectl exec
-    'error dialing backend:',
-    'connect: connection timed out',
-    'error sending request:',
-    '(abnormal closure): unexpected EOF',
-]
 INGRESS_JSONPATH = '{.status.loadBalancer.ingress[0]}'
-
-
-def RunKubectlCommand(command: list[str], **kwargs) -> tuple[str, str, int]:
-  """Run a kubectl command."""
-  kwargs = vm_util.IncrementStackLevel(**kwargs)
-  cmd = [
-      flags.FLAGS.kubectl,
-      '--kubeconfig',
-      flags.FLAGS.kubeconfig,
-  ] + command
-
-  orig_suppress_failure = None
-  if 'suppress_failure' in kwargs:
-    orig_suppress_failure = kwargs['suppress_failure']
-
-  def _DetectTimeoutViaSuppressFailure(stdout, stderr, retcode):
-    # Check for kubectl timeout. If found, treat it the same as a regular
-    # timeout.
-    if retcode != 0:
-      for error_substring in RETRYABLE_KUBECTL_ERRORS:
-        if error_substring in stderr:
-          # Raise timeout error regardless of raise_on_failure - as the intended
-          # semantics is to ignore expected errors caused by invoking the
-          # command not errors from PKB infrastructure.
-          raise_on_timeout = (
-              kwargs['raise_on_timeout']
-              if 'raise_on_timeout' in kwargs
-              else True
-          )
-          if raise_on_timeout:
-            raise errors.VmUtil.IssueCommandTimeoutError(stderr)
-    # Else, revert to user supplied kwargs values.
-    if orig_suppress_failure is not None:
-      return orig_suppress_failure(stdout, stderr, retcode)
-    if 'raise_on_failure' in kwargs:
-      return not kwargs['raise_on_failure']
-    return False
-
-  kwargs['suppress_failure'] = _DetectTimeoutViaSuppressFailure
-
-  return vm_util.IssueCommand(cmd, **kwargs)
-
-
-def RunRetryableKubectlCommand(
-    run_cmd: list[str], timeout: int | None = None, **kwargs
-) -> tuple[str, str, int]:
-  """Runs a kubectl command, retrying somewhat expected errors."""
-  if 'raise_on_timeout' in kwargs and kwargs['raise_on_timeout']:
-    raise ValueError(
-        'RunRetryableKubectlCommand does not allow `raise_on_timeout=True`'
-        ' (since timeouts are retryable).'
-    )
-
-  kwargs = vm_util.IncrementStackLevel(**kwargs)
-
-  @vm_util.Retry(
-      timeout=timeout,
-      retryable_exceptions=(errors.VmUtil.IssueCommandTimeoutError,),
-  )
-  def _RunRetryablePart(run_cmd: list[str], **kwargs) -> tuple[str, str, int]:
-    """Inner function retries command so timeout can be passed to decorator."""
-    kwargs['stack_level'] += 1
-    return RunKubectlCommand(run_cmd, raise_on_timeout=True, **kwargs)
-
-  return _RunRetryablePart(run_cmd, timeout=timeout, **kwargs)
 
 
 class KubernetesPod:
