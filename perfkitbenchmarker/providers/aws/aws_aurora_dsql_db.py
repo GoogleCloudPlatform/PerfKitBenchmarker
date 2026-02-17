c38854c61250325490063af19cc7596a33ad6813
Scott Lin <shuninglin@google.com>
2026-02-17 11:55:56 -0800

    Run ANALYZE after loading data
    PiperOrigin-RevId: 871428196

--- perfkitbenchmarker/providers/aws/aws_aurora_dsql_db.py
+++ perfkitbenchmarker/providers/aws/aws_aurora_dsql_db.py
@@ -19,6 +19,7 @@ from typing import Any
 
 from absl import flags
 from absl import logging
+import boto3
 from perfkitbenchmarker import errors
 from perfkitbenchmarker import relational_db_spec
 from perfkitbenchmarker import sql_engine_utils
@@ -266,6 +267,34 @@ class AwsAuroraDsqlRelationalDb(aws_relational_db.BaseAwsRelationalDb):
         raise errors.Resource.RetryableDeletionError('Not yet deleted')
 
     WaitUntilClusterDeleted()
+
+  def _GetHostname(self) -> str:
+    """Returns endpoint of DSQL cluster."""
+    return f'{self.cluster_id}.dsql.{self.region}.on.aws'
+
+# TODO(shuninglin): Extend PostgresCliQueryTools for DSQL.
+  def RunSqlQuery(self, sql_query: str) -> None:
+    """Runs a SQL query on the database."""
+    hostname = self._GetHostname()
+    # Since we are using aws cli v1 where the token generation is not supported,
+    # we are using sdk here to generate the token.
+    client = boto3.client('dsql', region_name=self.region)
+    token = client.generate_db_connect_admin_auth_token(hostname, self.region)
+    connection_properties = sql_engine_utils.DbConnectionProperties(
+        engine=self.spec.engine,
+        engine_version=self.spec.engine_version,
+        endpoint=hostname,
+        port=5432,
+        database_username='admin',
+        database_password=token,
+        instance_name=self.cluster_id,
+        database_name='postgres',
+    )
+    query_tools = sql_engine_utils.PostgresCliQueryTools(
+        self.client_vm, connection_properties
+    )
+    cmd = query_tools.MakeSqlCommand(sql_query, database_name='postgres')
+    self.client_vm.RemoteCommand(f'PGSSLMODE=require {cmd}')
 
   @staticmethod
   def GetDefaultEngineVersion(engine) -> str:
