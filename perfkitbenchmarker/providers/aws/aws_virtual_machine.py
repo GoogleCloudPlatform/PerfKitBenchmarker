771a2c5f62da33d124c8a660deb7df341052c9fb
Patrick Clay <pclay@google.com>
2026-09-01 10:41:55 -0700

    Support R9g on AWS EC2
    PiperOrigin-RevId: 974581098

--- perfkitbenchmarker/providers/aws/aws_virtual_machine.py
+++ perfkitbenchmarker/providers/aws/aws_virtual_machine.py
@@ -128,6 +128,7 @@ _MACHINE_TYPE_PREFIX_TO_ARM_ARCH = {
     'r8g': 'graviton4',
     'i8g': 'graviton4',
     'm9g': 'graviton5',
+    'r9g': 'graviton5',
     't4g': 'graviton2',
     'im4g': 'graviton2',
     'is4ge': 'graviton2',
